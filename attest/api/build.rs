f6969747a518c59ff7766b3831543e9616db098f
Eran Rundstein <eran@rundste.in>
2020-04-15 13:26:34 -0700

    Switch to build-grpc (#7)
    * switch mobilecoin-api to using the new build-grpc crate
    
    * switch attest-api to using the new build-grpc crate
    
    * switch grpc-util to using the new build-grpc crate

--- attest/api/build.rs
+++ attest/api/build.rs
@@ -1,20 +1,5 @@
 // Copyright (c) 2018-2020 MobileCoin Inc.
 
-extern crate protoc_grpcio;
-
-fn compile_protos() {
-    let proto_root = "./proto";
-    let proto_files = ["attest.proto"];
-    let output_destination = "src";
-    println!("cargo:rerun-if-changed={}", proto_root);
-    for file in &proto_files {
-        println!("cargo:rerun-if-changed={}/{}", proto_root, file);
-    }
-
-    protoc_grpcio::compile_grpc_protos(&proto_files, &[proto_root], output_destination)
-        .expect("Failed to compile gRPC definitions!");
-}
-
 fn main() {
-    compile_protos();
+    mc_build_grpc::compile_protos_and_generate_mod_rs(&["./proto"], &["attest.proto"]);
 }
