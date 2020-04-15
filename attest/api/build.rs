40336e61e21bf250caa6ab0d12f7dfcc5bad0291
James Cape <james@mobilecoin.com>
2020-04-15 15:55:19 -0700

    rustfmt

--- attest/api/build.rs
+++ attest/api/build.rs
@@ -5,7 +5,14 @@ use mcbuild_utils::Environment;
 fn main() {
     let env = Environment::default();
     let proto_dir = env.dir().join("proto");
-    cargo_emit::pair!("PROTOS_PATH", "{}", proto_dir.as_os_str().to_str().expect("Invalid UTF-8 in proto dir path"));
+    cargo_emit::pair!(
+        "PROTOS_PATH",
+        "{}",
+        proto_dir
+            .as_os_str()
+            .to_str()
+            .expect("Invalid UTF-8 in proto dir path")
+    );
 
     mc_build_grpc::compile_protos_and_generate_mod_rs(&["./proto"], &["attest.proto"]);
 }
--- consensus/api/build.rs
+++ consensus/api/build.rs
@@ -6,10 +6,16 @@ fn main() {
     let env = Environment::default();
 
     let proto_dir = env.dir().join("proto");
-    let proto_str = proto_dir.as_os_str().to_str().expect("Invalid UTF-8 in proto dir");
+    let proto_str = proto_dir
+        .as_os_str()
+        .to_str()
+        .expect("Invalid UTF-8 in proto dir");
     cargo_emit::pair!("PROTOS_PATH", "{}", proto_str);
 
-    let attest_proto_path = env.depvar("MC_ATTEST_API_PROTOS_PATH").expect("Could not read attest api's protos path").to_owned();
+    let attest_proto_path = env
+        .depvar("MC_ATTEST_API_PROTOS_PATH")
+        .expect("Could not read attest api's protos path")
+        .to_owned();
     let mut all_proto_dirs = attest_proto_path.split(':').collect::<Vec<&str>>();
     all_proto_dirs.push(proto_str);
 
