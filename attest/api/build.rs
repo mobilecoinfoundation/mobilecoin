3bee6f7961febb690cab2520bb13fa91b06b4bf9
James Cape <james@mobilecoin.com>
2020-04-15 15:44:18 -0700

    Fix path in attest-api, add to consensus

--- attest/api/build.rs
+++ attest/api/build.rs
@@ -4,7 +4,7 @@ use mcbuild_utils::Environment;
 
 fn main() {
     let env = Environment::default();
-    let proto_dir = env.out_dir().join("proto");
+    let proto_dir = env.dir().join("proto");
     cargo_emit::pair!("PROTOS_PATH", "{}", proto_dir.as_os_str().to_str().expect("Invalid UTF-8 in proto dir path"));
 
     mc_build_grpc::compile_protos_and_generate_mod_rs(&["./proto"], &["attest.proto"]);
--- consensus/api/build.rs
+++ consensus/api/build.rs
@@ -1,8 +1,20 @@
 // Copyright (c) 2018-2020 MobileCoin Inc.
 
+use mcbuild_utils::Environment;
+
 fn main() {
+    let env = Environment::default();
+
+    let proto_dir = env.dir().join("proto");
+    let proto_str = proto_dir.as_os_str().to_str().expect("Invalid UTF-8 in proto dir");
+    cargo_emit::pair!("PROTOS_PATH", "{}", proto_str);
+
+    let attest_proto_path = env.depvar("MC_ATTEST_API_PROTOS_PATH").expect("Could not read attest api's protos path").to_owned();
+    let mut all_proto_dirs = attest_proto_path.split(':').collect::<Vec<&str>>();
+    all_proto_dirs.push(proto_str);
+
     mc_build_grpc::compile_protos_and_generate_mod_rs(
-        &["./proto", "../../attest/api/proto"],
+        all_proto_dirs.as_slice(),
         &[
             "transaction.proto",
             "blockchain.proto",
