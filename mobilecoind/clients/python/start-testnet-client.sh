91b2c8f9ddf34fc75f7f28acc3312899370905a7
sugargoat <sara.drakeley@mobilecoin.com>
2020-04-15 10:07:59 -0700

    Fix missing check (#51)
    * Fix missing check
    
    * Speed up grpc install
    
    * Better wording

--- mobilecoind/clients/python/start-testnet-client.sh
+++ mobilecoind/clients/python/start-testnet-client.sh
@@ -5,7 +5,11 @@
 set -e
 
 if [[ PYTHON_ENV=$(python -c "import sys; sys.stdout.write('1') if hasattr(sys, 'real_prefix') else sys.stdout.write('0')") ]]; then
-    echo "Not running in a virtual env. Installing requirements may have side effects."
+    echo "Not running in a virtual env. Starting virtual env now. This will speed up requirements installation in docker."
+    # FIXME: Only apt-get if in the mob container
+    apt-get install -y python3-venv
+    python3 -m venv env
+    source ./env/bin/activate
 fi
 
 echo "Downloading requirements."
@@ -15,4 +19,4 @@ echo "Compiling Protos."
 ./compile_proto.sh
 
 echo "Starting python wallet client."
-python3 ./main.py --mobilecoind localhost:4444
+python3 ./main.py localhost:4444
