d173f6b17f4192eb5bf09b38b80c3e3ef4dad9db
Joe Kottke <joe.kottke@gmail.com>
2020-04-20 12:06:27 -0700

    Add container builds to Jenkinsfile

---
+++ ops/entrypoints/consensus_validator.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# This script will start the aesm_service daemon, if the isgx device node is
+# present in /dev, then run consensus service with any given arguments.
+#
+# NOTE: Some of the environment variables are set in the Docker container, and
+#       some are set in the docker run command
+
+# exit if anything fails.
+set -e
+
+if [ -c /dev/isgx ]; then
+  # Use a subshell to prevent environment leakage
+  (
+    export AESM_PATH=/opt/intel/sgx-aesm-service/aesm
+    export LD_LIBRARY_PATH=${AESM_PATH}
+
+    ${AESM_PATH}/linksgx.sh
+    /bin/mkdir -p /var/run/aesmd/
+    /bin/chown -R aesmd:aesmd /var/run/aesmd/
+    /bin/chmod 0755 /var/run/aesmd/
+    /bin/chown -R aesmd:aesmd /var/opt/aesmd/
+    ${AESM_PATH}/aesm_service &
+  )
+
+  sleep 1
+fi
+
+# If the ledgerdir is not already populated, copy the origin block.
+if [ ! -r "${NODE_LEDGER_DIR}/data.mdb" ]; then
+    mkdir -p "${NODE_LEDGER_DIR}"
+    rsync -a --delete /var/lib/mobilecoin/origin_data/* ${NODE_LEDGER_DIR}/
+fi
+
+if [[ -z "${AWS_PATH}" ]] || [[ -z "${AWS_SECRET_ACCESS_KEY}" ]] || [[ -z "${AWS_ACCESS_KEY_ID}" ]]; then
+  echo "Warning: Must provide AWS_PATH, AWS_SECRET_ACCESS_KEY, and AWS_ACCESS_KEY_ID to start ledger distribution";
+else
+  /usr/bin/ledger-distribution \
+    --ledger-path "${NODE_LEDGER_DIR}" \
+    --start-from "next" \
+    --dest "${AWS_PATH}" &
+fi
+
+# Clean old dump directory - consensus writes a new dir, which is owned by root due to docker volume ownership
+rm -rf /scp-debug-dump/${LOCAL_NODE_ID}
+
+exec env consensus-service $@
---
+++ ops/entrypoints/mobilecoind.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# NOTE: Some of the environment variables are set in the Docker container, and
+#       some are set in the docker run command
+
+# exit if anything fails.
+set -ex
+
+# Make the mobilecoind_db dir (should not yet exist)
+mkdir -p "${MOBILECOIND_DB_DIR}"
+
+mobilecoind \
+    --peer mc://node1.${BRANCH}.mobilecoin.com/ \
+    --peer mc://node2.${BRANCH}.mobilecoin.com/ \
+    --peer mc://node3.${BRANCH}.mobilecoin.com/ \
+    --peer mc://node4.${BRANCH}.mobilecoin.com/ \
+    --peer mc://node5.${BRANCH}.mobilecoin.com/ \
+    --tx-source-url https://s3-us-west-1.amazonaws.com/mobilecoin.chain/node1.${BRANCH}.mobilecoin.com/ \
+    --tx-source-url https://s3-us-west-1.amazonaws.com/mobilecoin.chain/node2.${BRANCH}.mobilecoin.com/ \
+    --ledger-db "${NODE_LEDGER_DIR}" \
+    --poll-interval 1 \
+    --mobilecoind-db "${MOBILECOIND_DB_DIR}" \
+    --service-port "${MOBILECOIND_SERVICE_PORT}" &
+
+mcd_pid=$!
+
+# Let ledger_db populate
+sleep 5
+
+wait "${mcd_pid}"
