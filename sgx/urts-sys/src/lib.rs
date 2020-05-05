a950df649b6a56202d88d108784f091fd4503b87
James Cape <james@mobilecoin.com>
2020-05-05 00:20:00 -0700

    [MC-1232] uRTS bindgen, fiddling with quote rustdoc #done
    * URTS bindgen, fiddling with quote rustdoc.
    * Add sgx-sim feature to urts-sys, epid-sys.

---
+++ sgx/urts-sys/src/lib.rs
@@ -0,0 +1,16 @@
+// Copyright (c) 2018-2020 MobileCoin Inc.
+
+//! Intel SGX URTS FFI
+
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(clippy::missing_safety_doc)]
+#![allow(clippy::useless_transmute)]
+
+use mc_sgx_core_types_sys::{
+    sgx_config_id_t, sgx_config_svn_t, sgx_enclave_id_t, sgx_misc_attribute_t, sgx_status_t,
+    sgx_target_info_t,
+};
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
