867a6ca534b963f4baf1f956d7192458d18c4158
sugargoat <sugargoat@mobilecoin.com>
2020-05-10 11:14:13 -0700

    Add display for block signers

--- mobilecoind/clients/python/blockchain_explorer.py
+++ mobilecoind/clients/python/blockchain_explorer.py
@@ -8,30 +8,26 @@ from sys import getsizeof
 client = mob_client('localhost:4444', False)
 app = Flask(__name__)
 
+
 def command_args():
     parser = argparse.ArgumentParser(description='MobileCoin Block Explorer')
-    parser.add_argument(
-        '--port',
-        type=int,
-        required=False,
-        default=5000,
-        help='Block Explorer listen port'
-    )
-    parser.add_argument(
-        '--mobilecoind_port',
-        type=int,
-        required=False,
-        default=4444,
-        help='Port of mobilecoind service to connect to'
-    )
-    parser.add_argument(
-        '--mobilecoind_host',
-        type=str,
-        required=False,
-        default='localhost',
-        help='Hostname of mobilecoind service to connect to'
-    )
+    parser.add_argument('--port',
+                        type=int,
+                        required=False,
+                        default=5000,
+                        help='Block Explorer listen port')
+    parser.add_argument('--mobilecoind_port',
+                        type=int,
+                        required=False,
+                        default=4444,
+                        help='Port of mobilecoind service to connect to')
+    parser.add_argument('--mobilecoind_host',
+                        type=str,
+                        required=False,
+                        default='localhost',
+                        help='Hostname of mobilecoind service to connect to')
     return parser.parse_args()
+
 
 @app.route('/')
 def index():
@@ -45,16 +41,14 @@ def index():
         if txo_count > 20000:
             continue
         block = client.get_block(i)
-        size_of_block = getsizeof(block) * .001
-        print("block.signatures = ", block.signatures)
-        block_row = (i, txo_count, size_of_block, len(block.signatures),
-                     bytes.hex(block.block.contents_hash.data))
+        block_row = (i, txo_count, bytes.hex(block.block.contents_hash.data),
+                     len(block.signatures))
         blocks.append(block_row)
 
         # Process signature data - sort by signer
         for signature_data in block.signatures:
-            signature = signature_data.signature
-            signer = signature_data.signer
+            signature = signature_data.signature.signature
+            signer = bytes.hex(signature_data.signature.signer.data)
             # If a new signer has appeared, prepend False for all previous blocks
             if signer not in signers:
                 signers[signer] = [False for i in range(i - 1)]
@@ -71,6 +65,9 @@ def index():
 def block(block_num):
     block = client.get_block(int(block_num))
     size_of_block = getsizeof(block)
+
+    for signature in block.signatures:
+        signature.src_url = signature.src_url.split('/')[-2]
     return render_template('block.html',
                            block_num=int(block_num),
                            block_hash=block.block.contents_hash.data,
@@ -81,7 +78,9 @@ def block(block_num):
                            size_of_block=size_of_block,
                            signatures=block.signatures)
 
+
 if __name__ == "__main__":
     args = command_args()
-    client = mob_client(args.mobilecoind_host + ':' + str(args.mobilecoind_port), False)
+    client = mob_client(
+        args.mobilecoind_host + ':' + str(args.mobilecoind_port), False)
     app.run(host='0.0.0.0', port=str(args.port))
