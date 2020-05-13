d56f67c954e5e90d83f9aa0e50145f8f71c006f9
sugargoat <sugargoat@mobilecoin.com>
2020-05-12 21:14:08 -0700

    interact_text -> interact

--- testnet-client/src/main.rs
+++ testnet-client/src/main.rs
@@ -234,7 +234,7 @@ dc74edf1d8842dfdf49d6db5d3d4e873665c2dd400c0955dd9729571826a26be
 
         Input::<EntropyBytes>::new()
             .with_prompt("Enter your master key")
-            .interact_text()
+            .interact()
             .expect("failed getting master key")
             .0
     }
@@ -390,7 +390,7 @@ string that we send you. It should look something like:
         let opt_request_code = Input::<WrappedRequestPayload>::new()
             .with_prompt("Enter the request code to fulfill, or leave blank to cancel")
             .allow_empty(true)
-            .interact_text()
+            .interact()
             .expect("failed getting request code")
             .0;
         if opt_request_code.is_none() {
@@ -586,7 +586,7 @@ MobileCoin forums. Visit http://community.mobilecoin.com
             0 => Input::<String>::new()
                 .with_prompt("Please enter your memo, or leave blank to cancel")
                 .allow_empty(true)
-                .interact_text()
+                .interact()
                 .expect("failed getting memo"),
             1 => String::from(""),
             _ => unreachable!(),
