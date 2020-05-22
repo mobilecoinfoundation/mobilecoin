d21b5eb407773415c94ea600fab484ef710f2f2f
Robb Walters <rjwalters@gmail.com>
2020-05-21 17:56:35 -0700

    test error condition

--- mobilecoind/clients/python/blockchain_explorer/blockchain_explorer.py
+++ mobilecoind/clients/python/blockchain_explorer/blockchain_explorer.py
@@ -69,12 +69,12 @@ def block(block_num):
 
     for signature in block.signatures:
         signature.src_url = signature.src_url.split('/')[-2]
-
+    '''
     if block_num >= num_blocks:
         return render_template('block404.html',
                                block_num=int(block_num),
                                num_blocks=num_blocks)
-
+    '''
     return render_template('block.html',
                            block_num=int(block_num),
                            block_hash=block.block.contents_hash.data,
