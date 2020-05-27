78dce268a95f99c5fa3051349a302cf6fe664f8f
Robb Walters <robb@mobilecoin.com>
2020-05-27 14:14:15 -0700

    return full codes

--- mobilecoind/clients/python/pizzamob_leaderboard/pizzamob_leaderboard.py
+++ mobilecoind/clients/python/pizzamob_leaderboard/pizzamob_leaderboard.py
@@ -121,7 +121,7 @@ def get_leaderboard():
     for p in players:
         monitor = get_or_add_monitor(p['subaddress'])
         balance = client.get_balance(monitor, p['subaddress'])
-        res.append({'balance': balance, 'code': p['code'][:17]})
+        res.append({'balance': balance, 'code': p['code']})
     res.sort(key=lambda player: player['balance'])
     res.reverse()
     return res
