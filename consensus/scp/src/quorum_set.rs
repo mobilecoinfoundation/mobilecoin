f267a7c3d210032f3f4e9378d29f9e8cd192ee7d
Robb Walters <rjwalters@gmail.com>
2020-06-21 14:01:35 -0700

    print pairs

--- consensus/scp/src/quorum_set.rs
+++ consensus/scp/src/quorum_set.rs
@@ -783,7 +783,9 @@ mod quorum_set_parser_tests {
     #[test]
     fn test_qs_parser() {
         // simple quorum set
-        let successful_parse = QuorumSetParser::parse(Rule::qs, "([3],1,2,3,4,([2],5,6,([1],8,7)))");
-        println!("{:?}", successful_parse);
+        let mut pairs = QuorumSetParser::parse(Rule::qs, "([3],1,2,3,4,([2],5,6,([1],8,7)))").expect("parsing failed!");
+        for p in pairs {
+            println!("{:?}", p);
+        }
     }
 }
