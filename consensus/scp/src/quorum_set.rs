f16bcd34791d82289a82cfd269b642389c3c22f9
Robb Walters <rjwalters@gmail.com>
2020-06-21 21:47:55 -0700

    fix test

--- consensus/scp/src/quorum_set.rs
+++ consensus/scp/src/quorum_set.rs
@@ -900,7 +900,7 @@ mod quorum_set_parser_tests {
         //assert_eq!(qs3, qs4);
 
         let qs5 = qs_from_string("([1], ([1],1,2), ([1],3,4) )").expect("failed to parse");
-        let qs6 = qs_from_string("([1], ([1],4,2), ([1],2,1) )").expect("failed to parse");
+        let qs6 = qs_from_string("([1], ([1],4,3), ([1],2,1) )").expect("failed to parse");
         assert_eq!(qs5, qs6);
 
     }
