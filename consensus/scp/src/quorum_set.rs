d204e214a13a5257ab0f7a7af67c45d049c908b9
Robb Walters <robb@mobilecoin.com>
2020-06-23 16:11:24 -0700

    add failing test

--- consensus/scp/src/quorum_set.rs
+++ consensus/scp/src/quorum_set.rs
@@ -386,7 +386,7 @@ mod quorum_set_tests {
 
     #[test]
     // ordering of QuorumSetMembers should not matter
-    fn test_quorum_set_equality() {
+    fn test_quorum_set_equality_with_reordering() {
         let quorum_set_1: QuorumSet = {
             let inner_quorum_set_one = QuorumSet::new_with_node_ids(
                 2,
@@ -395,6 +395,34 @@ mod quorum_set_tests {
             let inner_quorum_set_two = QuorumSet::new_with_node_ids(
                 2,
                 vec![test_node_id(5), test_node_id(6), test_node_id(7)],
+            );
+            QuorumSet::new_with_inner_sets(2, vec![inner_quorum_set_one, inner_quorum_set_two])
+        };
+        let quorum_set_2: QuorumSet = {
+            let inner_quorum_set_one = QuorumSet::new_with_node_ids(
+                2,
+                vec![test_node_id(2), test_node_id(3), test_node_id(4)],
+            );
+            let inner_quorum_set_two = QuorumSet::new_with_node_ids(
+                2,
+                vec![test_node_id(5), test_node_id(7), test_node_id(6)],
+            );
+            QuorumSet::new_with_inner_sets(2, vec![inner_quorum_set_two, inner_quorum_set_one])
+        };
+        assert_eq!(quorum_set_1, quorum_set_2);
+    }
+    
+    #[test]
+    // ordering within inner QuorumSetMembers should not matter
+    fn test_quorum_set_equality_with_inner_set_reordering() {
+        let quorum_set_1: QuorumSet = {
+            let inner_quorum_set_one = QuorumSet::new_with_node_ids(
+                2,
+                vec![test_node_id(2), test_node_id(3), test_node_id(4)],
+            );
+            let inner_quorum_set_two = QuorumSet::new_with_node_ids(
+                2,
+                vec![test_node_id(6), test_node_id(5), test_node_id(7)],
             );
             QuorumSet::new_with_inner_sets(2, vec![inner_quorum_set_one, inner_quorum_set_two])
         };
