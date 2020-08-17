076570736831cb57d5e709d0a0a5c4461042a2cd
Matt Faulkner <matt@mobilecoin.com>
2020-08-17 15:49:40 -0700

    creates a new module

---
+++ consensus/service/src/byzanine_ledger/mod.rs
@@ -0,0 +1,5 @@
+// Copyright (c) 2018-2020 MobileCoin Inc.
+
+//! A Federated, Byzantine Fault-Tolerant Ledger.
+//!
+//! Orchestrates running single-slot consensus, or performing ledger sync with peers.
