b49abaf7f7602344cebd9f7d4638f71ab599cbad
Matt Faulkner <matt@mobilecoin.com>
2020-08-26 20:17:36 -0700

    Edits for brevity

--- consensus/service/src/client_api_service.rs
+++ consensus/service/src/client_api_service.rs
@@ -10,13 +10,12 @@ use crate::{
 };
 use grpcio::{RpcContext, UnarySink};
 use mc_attest_api::attest::Message;
-use mc_common::logger::{log, Logger};
+use mc_common::logger::Logger;
 use mc_consensus_api::{
     consensus_client_grpc::ConsensusClientApi, consensus_common::ProposeTxResponse,
 };
 use mc_consensus_enclave::ConsensusEnclave;
 use mc_ledger_db::Ledger;
-use mc_transaction_core::validation::TransactionValidationError;
 use mc_util_grpc::{rpc_logger, send_result};
 use mc_util_metrics::{self, SVC_COUNTERS};
 use std::sync::Arc;
@@ -29,7 +28,9 @@ pub struct ClientApiService {
     enclave: Arc<dyn ConsensusEnclave + Send + Sync>,
     tx_manager: Arc<dyn TxManager + Send + Sync>,
     ledger: Arc<dyn Ledger + Send + Sync>,
-    scp_client_value_sender: ProposeTxCallback,
+    /// Passes proposed transactions to the consensus service.
+    propose_tx_callback: ProposeTxCallback,
+    /// Returns true if this node is able to process proposed transactions.
     is_serving_fn: Arc<(dyn Fn() -> bool + Sync + Send)>,
     logger: Logger,
 }
@@ -47,88 +48,51 @@ impl ClientApiService {
             enclave,
             tx_manager,
             ledger,
-            scp_client_value_sender,
+            propose_tx_callback: scp_client_value_sender,
             is_serving_fn,
             logger,
         }
     }
 
-    fn real_client_tx_propose(
+    /// Handles a client's proposed transaction.
+    ///
+    /// # Arguments
+    /// `msg` - An encrypted message from a client to the enclave.
+    /// `logger` - Logger
+    fn handle_proposed_tx(
         &mut self,
-        request: Message,
-        logger: &Logger,
+        msg: Message,
     ) -> Result<ProposeTxResponse, ConsensusGrpcError> {
         counters::ADD_TX_INITIATED.inc();
 
         if counters::CUR_NUM_PENDING_VALUES.get() > PENDING_LIMIT {
-            self.enclave.client_discard_message(request.into())?;
-
-            log::trace!(
-                logger,
-                "Ignoring add transaction call, node is over capacity."
-            );
+            // This node is over capacity, and is not accepting proposed transaction.
+            self.enclave.client_discard_message(msg.into())?;
             return Err(ConsensusGrpcError::OverCapacity);
         }
 
-        // Check if node is accepting requests.
         if !(self.is_serving_fn)() {
-            self.enclave.client_discard_message(request.into())?;
-
-            log::info!(
-                logger,
-                "Ignoring add transaction call, not currently serving requests."
-            );
+            // This node is unable to process transactions (e.g. is syncing its ledger).
+            self.enclave.client_discard_message(msg.into())?;
             return Err(ConsensusGrpcError::NotServing);
         }
 
-        let tx_context = self.enclave.client_tx_propose(request.into())?;
-        let tx_hash = tx_context.tx_hash;
+        let tx_context = self.enclave.client_tx_propose(msg.into())?;
 
-        match self.tx_manager.insert(tx_context) {
-            Ok(tx_hash) => {
+        self.tx_manager
+            .insert(tx_context)
+            .map(|tx_hash| {
                 // Submit for consideration in next SCP slot.
-                (*self.scp_client_value_sender)(tx_hash, None, None);
-
+                (*self.propose_tx_callback)(tx_hash, None, None);
                 counters::ADD_TX.inc();
-
-                // Return success.
                 Ok(ProposeTxResponse::new())
-            }
-
-            Err(TxManagerError::TransactionValidation(err)) => {
-                // These errors are common, so only trace them
-                if err == TransactionValidationError::TombstoneBlockExceeded
-                    || err == TransactionValidationError::ContainsSpentKeyImage
-                    || err == TransactionValidationError::ContainsExistingOutputPublicKey
-                {
-                    log::trace!(
-                        logger,
-                        "Error validating transaction {tx_hash}: {err}",
-                        tx_hash = tx_hash.to_string(),
-                        err = format!("{:?}", err)
-                    );
-                } else {
-                    log::debug!(
-                        logger,
-                        "Error validating transaction {tx_hash}: {err}",
-                        tx_hash = tx_hash.to_string(),
-                        err = format!("{:?}", err)
-                    );
+            })
+            .map_err(|err| {
+                if let TxManagerError::TransactionValidation(cause) = &err {
+                    counters::TX_VALIDATION_ERROR_COUNTER.inc(&format!("{:?}", cause));
                 }
-                counters::TX_VALIDATION_ERROR_COUNTER.inc(&format!("{:?}", err));
-                Err(err.into())
-            }
-
-            Err(err) => {
-                log::info!(
-                    logger,
-                    "tx_propose failed for {tx_hash}: {err}",
-                    tx_hash = tx_hash.to_string(),
-                    err = format!("{:?}", err)
-                );
-                Err(err.into())
-            }
-        }
+                err
+            })?
     }
 }
 
@@ -140,20 +104,17 @@ impl ConsensusClientApi for ClientApiService {
         sink: UnarySink<ProposeTxResponse>,
     ) {
         let _timer = SVC_COUNTERS.req(&ctx);
+
+        let response = self
+            .handle_proposed_tx(request)
+            .or_else(ConsensusGrpcError::into)
+            .and_then(|mut resp| {
+                resp.set_num_blocks(self.ledger.num_blocks().map_err(ConsensusGrpcError::from)?);
+                Ok(resp)
+            });
+
         mc_common::logger::scoped_global_logger(&rpc_logger(&ctx, &self.logger), |logger| {
-            send_result(
-                ctx,
-                sink,
-                self.real_client_tx_propose(request, &logger)
-                    .or_else(ConsensusGrpcError::into)
-                    .and_then(|mut resp| {
-                        resp.set_num_blocks(
-                            self.ledger.num_blocks().map_err(ConsensusGrpcError::from)?,
-                        );
-                        Ok(resp)
-                    }),
-                &logger,
-            )
+            send_result(ctx, sink, response, &logger)
         });
     }
 }
@@ -180,6 +141,7 @@ mod tests {
     use std::sync::Arc;
 
     #[test_with_logger]
+
     fn test_client_tx_propose_ok(logger: Logger) {
         let mut consensus_enclave = MockConsensusEnclave::new();
         consensus_enclave
