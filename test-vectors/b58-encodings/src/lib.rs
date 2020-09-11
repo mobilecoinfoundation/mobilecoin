44e93852b5564d793d48bc6973c87474409080b2
Kyle Fleming <kyle@kylefleming.net>
2020-09-11 11:14:15 -0700

    [MCC-1750] Add test vectors for b58 encoding of PublicAddress (#426)

---
+++ test-vectors/b58-encodings/src/lib.rs
@@ -0,0 +1,2 @@
+// Re-export for ease-of-use
+pub use mc_test_vectors_definitions::b58_encodings::*;
---
+++ test-vectors/definitions/src/b58_encodings.rs
@@ -0,0 +1,29 @@
+use mc_util_test_vector::TestVector;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct B58EncodePublicAddressWithoutFog {
+    pub view_public_key: [u8; 32],
+    pub spend_public_key: [u8; 32],
+    pub b58_encoded: String,
+}
+
+impl TestVector for B58EncodePublicAddressWithoutFog {
+    const FILE_NAME: &'static str = "b58_encode_public_address_without_fog";
+    const MODULE_SUBDIR: &'static str = "b58_encodings";
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct B58EncodePublicAddressWithFog {
+    pub view_public_key: [u8; 32],
+    pub spend_public_key: [u8; 32],
+    pub fog_report_url: String,
+    pub fog_report_id: String,
+    pub fog_authority_fingerprint_sig: Vec<u8>,
+    pub b58_encoded: String,
+}
+
+impl TestVector for B58EncodePublicAddressWithFog {
+    const FILE_NAME: &'static str = "b58_encode_public_address_with_fog";
+    const MODULE_SUBDIR: &'static str = "b58_encodings";
+}
