cc9be08069cca7d3d20a86d8bb7e4f54770eaa47
Kyle Fleming <kyle@kylefleming.net>
2020-09-10 17:43:57 -0700

    Add PublicAddress base58 encoding test vectors

--- api/src/display.rs
+++ api/src/display.rs
@@ -74,6 +74,9 @@ mod display_tests {
         external,
         printable::{PaymentRequest, PrintableWrapper, TransferPayload},
     };
+    use datatest::data;
+    use mc_test_vectors_b58_encodings::*;
+    use mc_util_test_vector::TestVector;
 
     fn sample_public_address() -> external::PublicAddress {
         let mut public_address = external::PublicAddress::new();
@@ -99,6 +102,79 @@ mod display_tests {
         let encoded = wrapper.b58_encode().unwrap();
         let decoded = PrintableWrapper::b58_decode(encoded).unwrap();
         assert_eq!(wrapper, decoded);
+    }
+
+    fn printable_wrapper_from_b58_encode_public_address_without_fog(
+        case: &B58EncodePublicAddressWithoutFog,
+    ) -> PrintableWrapper {
+        let mut public_address = external::PublicAddress::new();
+
+        let mut view_bytes = external::CompressedRistretto::new();
+        view_bytes.set_data(case.view_public_key.to_vec());
+        public_address.set_view_public_key(view_bytes);
+
+        let mut spend_bytes = external::CompressedRistretto::new();
+        spend_bytes.set_data(case.spend_public_key.to_vec());
+        public_address.set_spend_public_key(spend_bytes);
+
+        let mut wrapper = PrintableWrapper::new();
+        wrapper.set_public_address(public_address);
+
+        wrapper
+    }
+
+    #[data(B58EncodePublicAddressWithoutFog::from_jsonl("../test-vectors/vectors"))]
+    #[test]
+    fn test_b58_encode_public_address_without_fog(case: B58EncodePublicAddressWithoutFog) {
+        let wrapper = printable_wrapper_from_b58_encode_public_address_without_fog(&case);
+        assert_eq!(wrapper.b58_encode().unwrap(), case.b58_encoded);
+    }
+
+    #[data(B58EncodePublicAddressWithoutFog::from_jsonl("../test-vectors/vectors"))]
+    #[test]
+    fn test_b58_decode_public_address_without_fog(case: B58EncodePublicAddressWithoutFog) {
+        let decoded_wrapper = PrintableWrapper::b58_decode(case.b58_encoded.clone()).unwrap();
+        let expected = printable_wrapper_from_b58_encode_public_address_without_fog(&case);
+        assert_eq!(decoded_wrapper, expected);
+    }
+
+    fn printable_wrapper_from_b58_encode_public_address_with_fog(
+        case: &B58EncodePublicAddressWithFog,
+    ) -> PrintableWrapper {
+        let mut public_address = external::PublicAddress::new();
+
+        let mut view_bytes = external::CompressedRistretto::new();
+        view_bytes.set_data(case.view_public_key.to_vec());
+        public_address.set_view_public_key(view_bytes);
+
+        let mut spend_bytes = external::CompressedRistretto::new();
+        spend_bytes.set_data(case.spend_public_key.to_vec());
+        public_address.set_spend_public_key(spend_bytes);
+
+        public_address.set_fog_report_url(case.fog_report_url.clone());
+        public_address.set_fog_report_id(case.fog_report_id.clone());
+        public_address
+            .set_fog_authority_fingerprint_sig(case.fog_authority_fingerprint_sig.clone());
+
+        let mut wrapper = PrintableWrapper::new();
+        wrapper.set_public_address(public_address);
+
+        wrapper
+    }
+
+    #[data(B58EncodePublicAddressWithFog::from_jsonl("../test-vectors/vectors"))]
+    #[test]
+    fn test_b58_encode_public_address_with_fog(case: B58EncodePublicAddressWithFog) {
+        let wrapper = printable_wrapper_from_b58_encode_public_address_with_fog(&case);
+        assert_eq!(wrapper.b58_encode().unwrap(), case.b58_encoded);
+    }
+
+    #[data(B58EncodePublicAddressWithFog::from_jsonl("../test-vectors/vectors"))]
+    #[test]
+    fn test_b58_decode_public_address_with_fog(case: B58EncodePublicAddressWithFog) {
+        let decoded_wrapper = PrintableWrapper::b58_decode(case.b58_encoded.clone()).unwrap();
+        let expected = printable_wrapper_from_b58_encode_public_address_with_fog(&case);
+        assert_eq!(decoded_wrapper, expected);
     }
 
     #[test]
