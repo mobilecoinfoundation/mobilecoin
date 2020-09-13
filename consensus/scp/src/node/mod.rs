ddf8440c0148a60e4b8904b55402fa271b05ad2e
mfaulk <mfaulk@users.noreply.github.com>
2020-09-12 22:52:59 -0700

    Moves ScpNode trait into separate file. (#444)
    * Separates trait and implementation into different files
    
    * Fixes formatting and exports
    
    * Exports MockScNode

---
+++ consensus/scp/src/node/mod.rs
@@ -0,0 +1,12 @@
+// Copyright (c) 2018-2020 MobileCoin Inc.
+
+//! A node of the consensus network.
+
+mod node_impl;
+mod node_trait;
+
+pub use node_impl::Node;
+pub use node_trait::ScpNode;
+
+#[cfg(test)]
+pub use node_trait::MockScpNode;
