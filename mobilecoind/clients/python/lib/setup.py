0779ebcd7c79cf46491232c53a0b63cd1cc0edcc
Eran Rundstein <eran@rundste.in>
2020-09-21 10:22:36 -0700

    update python package version

--- mobilecoind/clients/python/lib/setup.py
+++ mobilecoind/clients/python/lib/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mobilecoin",
-    version="0.1.1",
+    version="0.2.0",
     author="MobileCoin",
     author_email="support@mobilecoin.com",
     description="Python bindings for the MobileCoin daemon API.",
