ea1b9db07137f91cf2466613fc54b6da99a4e268
Chris Beck <beck.ct@gmail.com>
2020-09-24 08:12:49 -0700

    Delete sgx-backtrace crate, and sgx-panic/panic_unwind feature
    And update some documentation
    
    Also remove dead dependencies in mc-sgx-urts

--- sgx/unwind/build.rs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2018-2020 MobileCoin Inc.
-
-// See rust src/libunwind/build.rs, we have taken only the parts relevant to sgx
-
-fn main() {
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rustc-link-lib=gcc_s");
-}
--- sgx/urts/src/lib.rs
+++ sgx/urts/src/lib.rs
@@ -1,24 +1,14 @@
 // Copyright (c) 2018-2020 MobileCoin Inc.
 
 extern crate mc_common;
-extern crate mc_sgx_libc_types;
 extern crate mc_sgx_slog;
 extern crate mc_sgx_types;
 
 extern crate prost;
 
-#[cfg(feature = "backtrace")]
-extern crate rustc_demangle;
-#[cfg(feature = "backtrace")]
-#[macro_use]
-extern crate lazy_static;
-
 mod enclave;
 mod eprintln;
 mod panic;
 mod slog;
-
-#[cfg(feature = "backtrace")]
-mod backtrace;
 
 pub use enclave::*;
