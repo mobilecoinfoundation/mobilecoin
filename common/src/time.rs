1eabc38892f0d71b5815261c4e663855a9925c82
Eran Rundstein <eran@rundste.in>
2020-09-28 13:47:35 -0700

    add common/src/time.rs

---
+++ common/src/time.rs
@@ -0,0 +1,80 @@
+// Copyright (c) 2018-2020 MobileCoin Inc.
+
+//! Utilities for handling time.
+
+use std::{
+    fmt::Debug,
+    sync::{Arc, Mutex},
+    time::{Duration, SystemTime, SystemTimeError},
+};
+
+/// Abstraction for getting the current time.
+pub trait TimeProvider {
+    type Error: Clone + Debug;
+
+    /// Get the duration of time passed since the unix epoch.
+    fn from_epoch(&self) -> Result<Duration, Self::Error>;
+}
+
+/// An implementation of TimeProvider that relies on Rust's builtin `SystemTime`.
+#[derive(Clone, Debug, Default)]
+pub struct SystemTimeProvider;
+
+impl TimeProvider for SystemTimeProvider {
+    type Error = SystemTimeError;
+
+    fn from_epoch(&self) -> Result<Duration, Self::Error> {
+        SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)
+    }
+}
+
+/// A mock time provider that always returns the same value.
+#[derive(Clone, Debug)]
+pub struct MockTimeProvider {
+    cur_from_epoch: Arc<Mutex<Duration>>,
+}
+
+impl Default for MockTimeProvider {
+    fn default() -> Self {
+        Self {
+            cur_from_epoch: Arc::new(Mutex::new(
+                SystemTimeProvider::default()
+                    .from_epoch()
+                    .expect("failed getting initial value for cur_from_epoch"),
+            )),
+        }
+    }
+}
+
+impl TimeProvider for MockTimeProvider {
+    type Error = ();
+
+    fn from_epoch(&self) -> Result<Duration, Self::Error> {
+        Ok(*self.cur_from_epoch.lock().expect("mutex poisoned"))
+    }
+}
+
+impl MockTimeProvider {
+    pub fn set_cur_from_epoch(&self, new_cur_from_epoch: Duration) {
+        let mut inner = self.cur_from_epoch.lock().expect("mutex poisoned");
+        *inner = new_cur_from_epoch;
+    }
+}
+
+// Blanket implementations
+
+impl<TP: TimeProvider> TimeProvider for Arc<TP> {
+    type Error = TP::Error;
+
+    fn from_epoch(&self) -> Result<Duration, Self::Error> {
+        (**self).from_epoch()
+    }
+}
+
+impl<TP: TimeProvider> TimeProvider for Box<TP> {
+    type Error = TP::Error;
+
+    fn from_epoch(&self) -> Result<Duration, Self::Error> {
+        (**self).from_epoch()
+    }
+}
