4a537f50be9b4268eb605cee0b4ba2234dc321a9
Robb Walters <robb@mobilecoin.com>
2020-09-30 06:12:39 -0700

    add nosetests

--- mobilecoind/clients/python/lib/setup.py
+++ mobilecoind/clients/python/lib/setup.py
@@ -19,4 +19,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    test_suite='nose.collector',
+    tests_require=['nose'],
 )
---
+++ mobilecoind/clients/python/lib/tests/__init__.py
@@ -0,0 +1,3 @@
+# Copyright (c) 2018-2020 MobileCoin Inc.
+
+from .test_utilities import *
