91476e0368c2d7530c022292ed15f1c2bee823b3
Robb Walters <robb@mobilecoin.com>
2020-10-07 14:29:37 -0700

    ::

--- mobilecoind-json/src/bin/main.rs
+++ mobilecoind-json/src/bin/main.rs
@@ -485,7 +485,7 @@ fn check_transfer_status(
 ) -> Result<Json<JsonStatusResponse>, String> {
     // allow receipt to be submitted as either JsonSendPaymentResponse or JsonSenderTxReceipt
     let receipt: JsonSenderTxReceipt = serde_json::from_str(&receipt)
-        .or_else(serde_json::from_str<JsonSendPaymentResponse>(&receipt))
+        .or_else(serde_json::from_str::<JsonSendPaymentResponse>(&receipt))
         .map(|response| response.sender_tx_receipt)
         .map_err(|err| format!("Failed to parse receipt: {}", err))?;
 
