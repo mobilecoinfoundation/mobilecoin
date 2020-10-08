477d65ee500d47606bde85f7a8ebfaf5f027c381
Matt Faulkner <matt@mobilecoin.com>
2020-10-08 13:39:05 -0700

    Uses displaydoc

--- consensus/service/src/api/peer_service_error.rs
+++ consensus/service/src/api/peer_service_error.rs
@@ -1,26 +1,21 @@
-use failure::Fail;
+use displaydoc::Display;
 use mc_transaction_core::tx::TxHash;
 
 /// Errors experienced when handling PeerAPI requests.
-#[derive(Debug, Fail)]
+#[derive(Debug, Display)]
 pub enum PeerServiceError {
-    /// Unknown peer.
-    #[fail(display = "Unknown peer: {}", _0)]
+    /// Unknown peer `{0}`.
     UnknownPeer(String),
 
     /// The ConsensusMsg's signature is invalid.
-    #[fail(display = "ConsensusMsgInvalidSignature")]
     ConsensusMsgInvalidSignature,
 
-    /// Unknown transactions
-    #[fail(display = "Unknown transactions")]
+    /// Unknown transactions `{0:?}`.
     UnknownTransactions(Vec<TxHash>),
 
-    /// Enclave-related error.
-    #[fail(display = "Enclave error: {}", _0)]
+    /// Enclave-related error `{0}`.
     Enclave(mc_consensus_enclave::Error),
 
     /// Something went wrong...
-    #[fail(display = "Internal error")]
     InternalError,
 }
