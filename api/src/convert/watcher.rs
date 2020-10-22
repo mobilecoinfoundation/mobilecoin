169a633eb8797c084ccc67ab7385e2c3178bea56
Eran Rundstein <eran@rundste.in>
2020-10-22 15:42:20 -0700

    move watcher proto stuff into a separate file

--- api/src/convert/watcher.rs
+++ api/src/convert/watcher.rs
@@ -1,40 +1,38 @@
 //! Convert to/from watcher_api::*
 
-use crate::{convert::ConversionError, external};
+use crate::{convert::ConversionError, watcher};
 use mc_watcher_api::TimestampResultCode;
 use std::convert::TryFrom;
 
-impl From<&TimestampResultCode> for external::TimestampResultCode {
+impl From<&TimestampResultCode> for watcher::TimestampResultCode {
     fn from(src: &TimestampResultCode) -> Self {
         match src {
-            TimestampResultCode::TimestampFound => external::TimestampResultCode::TimestampFound,
-            TimestampResultCode::WatcherBehind => external::TimestampResultCode::WatcherBehind,
-            TimestampResultCode::Unavailable => external::TimestampResultCode::Unavailable,
+            TimestampResultCode::TimestampFound => watcher::TimestampResultCode::TimestampFound,
+            TimestampResultCode::WatcherBehind => watcher::TimestampResultCode::WatcherBehind,
+            TimestampResultCode::Unavailable => watcher::TimestampResultCode::Unavailable,
             TimestampResultCode::WatcherDatabaseError => {
-                external::TimestampResultCode::WatcherDatabaseError
+                watcher::TimestampResultCode::WatcherDatabaseError
             }
             TimestampResultCode::BlockIndexOutOfBounds => {
-                external::TimestampResultCode::BlockIndexOutOfBounds
+                watcher::TimestampResultCode::BlockIndexOutOfBounds
             }
         }
     }
 }
 
-impl TryFrom<&external::TimestampResultCode> for TimestampResultCode {
+impl TryFrom<&watcher::TimestampResultCode> for TimestampResultCode {
     type Error = ConversionError;
 
-    fn try_from(src: &external::TimestampResultCode) -> Result<Self, Self::Error> {
+    fn try_from(src: &watcher::TimestampResultCode) -> Result<Self, Self::Error> {
         match src {
-            external::TimestampResultCode::UnusedField => Err(ConversionError::ObjectMissing),
-            external::TimestampResultCode::TimestampFound => {
-                Ok(TimestampResultCode::TimestampFound)
-            }
-            external::TimestampResultCode::WatcherBehind => Ok(TimestampResultCode::WatcherBehind),
-            external::TimestampResultCode::Unavailable => Ok(TimestampResultCode::Unavailable),
-            external::TimestampResultCode::WatcherDatabaseError => {
+            watcher::TimestampResultCode::UnusedField => Err(ConversionError::ObjectMissing),
+            watcher::TimestampResultCode::TimestampFound => Ok(TimestampResultCode::TimestampFound),
+            watcher::TimestampResultCode::WatcherBehind => Ok(TimestampResultCode::WatcherBehind),
+            watcher::TimestampResultCode::Unavailable => Ok(TimestampResultCode::Unavailable),
+            watcher::TimestampResultCode::WatcherDatabaseError => {
                 Ok(TimestampResultCode::WatcherDatabaseError)
             }
-            external::TimestampResultCode::BlockIndexOutOfBounds => {
+            watcher::TimestampResultCode::BlockIndexOutOfBounds => {
                 Ok(TimestampResultCode::BlockIndexOutOfBounds)
             }
         }
@@ -50,23 +48,23 @@ mod tests {
     fn test_timestamp_result_code_enum_values() {
         assert_eq!(
             TimestampResultCode::TimestampFound as u32,
-            external::TimestampResultCode::TimestampFound as u32
+            watcher::TimestampResultCode::TimestampFound as u32
         );
         assert_eq!(
             TimestampResultCode::WatcherBehind as u32,
-            external::TimestampResultCode::WatcherBehind as u32
+            watcher::TimestampResultCode::WatcherBehind as u32
         );
         assert_eq!(
             TimestampResultCode::Unavailable as u32,
-            external::TimestampResultCode::Unavailable as u32
+            watcher::TimestampResultCode::Unavailable as u32
         );
         assert_eq!(
             TimestampResultCode::WatcherDatabaseError as u32,
-            external::TimestampResultCode::WatcherDatabaseError as u32
+            watcher::TimestampResultCode::WatcherDatabaseError as u32
         );
         assert_eq!(
             TimestampResultCode::BlockIndexOutOfBounds as u32,
-            external::TimestampResultCode::BlockIndexOutOfBounds as u32
+            watcher::TimestampResultCode::BlockIndexOutOfBounds as u32
         );
     }
 }
