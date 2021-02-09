647cc92f7fa1408b6e03ef531f28797baec9d2cb
James Cape <james@mobilecoin.com>
2021-02-08 17:07:03 -0800

    Fix Fog API roundtrip tests.

--- fog/api/tests/fog_types.rs
+++ fog/api/tests/fog_types.rs
@@ -1,4 +1,8 @@
-use mc_fog_api::report::{Report as ProtobufReport, ReportResponse as ProtobufReportResponse};
+use mc_attest_core::VerificationReport as ProstVerificationReport;
+use mc_fog_api::{
+    external::VerificationReport as ProtobufVerificationReport,
+    report::{Report as ProtobufReport, ReportResponse as ProtobufReportResponse},
+};
 use mc_fog_types::{Report as ProstReport, ReportResponse as ProstReportResponse};
 use prost::Message as ProstMessage;
 use protobuf::Message as ProtobufMessage;
@@ -29,41 +33,60 @@ where
     assert!(protobuf_val == protobuf_val2);
 }
 
+fn prost_report(name: &str) -> ProstVerificationReport {
+    ProstVerificationReport {
+        sig: format!("{} sig", name).as_bytes().to_vec().into(),
+        chain: Default::default(),
+        http_body: format!("{} body", name),
+    }
+}
+
 // Make some prost test cases
 fn prost_test_cases() -> Vec<ProstReport> {
     vec![
         ProstReport {
             fog_report_id: Default::default(),
-            report: b"foobar".to_vec(),
+            report: prost_report("foobar"),
             pubkey_expiry: 1000,
         },
         ProstReport {
             fog_report_id: "eap".to_string(),
-            report: b"baz".to_vec(),
+            report: prost_report("baz"),
             pubkey_expiry: 10000,
         },
         ProstReport {
             fog_report_id: "".to_string(),
-            report: b"quz".to_vec(),
+            report: prost_report("quz"),
             pubkey_expiry: 0,
         },
     ]
 }
 
+fn protobuf_report(name: &str) -> ProtobufVerificationReport {
+    let mut report = ProtobufVerificationReport::new();
+    report.set_sig(format!("{} sig", name).as_bytes().to_vec());
+    report.set_http_body(format!("{} body", name));
+    report
+}
+
 // Make some prost test cases
 fn protobuf_test_cases() -> Vec<ProtobufReport> {
     let mut rep1 = ProtobufReport::new();
-    rep1.set_report(b"asdf".to_vec());
+    rep1.set_report(protobuf_report("asdf"));
     rep1.set_pubkey_expiry(199);
 
     let mut rep2 = ProtobufReport::new();
     rep2.set_fog_report_id("non".to_string());
-    rep2.set_report(b"jkl".to_vec());
+    rep2.set_report(protobuf_report("jkl"));
     rep2.set_pubkey_expiry(11);
 
     let mut rep3 = ProtobufReport::new();
-    rep3.set_report(b";;;".to_vec());
+    rep3.set_report(protobuf_report(";;;"));
     vec![rep1, rep2, rep3]
+}
+
+fn make_chain() -> Vec<Vec<u8>> {
+    vec![b"abc".to_vec(), b"easy as".to_vec(), b"123".to_vec()]
 }
 
 #[test]
@@ -84,6 +107,8 @@ fn round_trip_protobuf_report() {
 fn round_trip_prost_report_response() {
     round_trip_prosty::<ProstReportResponse, ProtobufReportResponse>(ProstReportResponse {
         reports: prost_test_cases(),
+        chain: make_chain(),
+        signature: b"report sig".to_vec(),
     });
 }
 
@@ -91,5 +116,7 @@ fn round_trip_prost_report_response() {
 fn round_trip_protobuf_report_response() {
     let mut case = ProtobufReportResponse::new();
     case.set_reports(protobuf::RepeatedField::from_vec(protobuf_test_cases()));
+    case.set_chain(make_chain());
+    case.set_sig(b"report sig".to_vec());
     round_trip_protobuf::<ProtobufReportResponse, ProstReportResponse>(case);
 }
