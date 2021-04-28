dfa52dcec73f9378222d1e525ef8bfc95b2fe918
isis lovecruft <isis@mobilecoin.com>
2021-04-28 11:28:40 -0700

    ct-aead: Provide misuse resistance against chosen ciphertext attacks.

--- crypto/ct-aead/src/aes_impl.rs
+++ crypto/ct-aead/src/aes_impl.rs
@@ -1,12 +1,14 @@
 use crate::{CtAeadDecrypt, CtDecryptResult};
 
+use alloc::vec::Vec;
 use aes_gcm::{AesGcm, Tag, A_MAX, C_MAX};
 use block_cipher::{
     consts::U16,
     generic_array::{ArrayLength, GenericArray},
     Block, BlockCipher,
 };
-use subtle::{Choice, ConstantTimeEq};
+use subtle::{Choice, ConditionallySelectable, ConstantTimeEq};
+use zeroize::Zeroize;
 
 impl<Aes, NonceSize> CtAeadDecrypt for AesGcm<Aes, NonceSize>
 where
@@ -23,7 +25,9 @@ where
         buffer: &mut [u8],
         tag: &Tag,
     ) -> CtDecryptResult {
-        if buffer.len() as u64 > C_MAX || associated_data.len() as u64 > A_MAX {
+        let len = buffer.len();
+
+        if len as u64 > C_MAX || associated_data.len() as u64 > A_MAX {
             return CtDecryptResult(Choice::from(0));
         }
 
@@ -31,9 +35,25 @@ where
         // See: <https://github.com/RustCrypto/AEADs/issues/74>
         let mut expected_tag = self.compute_tag(associated_data, buffer);
         let mut ctr = self.init_ctr(nonce);
-        ctr.apply_keystream(&self.cipher, expected_tag.as_mut_slice());
-        ctr.apply_keystream(&self.cipher, buffer);
+        let mut ciphertext = Vec::with_capacity(len);
 
-        CtDecryptResult(expected_tag.ct_eq(&tag))
+        ciphertext.copy_from_slice(&buffer);
+
+        ctr.apply_keystream(&self.cipher, expected_tag.as_mut_slice());
+        ctr.apply_keystream(&self.cipher, &mut ciphertext);
+
+        let result = expected_tag.ct_eq(&tag);
+
+        // Conditionally copy the actual plaintext _only_ if the tag verified
+        // correctly, in order to increase misuse resistance and reduce attack
+        // surface for chosen ciphertext attacks.
+        for i in 0..len {
+            buffer[i] = u8::conditional_select(&buffer[i], &ciphertext[i], result);
+        }
+        // Unconditionally zeroize the decryption result to refrain from keeping
+        // a CCA oracle in memory.
+        ciphertext.zeroize();
+
+        CtDecryptResult(result)
     }
 }
