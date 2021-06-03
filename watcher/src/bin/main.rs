1a37cfb89342cbcd10f061d021d01da10a5ba1b2
Christian Oudard <christian.oudard@gmail.com>
2021-06-03 12:09:09 -0700

    Move watcher main loop to its own thread.

--- watcher/src/bin/main.rs
+++ watcher/src/bin/main.rs
@@ -5,13 +5,21 @@
 
 //! A standalone watcher program that can sync data from multiple sources.
 
+use displaydoc::Display;
 use mc_watcher::{
     config::WatcherConfig, verification_reports_collector::VerificationReportsCollector,
     watcher::Watcher, watcher_db::create_or_open_rw_watcher_db,
 };
 
-use mc_common::logger::{create_app_logger, log, o};
-use std::thread::sleep;
+use mc_common::logger::{Logger, create_app_logger, log, o};
+use std::{
+    io::Error as IOError,
+    thread::{sleep, Builder as ThreadBuilder, JoinHandle},
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+};
 use structopt::StructOpt;
 
 fn main() {
@@ -37,16 +45,104 @@ fn main() {
         logger.clone(),
     );
 
-    loop {
-        // For now, ignore origin block, as it does not have a signature.
-        let syncing_done = watcher
-            .sync_blocks(1, config.max_block_height)
-            .expect("Could not sync signatures");
-        if syncing_done {
-            log::info!(logger, "sync_signatures indicates we're done");
-            break;
+    let mut sync_thread = WatcherSyncThread::start(
+        watcher,
+        config,
+        logger.clone(),
+    ).expect("Failed starting watcher sync thread.");
+
+    sync_thread.join_handle.take()
+        .expect("No join handle for watcher sync thread")
+        .join()
+        .expect("Failed waiting for watcher sync thread");
+}
+
+/// Possible errors.
+#[derive(Debug, Display)]
+pub enum Error {
+    /// Thread join error
+    ThreadJoin,
+
+    /// IO error: {0}
+    IO(IOError),
+}
+
+impl From<IOError> for Error {
+    fn from(src: IOError) -> Self {
+        Self::IO(src)
+    }
+}
+
+pub struct WatcherSyncThread {
+    /// Join handle used to wait for the thread to terminate.
+    join_handle: Option<JoinHandle<()>>,
+
+    /// Stop request trigger, used to signal the thread to stop.
+    stop_requested: Arc<AtomicBool>,
+}
+
+impl WatcherSyncThread {
+    pub fn start(
+        watcher: Watcher,
+        config: WatcherConfig,
+        logger: Logger,
+    ) -> Result<Self, Error> {
+        let stop_requested = Arc::new(AtomicBool::new(false));
+        let thread_stop_requested = stop_requested.clone();
+
+        let join_handle = Some(ThreadBuilder::new()
+            .name("WatcherSync".to_string())
+            .spawn(move || {
+                Self::thread_entrypoint(watcher, config, thread_stop_requested, logger)
+            })?,
+        );
+
+        Ok(Self {
+            join_handle,
+            stop_requested,
+        })
+
+    }
+
+    pub fn stop(&mut self) -> Result<(), Error> {
+        if let Some(join_handle) = self.join_handle.take() {
+            self.stop_requested.store(true, Ordering::SeqCst);
+            join_handle.join().map_err(|_| Error::ThreadJoin)?;
         }
 
-        sleep(config.poll_interval);
+        Ok(())
+    }
+
+    fn thread_entrypoint(
+        watcher: Watcher,
+        config: WatcherConfig,
+        stop_requested: Arc<AtomicBool>,
+        logger: Logger,
+    ) {
+        log::debug!(logger, "Watcher sync thread started");
+
+        loop {
+            if stop_requested.load(Ordering::SeqCst) {
+                log::debug!(logger, "Watcher sync thread stop requested.");
+                break;
+            }
+
+            // For now, ignore origin block, as it does not have a signature.
+            let syncing_done = watcher
+                .sync_blocks(1, config.max_block_height)
+                .expect("Could not sync signatures");
+            if syncing_done {
+                log::info!(logger, "sync_signatures indicates we're done");
+                break;
+            }
+
+            sleep(config.poll_interval);
+        }
+    }
+}
+
+impl Drop for WatcherSyncThread {
+    fn drop(&mut self) {
+        let _ = self.stop();
     }
 }
