8b61588c09d64d58830a18d1f7f00dd12fa3e53b
Christian Oudard <christian.oudard@gmail.com>
2021-06-04 13:43:31 -0700

    Add a health check endpoint to the Watcher.

--- watcher/src/config.rs
+++ watcher/src/config.rs
@@ -7,8 +7,9 @@ use serde::{Deserialize, Serialize};
 use std::{fs, path::PathBuf, str::FromStr, time::Duration};
 use structopt::StructOpt;
 use url::Url;
+use mc_util_uri::WatcherUri;
 
-#[derive(Debug, StructOpt)]
+#[derive(Clone, Debug, StructOpt)]
 #[structopt(
     name = "mc-watcher",
     about = "Sync data from multiple sources, reconcile blocks, and verify signatures."
@@ -34,6 +35,10 @@ pub struct WatcherConfig {
     /// Store block data for every fetched block.
     #[structopt(long)]
     pub store_block_data: bool,
+
+    /// gRPC listening URI.
+    #[structopt(long, default_value = "insecure-watcher://0.0.0.0:3226/")]
+    pub client_listen_uri: WatcherUri,
 }
 
 impl WatcherConfig {
