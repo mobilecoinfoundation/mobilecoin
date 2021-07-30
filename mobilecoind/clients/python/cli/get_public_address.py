a7e3de17794de6c2ec89c1981ed72c602a890a2e
gscrypto <88025576+gscrypto@users.noreply.github.com>
2021-07-30 13:38:22 -0700

    Update Python client library to reflect changes to protocol. (#861)
    * Update Python client library to reflect changes to protocol moving to mnemonic entropy.
    
    * Update the rest of the python scripts to reflect API change.

--- mobilecoind/clients/python/cli/get_public_address.py
+++ mobilecoind/clients/python/cli/get_public_address.py
@@ -19,18 +19,25 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Displays public address information for a provided master key, or for a random master key if no key is provided.')
     parser.add_argument('-k', '--key', help='account master key', type=str)
+    parser.add_argument('-m', '--mnemonic', help='account key as mnemonic string', type=str)
     parser.add_argument('-s', '--subaddress', help='(optional) subaddress', nargs='?', const=mobilecoin.DEFAULT_SUBADDRESS_INDEX, type=int, default=mobilecoin.DEFAULT_SUBADDRESS_INDEX)
     args = parser.parse_args()
 
     # create a monitor and use it to calculate the public address
-    entropy_bytes = bytes.fromhex(args.key) if args.key else mobilecoind.generate_entropy().entropy
-    account_key = mobilecoind.get_account_key(entropy_bytes).account_key
+    if args.key:
+        entropy_bytes = bytes.fromhex(args.key) if args.key else mobilecoind.generate_entropy()
+        account_key = mobilecoind.get_account_key(entropy_bytes).account_key
+        entropy_display = entropy_bytes.hex()
+    else:
+        mnemonic = args.mnemonic if args.mnemonic else mobilecoind.generate_mnemonic()
+        account_key = mobilecoind.get_account_key_from_mnemonic(mnemonic)
+        entropy_display = mnemonic
     monitor_id = mobilecoind.add_monitor(account_key, first_subaddress=args.subaddress).monitor_id
     public_address = mobilecoind.get_public_address(monitor_id, subaddress_index=args.subaddress).public_address
 
     # print the public address information
     print("\n")
-    print("    {:<18}{}".format("Master Key:", entropy_bytes.hex()))
+    print("    {:<18}{}".format("Master Key:", entropy_display))
     print("    {:<18}{}".format("Subaddress Index:", args.subaddress))
     print("    {:<18}{}".format("Address Code:", public_address.b58_code))
     print("    {:<18}{}".format("Address URL:", "mob58://"+ public_address.b58_code))
