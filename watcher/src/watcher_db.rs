993873875213e8dc30c45cc684d51f3c2fc9ebc7
root <tubi70@yahoo.com>
2021-08-23 21:17:06 +0000

    review changes for watcher timestamp

--- watcher/src/watcher_db.rs
+++ watcher/src/watcher_db.rs
@@ -75,6 +75,22 @@ pub const CONFIG_DB_NAME: &str = "watcher_db:config";
 /// Keys used by the `config` database.
 pub const CONFIG_DB_KEY_TX_SOURCE_URLS: &str = "tx_source_urls";
 
+/// Poll for new data every 10 ms
+pub const POLLING_FREQUENCY: Duration = Duration::from_millis(10);
+
+/// If a database invariant is violated, e.g. we get block but not block
+/// contents, it typically will not be fixed and so we won't be able to
+/// proceed. Generally when an invariant is violated we would panic, but
+/// this code is used in services that are expensive to restart (such as the
+/// ingest enclave)
+///
+/// So instead, if this happens, we log an error, and retry in 1s.
+/// This avoids logging at > 1hz when there is this error, which would be
+/// very spammy. But the retries are unlikely to eventually lead to
+/// progress. Another strategy might be for the server to enter a
+/// "paused" state and signal for intervention.
+pub const ERROR_RETRY_FREQUENCY: Duration = Duration::from_millis(1000);
+
 /// Block Signature Data for Signature Store.
 #[derive(Message, Eq, PartialEq)]
 pub struct BlockSignatureData {
@@ -424,6 +440,59 @@ impl WatcherDB {
                     Ok((u64::MAX, TimestampResultCode::Unavailable))
                 }
             }
+        }
+    }
+
+    /// Get the timestamp from the watcher, or an error code,
+    /// using retries if the watcher fell behind
+    /// Fog ledger server db fetcher and Ingest Server worker are both affected
+    /// by this change to move get_watcher_timestamp to a shared crate in mc
+    /// watcher
+    pub fn get_watcher_timestamp(
+        &self,
+        block_index: BlockIndex,
+        watcher_timeout: Duration,
+        logger: &Logger,
+    ) -> u64 {
+        // Timer that tracks how long we have had WatcherBehind error for,
+        // if this exceeds watcher_timeout, we log a warning.
+        let mut watcher_behind_timer = Instant::now();
+        loop {
+            match self.get_block_timestamp(block_index) {
+                Ok((ts, res)) => match res {
+                    TimestampResultCode::WatcherBehind => {
+                        if watcher_behind_timer.elapsed() > watcher_timeout {
+                            log::warn!(logger, "watcher is still behind on block index = {} after waiting {} seconds, caller will be blocked", block_index, watcher_timeout.as_secs());
+                            watcher_behind_timer = Instant::now();
+                        }
+                        std::thread::sleep(POLLING_FREQUENCY);
+                    }
+                    TimestampResultCode::BlockIndexOutOfBounds => {
+                        log::warn!(logger, "block index {} was out of bounds, we should not be scanning it, we will have junk timestamps for it", block_index);
+                        return u64::MAX;
+                    }
+                    TimestampResultCode::Unavailable => {
+                        log::crit!(logger, "watcher configuration is wrong and timestamps will not be available with this configuration. caller is blocked at block index {}", block_index);
+                        std::thread::sleep(ERROR_RETRY_FREQUENCY);
+                    }
+                    TimestampResultCode::WatcherDatabaseError => {
+                        log::crit!(logger, "The watcher database has an error which prevents us from getting timestamps. caller is blocked at block index {}", block_index);
+                        std::thread::sleep(ERROR_RETRY_FREQUENCY);
+                    }
+                    TimestampResultCode::TimestampFound => {
+                        return ts;
+                    }
+                },
+                Err(err) => {
+                    log::error!(
+                            logger,
+                            "Could not obtain timestamp for block {} due to error {}, this may mean the watcher is not correctly configured. will retry",
+                            block_index,
+                            err
+                        );
+                    std::thread::sleep(ERROR_RETRY_FREQUENCY);
+                }
+            };
         }
     }
 
@@ -996,73 +1065,6 @@ impl WatcherDB {
         // Done
         db_txn.commit()?;
         Ok(())
-    }
-
-    /// Poll for new data every 10 ms
-    const POLLING_FREQUENCY: Duration = Duration::from_millis(10);
-    /// If a database invariant is violated, e.g. we get block but not block
-    /// contents, it typically will not be fixed and so we won't be able to
-    /// proceed. Generally when an invariant is violated we would panic, but
-    /// this code is used in services that are expensive to restart (such as the ingest enclave)
-    ///
-    /// So instead, if this happens, we log an error, and retry in 1s.
-    /// This avoids logging at > 1hz when there is this error, which would be
-    /// very spammy. But the retries are unlikely to eventually lead to
-    /// progress. Another strategy might be for the server to enter a
-    /// "paused" state and signal for intervention.
-    const ERROR_RETRY_FREQUENCY: Duration = Duration::from_millis(1000);
-
-    /// Get the timestamp from the watcher, or an error code,
-    /// using retries if the watcher fell behind
-    /// Fog ledger server db fetcher and Ingest Server worker are both affected
-    /// by this change to move get_watcher_timestamp to a shared crate in mc
-    /// watcher
-    pub fn get_watcher_timestamp(
-        block_index: BlockIndex,
-        watcher: &WatcherDB,
-        watcher_timeout: Duration,
-        logger: &Logger,
-    ) -> u64 {
-        // Timer that tracks how long we have had WatcherBehind error for,
-        // if this exceeds watcher_timeout, we log a warning.
-        let mut watcher_behind_timer = Instant::now();
-        loop {
-            match watcher.get_block_timestamp(block_index) {
-                Ok((ts, res)) => match res {
-                    TimestampResultCode::WatcherBehind => {
-                        if watcher_behind_timer.elapsed() > watcher_timeout {
-                            log::warn!(logger, "watcher is still behind on block index = {} after waiting {} seconds, caller will be blocked", block_index, watcher_timeout.as_secs());
-                            watcher_behind_timer = Instant::now();
-                        }
-                        std::thread::sleep(POLLING_FREQUENCY);
-                    }
-                    TimestampResultCode::BlockIndexOutOfBounds => {
-                        log::warn!(logger, "block index {} was out of bounds, we should not be scanning it, we will have junk timestamps for it", block_index);
-                        return u64::MAX;
-                    }
-                    TimestampResultCode::Unavailable => {
-                        log::crit!(logger, "watcher configuration is wrong and timestamps will not be available with this configuration. caller is blocked at block index {}", block_index);
-                        std::thread::sleep(ERROR_RETRY_FREQUENCY);
-                    }
-                    TimestampResultCode::WatcherDatabaseError => {
-                        log::crit!(logger, "The watcher database has an error which prevents us from getting timestamps. caller is blocked at block index {}", block_index);
-                        std::thread::sleep(ERROR_RETRY_FREQUENCY);
-                    }
-                    TimestampResultCode::TimestampFound => {
-                        return ts;
-                    }
-                },
-                Err(err) => {
-                    log::error!(
-                            logger,
-                            "Could not obtain timestamp for block {} due to error {}, this may mean the watcher is not correctly configured. will retry",
-                            block_index,
-                            err
-                        );
-                    std::thread::sleep(ERROR_RETRY_FREQUENCY);
-                }
-            };
-        }
     }
 }
 
