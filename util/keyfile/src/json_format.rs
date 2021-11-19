c50ed9b630bbcf1992c22c894ad133945eb28e31
Chris Beck <beck.ct@gmail.com>
2021-11-19 16:47:19 -0700

    Use slip10 accounts instead of legacy root entropy in internal testing
    This migrates the `mc-util-keyfile` crate to write private key json
    files containing slip10 root entropies instead of the legacy root entropy.
    The legacy root entropies are deprecated but they must still exist
    in some capacity because there are accounts with money on them IRL.
    
    This will simplify the effort to get uniform integration tests
    because many wallet products may never implement root entropies.
    
    The bootstrap program does not need to be touched because it only
    reads the public key files.
    
    The keygen program is touched, fog-distro is touched, the sample
    paykit is touched, the test-client is touched, slam is touched,
    but most of the changes are very localized.
    
    The main thing that would make this not work is if someone somewhere
    is storing keyfiles in a persistent way, however this is unlikely
    and whatever system that is likely needs to be reworked anyways.

--- util/keyfile/src/json_format.rs
+++ util/keyfile/src/json_format.rs
@@ -1,22 +1,95 @@
-//! We mostly try to serialize our types using protobuf,
-//! but early in the project we were using a json-based representation of
-//! account key root entropy. As part of our CD, we maintain the
-//! strategies/test_client code based on this format.
-//!
-//! It is NOT RECOMMENDED to use this for new code, please use protobuf
-//! RootIdentity This is only being used in the .json files.
+//! JSON formats for private keys together with fog data.
+//! Files formatted in this way are sufficient to derive an account key in
+//! a self-contained way without any context, which is useful for many tools.
 
-use mc_account_keys::{RootEntropy, RootIdentity};
-use serde::{Deserialize, Serialize};
+use core::convert::TryFrom;
+use mc_account_keys::{AccountKey, RootEntropy, RootIdentity};
+use mc_account_keys_slip10::{Error as AccountKeyError, Slip10Key};
+use mc_crypto_rand::{CryptoRng, RngCore};
+use mc_util_from_random::FromRandom;
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+/// JSON schema for a slip10 identity
+#[derive(Clone, PartialEq, Eq, Hash, Default, Debug, Serialize, Deserialize)]
+pub struct Slip10IdentityJson {
+    /// Slip10 key
+    #[serde(serialize_with = "as_hex", deserialize_with = "from_hex")]
+    pub slip10_key: [u8; 32],
+    /// User's fog report url, if any.
+    pub fog_report_url: String,
+    /// User's report id, if any.
+    pub fog_report_id: String,
+    /// User's fog authority subjectPublicKeyInfo bytes, if any
+    pub fog_authority_spki: Vec<u8>,
+}
+
+impl Slip10IdentityJson {
+    /// Construct an identity without fog and with a random slip10 key
+    pub fn random<T: RngCore + CryptoRng>(rng: &mut T) -> Self {
+        Self {
+            slip10_key: FromRandom::from_random(rng),
+            ..Default::default()
+        }
+    }
+
+    /// Construct an identity with fog and with a random slip10 key
+    pub fn random_with_fog<T: RngCore + CryptoRng>(
+        rng: &mut T,
+        fog_report_url: &str,
+        fog_report_id: &str,
+        fog_authority_spki: &[u8],
+    ) -> Self {
+        let mut result = Self::random(rng);
+
+        if !fog_report_url.is_empty() {
+            result.fog_report_url = fog_report_url.to_owned();
+            result.fog_report_id = fog_report_id.to_owned();
+            result.fog_authority_spki = fog_authority_spki.to_owned();
+        }
+
+        result
+    }
+}
+
+impl TryFrom<&Slip10IdentityJson> for AccountKey {
+    type Error = AccountKeyError;
+    fn try_from(src: &Slip10IdentityJson) -> Result<AccountKey, AccountKeyError> {
+        Slip10Key::from(src.slip10_key.clone()).try_into_account_key(
+            &src.fog_report_url,
+            &src.fog_report_id,
+            &src.fog_authority_spki,
+        )
+    }
+}
+
+fn as_hex<S>(key: &[u8; 32], serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    serializer.serialize_str(&hex::encode(&key[..]))
+}
+
+fn from_hex<'de, D>(deserializer: D) -> Result<[u8; 32], D::Error>
+where
+    D: Deserializer<'de>,
+{
+    use serde::de::Error;
+    String::deserialize(deserializer)
+        .and_then(|string| hex::decode(&string).map_err(|err| Error::custom(err.to_string())))
+        .and_then(|bytes| {
+            <[u8; 32] as TryFrom<&[u8]>>::try_from(&bytes)
+                .map_err(|err| Error::custom(err.to_string()))
+        })
+}
 
 /// Historical JSON schema for a root identity
 #[derive(Clone, PartialEq, Eq, Hash, Default, Debug, Serialize, Deserialize)]
 pub struct RootIdentityJson {
     /// Root entropy used to derive a user's private keys.
     pub root_entropy: [u8; 32],
-    /// User's account server, if any.
+    /// User's fog url, if any.
     pub fog_url: String,
-    /// User's report id
+    /// User's report id, if any.
     pub fog_report_id: String,
     /// User's fog authority subjectPublicKeyInfo bytes, if any
     pub fog_authority_spki: Vec<u8>,
