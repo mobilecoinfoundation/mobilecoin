b21d37abca5a6f889c61d71ff80c5d07755254a3
James Cape <james@mobilecoin.com>
2022-01-03 13:19:07 -0800

    Move IAS verifier to it's own module.

--- attest/verifier/src/lib.rs
+++ attest/verifier/src/lib.rs
@@ -53,6 +53,7 @@ cfg_if::cfg_if! {
 
 use crate::{
     avr::{Kind as AvrKind, PseVerifier},
+    ias::IasReportVerifier,
     quote::{Kind as QuoteKind, XeidVerifier},
     report_body::{
         ConfigVersionVerifier, DebugVerifier, Kind as ReportBodyKind, MiscSelectVerifier,
@@ -63,25 +64,16 @@ use crate::{
 use alloc::{
     borrow::ToOwned,
     string::{String, ToString},
-    vec,
     vec::Vec,
 };
-use core::convert::TryFrom;
 use displaydoc::Display;
-use mbedtls::{
-    alloc::{Box as MbedtlsBox, List as MbedtlsList},
-    hash::Type as HashType,
-    pk::{EcGroupId, Type as PkType},
-    x509::{Certificate, Profile},
-    Error as TlsError,
-};
+use mbedtls::{alloc::Box as MbedtlsBox, x509::Certificate, Error as TlsError};
 use mc_attest_core::{
     Attributes, Basename, ConfigId, ConfigSecurityVersion, CpuSecurityVersion, EpidGroupId,
     ExtendedProductId, FamilyId, IasNonce, MiscSelect, ProductId, Quote, QuoteSignType,
     ReportDataMask, SecurityVersion, VerificationReport, VerificationReportData, VerifyError,
 };
 use serde::{Deserialize, Serialize};
-use sha2::{digest::Digest, Sha256};
 
 /// Private macros used inside this crate.
 mod macros {
@@ -147,6 +139,12 @@ pub enum Error {
      * requirements, report contents: {0:?}
      */
     Verification(VerificationReportData),
+}
+
+impl From<VerifyError> for Error {
+    fn from(src: VerifyError) -> Error {
+        Error::Parse(src)
+    }
 }
 
 /// A builder structure used to construct a report verifier based on the
@@ -375,237 +373,16 @@ impl Verifier {
             })
             .collect::<Vec<MbedtlsBox<Certificate>>>();
 
-        // Construct the top-level verifier.
-        IasReportVerifier {
-            trust_anchors,
-            or_verifiers: self.status_verifiers.clone(),
-            and_verifiers,
-        }
-        .verify(report)
-    }
-}
-
-/// A structure which can verify a top-level report.
-#[derive(Debug)]
-struct IasReportVerifier {
-    /// A vector of trust anchor certificates to verify the report signature and
-    /// chain against.
-    trust_anchors: Vec<MbedtlsBox<Certificate>>,
-    /// A vector of report verifiers, one of which must succeed.
-    or_verifiers: Vec<StatusKind>,
-    /// A vector of report verifiers, all of which must succeed.
-    and_verifiers: Vec<AvrKind>,
-}
-
-impl From<VerifyError> for Error {
-    fn from(src: VerifyError) -> Error {
-        Error::Parse(src)
-    }
-}
-
-const MAX_CHAIN_DEPTH: usize = 5;
-
-impl IasReportVerifier {
-    /// Verify the given IAS report using this verifier object.
-    pub fn verify(&self, report: &VerificationReport) -> Result<VerificationReportData, Error> {
-        // Here's the background information for this code:
-        //
-        //  1. An X509 certificate can be signed by only one issuer.
-        //  2. mbedtls' certificates-list API demands certs in the RFC5246
-        //     order (endpoint cert first, every other cert signed the
-        //     cert preceeding it in the list).
-        //  3. I don't recall Intel's specification mentioning certificate
-        //     ordering at all (meaning they can change it w/o warning).
-        //  4. mbedtls' certificates-list API isn't actually exposed to us,
-        //     anyways.
-        //
-        // As a result, we need to find the cert which signed the data (this
-        // doubles as the signature check), then find a way back up the
-        // derived chain until we either hit a max-height limit (and fail),
-        // or top out at something that was itself signed by a trust_anchor.
-        //
-        // If we have the root CA that's in our trust_anchor list in the
-        // provided chain, then it will pass the "signed by trust_anchor"
-        // check, because all root CAs are self-signed by definition.
-        //
-        // If Intel doesn't provide the root CA in the chain, then the last
-        // entry in the derived chain will still contain the intermediate CA,
-        // which will (obviously) be signed by the root CA. Combined, these
-        // two situations mean checking that the last cert in the list was
-        // signed by a trust anchor will result in success.
-        //
-        // The third possible scenario, which is that one of the certs in the
-        // middle of the chain is in our trust_anchors list. In this case, our
-        // explicit trust of a cert makes any other issuer relationships
-        // irrelevant, including relationships with blocklisted issuers.
-        //
-        // This scenario is less likely, but would occur when someone is
-        // trying to deprecate an existing authority in favor of a new one. In
-        // this case, they sign the old CA with the new CA, so things which
-        // trust the new CA also trust the old CA "for free". When everyone
-        // has the new CA in their trust list, they start issuing certs from
-        // the new CA, and stop renewing certs from the old CA. The old CA is
-        // gradually phased out of use as the certs it issued expire, and is
-        // eventually allowed to expire itself, or revoked by the new CA.
-        //
-        // As a result, if any pubkey in the actual chain matches the pubkey
-        // of a trust anchor, then we can consider the actual chain trusted.
-        //
-        // Lastly, it's possible that Intel provides multiple complete chains
-        // terminating at different root CAs. That is, the signature is tied
-        // to pubkey X, but there are multiple leaf certificates in the
-        // provided certs for pubkey X, and each one has its own path back to
-        // a trust anchor.
-
-        if report.chain.is_empty() {
-            return Err(Error::NoChain);
-        }
-
-        // Construct a verification profile for what kind of X509 chain we
-        // will support
-        let profile = Profile::new(
-            vec![HashType::Sha256, HashType::Sha384, HashType::Sha512],
-            vec![PkType::Rsa, PkType::Ecdsa],
-            vec![
-                EcGroupId::Curve25519,
-                EcGroupId::SecP256K1,
-                EcGroupId::SecP256R1,
-                EcGroupId::SecP384R1,
-                EcGroupId::SecP521R1,
-            ],
-            2048,
-        );
-
-        // Intel uses rsa-sha256 as their signature algorithm, which means
-        // the signature is actually over the sha256 hash of the data, not
-        // the data itself. mbedtls is primitive enough that we need to do
-        // these steps ourselves.
-        let hash = Sha256::digest(report.http_body.as_bytes());
-
-        // Cloned here because it needs to be mutable because mbedtls.
-        let mut trust_anchors = self.trust_anchors.clone();
-
-        let parsed_chain: Vec<MbedtlsBox<Certificate>> = report
-            .chain
-            .iter()
-            .filter_map(|maybe_der_bytes| Certificate::from_der(maybe_der_bytes).ok())
-            .collect();
-
-        parsed_chain
-            .iter()
-            // First, find any certs for the signer pubkey
-            .filter_map(|src_cert| {
-                let mut newcert = src_cert.clone();
-                newcert
-                    .public_key_mut()
-                    .verify(HashType::Sha256, hash.as_slice(), report.sig.as_ref())
-                    .and(Ok(newcert))
-                    .ok()
-            })
-            // Then construct a set of chains, one for each signer certificate
-            .filter_map(|cert| {
-                let mut signer_chain: Vec<MbedtlsBox<Certificate>> = vec![cert];
-                'outer: loop {
-                    // Exclude any signing changes greater than our max depth
-                    if signer_chain.len() > MAX_CHAIN_DEPTH {
-                        return None;
-                    }
-
-                    for chain_cert in &parsed_chain {
-                        // The verify function takes lists of certificates, so make lists of size
-                        // one for each verification.
-                        let mut chain_cert = chain_cert.clone();
-                        let mut chain_cert_single = MbedtlsList::new();
-                        chain_cert_single.push(chain_cert.clone());
-
-                        let existing_cert = signer_chain
-                            .last_mut()
-                            .expect("Somehow our per-signer chain was empty");
-                        let mut existing_cert_single = MbedtlsList::new();
-                        existing_cert_single.push(existing_cert.clone());
-
-                        if existing_cert.public_key_mut().write_public_der_vec()
-                            != chain_cert.public_key_mut().write_public_der_vec()
-                            && Certificate::verify_with_profile(
-                                &existing_cert_single,
-                                &chain_cert_single,
-                                None,
-                                Some(&profile),
-                                None,
-                            )
-                            .is_ok()
-                        {
-                            signer_chain.push(chain_cert);
-                            continue 'outer;
-                        }
-                    }
-
-                    break;
-                }
-                Some(signer_chain)
-            })
-            // Then see if any of those chains are connected to a trust anchor
-            .find_map(|mut signer_chain| {
-                let signer_toplevel = signer_chain
-                    .last()
-                    .expect("Signer chain was somehow emptied before use.");
-                let mut signer_toplevel_single = MbedtlsList::new();
-                signer_toplevel_single.push(signer_toplevel.clone());
-
-                // First, check if the last element in the chain is signed by a trust anchor
-                for cacert in &trust_anchors {
-                    let mut cacert_single = MbedtlsList::new();
-                    cacert_single.push(cacert.clone());
-                    if Certificate::verify_with_profile(
-                        &signer_toplevel_single,
-                        &cacert_single,
-                        None,
-                        Some(&profile),
-                        None,
-                    )
-                    .is_ok()
-                    {
-                        return Some(());
-                    }
-                }
-
-                // Otherwise, check if any of the pubkeys in the chain are a trust anchor
-                for cert in &mut signer_chain {
-                    for cacert in &mut trust_anchors {
-                        if cert.public_key_mut().write_public_der_vec()
-                            == cacert.public_key_mut().write_public_der_vec()
-                        {
-                            return Some(());
-                        }
-                    }
-                }
-                None
-            })
-            .ok_or(Error::BadSignature)?;
-
-        let report_data = VerificationReportData::try_from(report)?;
-
-        if (self.and_verifiers.is_empty()
-            || self
-                .and_verifiers
-                .iter()
-                .all(|verifier| verifier.verify(&report_data)))
-            && (self.or_verifiers.is_empty()
-                || self
-                    .or_verifiers
-                    .iter()
-                    .any(|verifier| verifier.verify(&report_data)))
-        {
-            Ok(report_data)
-        } else {
-            Err(Error::Verification(report_data))
-        }
+        // Construct the top-level verifier, and verify the IAS report
+        IasReportVerifier::new(trust_anchors, self.status_verifiers.clone(), and_verifiers)
+            .verify(report)
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use alloc::vec;
     use mc_attest_core::{MrEnclave, MrSigner, VerificationSignature};
     use mc_util_encodings::FromHex;
 
