45e237b71cefc0a3993bc3a3cf0dd5a6ad28acbe
Remoun Metyas <remoun@mobilecoin.com>
2022-02-27 20:35:26 -0800

    Switch from list of versions to a set, and check whether the enclave versions are a strict subset of the top-level versions.

--- tools/dep-compare.py
+++ tools/dep-compare.py
@@ -6,9 +6,9 @@ from collections import defaultdict
 # Given a Cargo.lock path, get a dictionary mapping package names to all version numbers for that package
 def load_cargo_lock_packages(path):
     lock = toml.load(path)
-    result = defaultdict(list)
+    result = defaultdict(set)
     for package in lock['package']:
-        result[package['name']].append(package['version'])
+        result[package['name']].add(package['version'])
     return result
 
 root = load_cargo_lock_packages("Cargo.lock")
@@ -18,12 +18,12 @@ fog_ledger = load_cargo_lock_packages("fog/ledger/enclave/trusted/Cargo.lock")
 fog_view = load_cargo_lock_packages("fog/view/enclave/trusted/Cargo.lock")
 
 # Display whenever things differ from the root cargo lock
-for name, version in root.items():
-    if name in consensus and consensus[name] != version:
-        print(f"{name}: root = {version}, consensus = {consensus[name]}")
-    if name in fog_ingest and fog_ingest[name] != version:
-        print(f"{name}: root = {version}, fog_ingest = {fog_ingest[name]}")
-    if name in fog_ledger and fog_ledger[name] != version:
-        print(f"{name}: root = {version}, fog_ledger = {fog_ledger[name]}")
-    if name in fog_view and fog_view[name] != version:
-        print(f"{name}: root = {version}, fog_view = {fog_view[name]}")
+for name, versions in root.items():
+    if name in consensus and not versions.issuperset(consensus[name]):
+        print(f"{name}: root = {versions}, consensus = {consensus[name]}")
+    if name in fog_ingest and not versions.issuperset(fog_ingest[name]):
+        print(f"{name}: root = {versions}, fog_ingest = {fog_ingest[name]}")
+    if name in fog_ledger and not versions.issuperset(fog_ledger[name]):
+        print(f"{name}: root = {versions}, fog_ledger = {fog_ledger[name]}")
+    if name in fog_view and not versions.issuperset(fog_view[name]):
+        print(f"{name}: root = {versions}, fog_view = {fog_view[name]}")
