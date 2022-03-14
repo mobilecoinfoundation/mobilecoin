85aa21aaf3cb3ffef74a7d65f64e2d0f1c230648
Eran Rundstein <eran@rundste.in>
2022-03-14 15:28:53 -0700

    add back display

--- peers/src/consensus_msg.rs
+++ peers/src/consensus_msg.rs
@@ -14,9 +14,10 @@ use std::{convert::TryFrom, hash::Hash, result::Result as StdResult};
 
 /// A single value in a consensus round.
 #[derive(
-    Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize, Digestible,
+    Clone, Debug, Deserialize, Digestible, Display, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize,
 )]
 pub enum ConsensusValue {
+    /// TxHash({0})
     TxHash(TxHash),
 }
 
