57ea3a57231beeb1930bc31ab51790796c5dde79
iamalwaysuncomfortable <zaphrod.beeblebrox@gmail.com>
2022-03-15 12:06:08 -0400

    Prometheus metrics that track block & ledger height for each node
    Publishes prometheus metrics on latest block height synced & ledger
    height after every sync iteration. Motivation is to be able to set
    alerts when nodes falls behind. This addresses gh issue #1295:
    https://github.com/mobilecoinfoundation/mobilecoin/issues/1295

--- watcher/src/lib.rs
+++ watcher/src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2018-2021 The MobileCoin Foundation
+// Copyright (c) 2018-2022 The MobileCoin Foundation
 
 #![doc = include_str!("../README.md")]
 #![deny(missing_docs)]
@@ -7,6 +7,7 @@
 pub mod block_data_store;
 pub mod config;
 pub mod error;
+pub mod metrics;
 pub mod verification_reports_collector;
 pub mod watcher;
 pub mod watcher_db;
---
+++ watcher/src/metrics.rs
@@ -0,0 +1,44 @@
+// Copyright (c) 2018-2022 The MobileCoin Foundation
+
+//! Watcher metrics comparing ledger height and block height
+
+use mc_common::HashMap;
+use mc_util_metrics::{IntGauge, OpMetrics};
+use url::Url;
+
+lazy_static::lazy_static! {
+    /// Create metric object for tracking watcher
+    pub static ref COLLECTOR: OpMetrics = OpMetrics::new_and_registered("watcher");
+}
+
+/// Watcher metrics tracker used to report metrics on watcher to Prometheus
+pub struct WatcherMetrics {
+    /// Number of blocks in the ledger
+    pub ledger_block_height: IntGauge,
+}
+
+impl Default for WatcherMetrics {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl WatcherMetrics {
+    /// Initialize new metrics object
+    pub fn new() -> Self {
+        let ledger_block_height = COLLECTOR.gauge("ledger_block_height");
+        Self {
+            ledger_block_height,
+        }
+    }
+
+    /// Measure blocks synced so far for each peer
+    pub fn collect_peer_blocks_synced(&self, peer_sync_states: HashMap<Url, Option<u64>>) {
+        peer_sync_states.iter().for_each(|metric| {
+            let (url, num_blocks) = metric;
+            COLLECTOR
+                .peer_gauge("watcher_blocks_synced", url.as_str())
+                .set(num_blocks.unwrap_or(0) as i64);
+        });
+    }
+}
