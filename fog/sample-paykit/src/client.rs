7233bb77fd7c5a2f4fddc33ffdb099c31aaf5f0e
Chris Beck <beck.ct@gmail.com>
2022-03-18 11:23:39 -0600

    fix test build

--- fog/sample-paykit/src/client.rs
+++ fog/sample-paykit/src/client.rs
@@ -722,8 +722,10 @@ mod test_build_transaction_helper {
     use mc_fog_types::view::{FogTxOut, FogTxOutMetadata, TxOutRecord};
     use mc_transaction_core::{
         constants::MILLIMOB_TO_PICOMOB,
+        one_time_keys::recover_public_subaddress_spend_key,
+        tokens::Mob,
         tx::{TxOut, TxOutMembershipProof},
-        Amount,
+        Amount, Token,
     };
     use mc_transaction_core_test_utils::get_outputs;
     use rand::{rngs::StdRng, SeedableRng};
