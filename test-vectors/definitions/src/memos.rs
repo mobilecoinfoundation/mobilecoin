2433c25fb64c04254a388f863b06de7fec795acc
Sam Dealy <sam.dealy@gmail.com>
2022-03-18 14:13:52 -0700

    Generate correct sender with payment request id memos

--- test-vectors/definitions/src/memos.rs
+++ test-vectors/definitions/src/memos.rs
@@ -97,3 +97,27 @@ impl TestVector for IncorrectEncryptedDestinationMemoData {
     const FILE_NAME: &'static str = "incorrect_encrypted_destination_memos";
     const MODULE_SUBDIR: &'static str = "memos";
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+/// Contains data associated with an encrypted destination memo that has the 
+/// correct sender and recipient data.
+pub struct CorrectEncryptedSenderWithPaymentRequestIdMemoData {
+    /// The transaction sender's public address bytes encoded as hex. This user
+    /// wrote the sender memo.
+    pub sender_public_address: String,
+
+    /// The transaction recipient's view public key bytes encoded as hex. This
+    /// user received the transaction with the sender memo.
+    pub recipient_view_public_key: String,
+
+    /// The payment request ID included in the memo.
+    pub payment_request_id: u64,
+
+    /// The encrypted sender memo bytes encoded as hex.
+    pub encrypted_sender_with_payment_request_id_memo: String,
+}
+
+impl TestVector for CorrectEncryptedSenderWithPaymentRequestIdMemoData {
+    const FILE_NAME: &'static str = "correct_encrypted_sender_with_payment_request_id_memos";
+    const MODULE_SUBDIR: &'static str = "memos";
+}
