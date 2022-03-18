c7b3dfa5406dc656b25339f22c305b02624b7b36
William J <william@mobilecoin.com>
2022-03-18 02:56:03 -0400

    Cleanup lint file

--- tools/lint.sh
+++ tools/lint.sh
@@ -17,16 +17,7 @@ export CONSENSUS_ENCLAVE_CSS=$(pwd)/consensus-enclave.css
 
 export PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-. $PARENT_PATH/download_sigstruct.sh
-
-export CONSENSUS_ENCLAVE_CSS=$(pwd)/consensus-enclave.css
-export INGEST_ENCLAVE_CSS=$(pwd)/ingest-enclave.css
-export LEDGER_ENCLAVE_CSS=$(pwd)/ledger-enclave.css
-export VIEW_ENCLAVE_CSS=$(pwd)/view-enclave.css
-
-echo "view enclave css: $VIEW_ENCLAVE_CSS"
-# Move enclave into its own crate
-
+source $PARENT_PATH/download_sigstruct.sh
 
 for toml in $(grep --exclude-dir cargo --exclude-dir rust-mbedtls --include=Cargo.toml -r . -e '\[workspace\]' | cut -d: -f1); do
   pushd $(dirname $toml) >/dev/null
