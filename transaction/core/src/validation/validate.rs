cd0dec79a93da0d990a540bf269fe4cce94e17cb
Eugen Rata <eugen.rata@gmail.com>
2022-03-29 15:39:16 -0500

    added a mechanism to generate unsorted outputs to be able to execute … (#1705)
    * added a mechanism to generate unsorted outputs to be able to execute the global transaction validate method
    
    * fixed linting error
    
    * removed needless return
    
    * fixed teh logic to detect whether the block requires sorted transaction outputs
    
    * changed to use traits instead of function pointers
    
    * linting issue
    
    * linting issue #2
    
    * using static method instead of instance one for the Ordering trait
    
    Co-authored-by: Eugene Rata <no-email@no-email.com>

--- transaction/core/src/validation/validate.rs
+++ transaction/core/src/validation/validate.rs
@@ -510,9 +510,10 @@ mod tests {
     use mc_crypto_keys::{CompressedRistrettoPublic, ReprBytes};
     use mc_ledger_db::{Ledger, LedgerDB};
     use mc_transaction_core_test_utils::{
-        create_ledger, create_transaction, create_transaction_with_amount, initialize_ledger,
-        AccountKey, INITIALIZE_LEDGER_AMOUNT,
+        create_ledger, create_transaction, create_transaction_with_amount_and_comparer,
+        initialize_ledger, AccountKey, InverseTxOutputsOrdering, INITIALIZE_LEDGER_AMOUNT,
     };
+    use mc_transaction_std::{DefaultTxOutputsOrdering, TxOutputsOrdering};
     use rand::{rngs::StdRng, SeedableRng};
     use serde::{de::DeserializeOwned, ser::Serialize};
 
@@ -567,6 +568,18 @@ mod tests {
         amount: u64,
         fee: u64,
     ) -> (Tx, LedgerDB) {
+        create_test_tx_with_amount_and_comparer::<DefaultTxOutputsOrdering>(
+            block_version,
+            amount,
+            fee,
+        )
+    }
+
+    fn create_test_tx_with_amount_and_comparer<O: TxOutputsOrdering>(
+        block_version: BlockVersion,
+        amount: u64,
+        fee: u64,
+    ) -> (Tx, LedgerDB) {
         let mut rng: StdRng = SeedableRng::from_seed([1u8; 32]);
         let sender = AccountKey::random(&mut rng);
         let mut ledger = create_ledger();
@@ -584,7 +597,7 @@ mod tests {
         let tx_out = block_contents.outputs[0].clone();
 
         let recipient = AccountKey::random(&mut rng);
-        let tx = create_transaction_with_amount(
+        let tx = create_transaction_with_amount_and_comparer::<_, _, O>(
             adapt_hack(&block_version),
             &mut ledger,
             &tx_out,
@@ -1277,6 +1290,45 @@ mod tests {
                 validate_tombstone(current_block_index, tombstone_block_index),
                 Err(TransactionValidationError::TombstoneBlockTooFar)
             );
+        }
+    }
+
+    #[test]
+    fn test_global_validate_for_blocks_with_sorted_outputs() {
+        let mut rng: StdRng = SeedableRng::from_seed([1u8; 32]);
+        let fee = Mob::MINIMUM_FEE + 1;
+        for block_version in BlockVersion::iterator() {
+            // for block version < 3 it doesn't matter
+            // for >= 3 it shall return an error about unsorted outputs
+            let (tx, _ledger) = create_test_tx_with_amount_and_comparer::<InverseTxOutputsOrdering>(
+                block_version,
+                INITIALIZE_LEDGER_AMOUNT - fee,
+                fee,
+            );
+
+            let highest_indices = tx.get_membership_proof_highest_indices();
+            let root_proofs: Vec<TxOutMembershipProof> = adapt_hack(
+                &_ledger
+                    .get_tx_out_proof_of_memberships(&highest_indices)
+                    .expect("failed getting proofs"),
+            );
+
+            let result = validate(
+                &tx,
+                tx.prefix.tombstone_block - 1,
+                block_version,
+                &root_proofs,
+                0,
+                &mut rng,
+            );
+
+            assert_eq!(
+                result,
+                match block_version.validate_transaction_outputs_are_sorted() {
+                    true => Err(TransactionValidationError::UnsortedOutputs),
+                    false => Ok(()),
+                }
+            )
         }
     }
 }
