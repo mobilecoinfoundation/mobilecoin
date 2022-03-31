71305edef85c2cff5672097346bdd4f60530a852
Eran Rundstein <eran@rundste.in>
2022-03-31 12:22:42 -0700

    Update mint-auditor/api/build.rs
    Co-authored-by: Remoun Metyas <remoun@mobilecoin.com>

--- mint-auditor/api/build.rs
+++ mint-auditor/api/build.rs
@@ -12,10 +12,8 @@ fn main() {
         .expect("Invalid UTF-8 in proto dir");
     cargo_emit::pair!("PROTOS_PATH", "{}", proto_str);
 
-    let all_proto_dirs = vec![proto_str];
-
     mc_util_build_grpc::compile_protos_and_generate_mod_rs(
-        all_proto_dirs.as_slice(),
+        &[proto_str],
         &["mint_auditor.proto"],
     );
 }
