437956ab2d66d51c2add0a8faf1f2149b6632cb7
Eran Rundstein <eran@rundste.in>
2022-04-01 18:53:02 -0700

    add prometheus gauge

--- mint-auditor/src/counters.rs
+++ mint-auditor/src/counters.rs
@@ -13,4 +13,7 @@ lazy_static::lazy_static! {
 
     /// Number of burns exceeding calculated balance.
     pub static ref NUM_BURNS_EXCEEDING_BALANCE: IntGauge = OP_COUNTERS.gauge("num_burns_exceeding_balance");
+
+    /// Number of MintTxs without a matching MintConfig.
+    pub static ref NUM_MINT_TXS_WITHOUT_MATCHING_MINT_CONFIG = OP_COUNTERS.gauge("num_mint_txs_without_matching_mint_config");
 }
