ad2ea78c4d573c89fef2bc3b2775827393ca5b0f
Eran Rundstein <eran@rundste.in>
2022-04-04 14:29:26 -0700

    lint

--- transaction/core/test-utils/src/lib.rs
+++ transaction/core/test-utils/src/lib.rs
@@ -147,7 +147,7 @@ pub fn create_transaction_with_amount_and_comparer<
     tombstone_block: BlockIndex,
     rng: &mut R,
 ) -> Tx {
-    let (sender_amount, _) = tx_out.view_key_match(&sender.view_private_key()).unwrap();
+    let (sender_amount, _) = tx_out.view_key_match(sender.view_private_key()).unwrap();
 
     let mut transaction_builder = TransactionBuilder::new(
         block_version,
