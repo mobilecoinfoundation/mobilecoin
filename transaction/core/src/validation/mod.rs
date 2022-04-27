8fe5d98e4bb4a79cafa29d9f83e556ead8e16ef5
Chris Beck <beck.ct@gmail.com>
2022-04-27 16:38:40 -0600

    move validation tests to be an integration test, remove adapt_hack
    This works because, when cargo compiles in-line unit tests, they are
    compiled into the module of the code under test, but when cargo
    compiles integration tests (in tests directory), that is considered
    a separate module (effectively a separate crate, just without its
    own cargo toml).
    
    This fixes the adapt hack issue because now we no longer need to
    compile the `mc-transaction-core` crate in test mode, which also
    references the `mc-transaction-core` crate which `mc-ledger-db`
    depends on. The types in the test code always refer to the types
    in the same version of `mc-transaction-core` that `mc-ledger-db`
    is using.

--- transaction/core/src/validation/mod.rs
+++ transaction/core/src/validation/mod.rs
@@ -7,5 +7,13 @@ mod validate;
 
 pub use self::{
     error::{TransactionValidationError, TransactionValidationResult},
-    validate::{validate, validate_signature, validate_tombstone, validate_tx_out},
+    validate::{
+        validate, validate_inputs_are_sorted, validate_key_images_are_unique,
+        validate_masked_token_id_exists, validate_membership_proofs, validate_memo_exists,
+        validate_no_masked_token_id_exists, validate_no_memo_exists, validate_number_of_inputs,
+        validate_number_of_outputs, validate_outputs_are_sorted,
+        validate_outputs_public_keys_are_unique, validate_ring_elements_are_sorted,
+        validate_ring_elements_are_unique, validate_ring_sizes, validate_signature,
+        validate_tombstone, validate_transaction_fee, validate_tx_out,
+    },
 };
