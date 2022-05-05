a59be4c402b2b71aeb90ac4c4bef29d833501537
Remoun Metyas <remoun@mobilecoin.com>
2022-05-05 16:37:27 -0700

    Clean up mc-util-test-helper (#1925)
    * Use rand::distributions::Alphanumeric in random_string()
    * Use lazy_static to call get_seeds only once

--- util/test-helper/src/lib.rs
+++ util/test-helper/src/lib.rs
@@ -5,26 +5,24 @@
 #[macro_use]
 extern crate lazy_static;
 
-pub use rand_core::{CryptoRng, RngCore, SeedableRng};
-use rand_hc::Hc128Rng;
-
-// re-export AccountKey and PublicAddress to save an import elsewhere
-pub use mc_account_keys::{AccountKey, PublicAddress};
-
 pub mod known_accounts;
 
-type Seed = <RngType as SeedableRng>::Seed;
+pub use rand_core::{CryptoRng, RngCore, SeedableRng};
+// re-export AccountKey and PublicAddress to save an import elsewhere
+pub use mc_account_keys::{AccountKey, PublicAddress};
 
 const NUM_TRIALS: usize = 3;
 
 // Sometimes you need to have the type in scope to call trait functions
+use rand_hc::Hc128Rng;
 pub type RngType = Hc128Rng;
+type Seed = <RngType as SeedableRng>::Seed;
 
 // Helper for running a unit test that requires randomness, but doing it
 // seeded and deterministically
 pub fn run_with_several_seeds<F: FnMut(RngType)>(mut f: F) {
-    for seed in &get_seeds() {
-        f(RngType::from_seed(*seed));
+    for seed in *SEEDS {
+        f(RngType::from_seed(seed));
     }
 }
 
@@ -32,7 +30,10 @@ pub fn run_with_one_seed<F: FnOnce(RngType)>(f: F) {
     f(get_seeded_rng());
 }
 
-// TODO(chris): Can we store the result of this function in a const somehow?
+lazy_static! {
+    static ref SEEDS: [Seed; NUM_TRIALS] = get_seeds();
+}
+
 fn get_seeds() -> [Seed; NUM_TRIALS] {
     let mut rng = get_seeded_rng();
 
@@ -48,16 +49,9 @@ pub fn get_seeded_rng() -> RngType {
 }
 
 pub fn random_str(rng: &mut RngType, len: usize) -> String {
-    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
-                            abcdefghijklmnopqrstuvwxyz\
-                            0123456789";
-
-    let output: String = (0..len)
-        .map(|_| {
-            let idx = (rng.next_u64() % CHARSET.len() as u64) as usize;
-            char::from(CHARSET[idx])
-        })
-        .collect();
-
-    output
+    use rand::{distributions::Alphanumeric, Rng};
+    rng.sample_iter(&Alphanumeric)
+        .take(len)
+        .map(char::from)
+        .collect()
 }
