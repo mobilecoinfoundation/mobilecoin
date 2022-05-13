d88c10966f80fca5c42e1cb831aa7e068db98595
Bernie Dolan <bernie@mobilecoin.com>
2022-05-13 16:15:18 -0700

    Fixing tests

--- mobilecoind/src/payments.rs
+++ mobilecoind/src/payments.rs
@@ -971,12 +971,12 @@ impl<T: BlockchainConnection + UserTxConnection + 'static, FPR: FogPubkeyResolve
         let mut tx_out_to_outlay_index = HashMap::default();
         let mut outlay_confirmation_numbers = Vec::default();
         for (i, outlay) in destinations.iter().enumerate() {
-            let TxOutContext {tx_out, confirmation_number, ..} = tx_builder
+            let TxOutContext {tx_out, confirmation, ..} = tx_builder
                 .add_output(outlay.value, &outlay.receiver, rng)
                 .map_err(|err| Error::TxBuild(format!("failed adding output: {}", err)))?;
 
             tx_out_to_outlay_index.insert(tx_out, i);
-            outlay_confirmation_numbers.push(confirmation_number);
+            outlay_confirmation_numbers.push(confirmation);
 
             total_value += outlay.value;
         }
