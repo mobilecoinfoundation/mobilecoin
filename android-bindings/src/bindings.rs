52d39bb0d27c8b9e9fda4964020b97282231c4e1
Bernie Dolan <bernie@mobilecoin.com>
2022-05-27 13:50:51 -0700

    Lint

--- android-bindings/src/bindings.rs
+++ android-bindings/src/bindings.rs
@@ -46,9 +46,8 @@ use mc_transaction_core::{
         create_shared_secret, recover_onetime_private_key, recover_public_subaddress_spend_key,
     },
     ring_signature::KeyImage,
-    TokenId,
     tx::{Tx, TxOut, TxOutConfirmationNumber, TxOutMembershipProof},
-    Amount, BlockVersion, CompressedCommitment, MaskedAmount,
+    Amount, BlockVersion, CompressedCommitment, MaskedAmount, TokenId,
 };
 
 use mc_transaction_std::{
@@ -341,9 +340,12 @@ pub unsafe extern "C" fn Java_com_mobilecoin_lib_MaskedAmount_init_1jni_1with_1s
     jni_ffi_call(&env, |env| {
         let tx_out_shared_secret: MutexGuard<RistrettoPublic> =
             env.get_rust_field(tx_out_shared_secret, RUST_OBJ_FIELD)?;
-            let masked_token_id_bytes = env.convert_byte_array(masked_token_id)?;
-        let masked_amount =
-            MaskedAmount::reconstruct(masked_value as u64, &masked_token_id_bytes, &tx_out_shared_secret)?;
+        let masked_token_id_bytes = env.convert_byte_array(masked_token_id)?;
+        let masked_amount = MaskedAmount::reconstruct(
+            masked_value as u64,
+            &masked_token_id_bytes,
+            &tx_out_shared_secret,
+        )?;
 
         Ok(env.set_rust_field(obj, RUST_OBJ_FIELD, masked_amount)?)
     })
