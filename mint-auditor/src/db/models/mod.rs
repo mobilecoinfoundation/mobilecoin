1dadf815abb26111bd3ab90d00d6701e94dd8bcf
Eran Rundstein <eran@rundste.in>
2022-06-15 13:07:01 -0700

    fmt

--- mint-auditor/src/db/models/mod.rs
+++ mint-auditor/src/db/models/mod.rs
@@ -4,8 +4,4 @@ mod block_audit_data;
 mod block_balance;
 mod counters;
 
-pub use self::{
-    block_audit_data::BlockAuditData,
-    block_balance::BlockBalance,
-    counters::Counters,
-};
+pub use self::{block_audit_data::BlockAuditData, block_balance::BlockBalance, counters::Counters};
