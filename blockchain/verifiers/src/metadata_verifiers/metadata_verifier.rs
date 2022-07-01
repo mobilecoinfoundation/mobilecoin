bd825f2679733ad9f2638f7e037eaeb3b7a667fc
iamalwaysuncomfortable <zaphrod.beeblebrox@gmail.com>
2022-07-01 12:54:48 -0400

    Documentation overhaul

--- blockchain/verifiers/src/metadata_verifiers/metadata_verifier.rs
+++ blockchain/verifiers/src/metadata_verifiers/metadata_verifier.rs
@@ -9,7 +9,7 @@ use crate::{
 use mc_blockchain_types::{BlockData, BlockIndex, BlockMetadata};
 use mc_common::ResponderId;
 
-/// A [BlockMetadata] verifier that validates metadata signatures, signing
+/// A [BlockMetadata] verifier that verifies metadata signatures, signing
 /// keys, and AVRs.
 pub struct MetadataVerifier {
     /// Message signing key & metadata signature verifier
@@ -35,11 +35,13 @@ impl MetadataVerifier {
     ///
     /// # Args:
     ///
-    /// * `block_data` - [BlockData] for the block we want to validate AVR for
-    /// * `responder_id` - Optional [ResponderId] to validate AVR for. If a
-    /// [ResponderId] is provided, it will attempt to verify the block signing
-    /// key matched the key within the [VerificationReport] for the given
-    /// ResponderId for historical blocks.
+    /// * `block_data` - [BlockData] for the block we want to verify AVRs,
+    /// signing keys, and metadata signatures for.
+    /// * `responder_id` - Optional [ResponderId] to verify historical AVRs
+    /// against. Each [AvrVerificationRecord] includes a BlockRange and
+    /// ResponderId an AVR was valid for. If this arg is provided, the function
+    /// additionally ensures that the ResponderId matches the ResponderId in
+    /// the record.
     pub fn verify(
         &self,
         block_data: &BlockData,
@@ -47,23 +49,25 @@ impl MetadataVerifier {
     ) -> Result<(), VerificationError> {
         let block_index = block_data.block().index;
 
-        self.validate_block_signing_key_and_avr(block_data, responder_id)?;
+        self.verify_block_signing_key_and_avr(block_data, responder_id)?;
         block_data.metadata().map_or(Ok(()), |metadata| {
-            self.validate_block_signing_key_and_metadata_signature(metadata, block_index)
+            self.verify_block_signing_key_and_metadata_signature(metadata, block_index)
         })
     }
 
-    /// Validate the Block Signing Key and Attestation Verification Report
+    /// Verify the Block Signing Key and Attestation Verification Report
     /// for a block
     ///
     /// # Args:
     ///
-    /// * `block_data` - [BlockData] for the block we want to validate AVR for
-    /// * `responder_id` - Optional [ResponderId] to validate AVR for. If a
-    /// [ResponderId] is provided, it will attempt to verify the block signing
-    /// key matched the key within the [VerificationReport] for the given
-    /// ResponderId for historical blocks.
-    pub fn validate_block_signing_key_and_avr(
+    /// * `block_data` - [BlockData] for the block we want to verify an AVR
+    /// and block signing key for.
+    /// * `responder_id` - Optional [ResponderId] to verify historical AVRs
+    /// against. Each [AvrVerificationRecord] includes a BlockRange and
+    /// ResponderId an AVR was valid for. If this arg is provided, the function
+    /// additionally ensures that the ResponderId matches the ResponderId in
+    /// the record.
+    pub fn verify_block_signing_key_and_avr(
         &self,
         block_data: &BlockData,
         responder_id: &Option<ResponderId>,
@@ -72,11 +76,13 @@ impl MetadataVerifier {
             .verify_block_signing_key_and_avr(block_data, responder_id)
     }
 
-    /// Validate that the given metadata is valid at the given block index.
+    /// Verify that the given metadata is valid at the given block index.
     ///
-    /// `metadata` - [BlockMetadata] to validate
-    /// `block_index` - [BlockIndex] to validate metadata at
-    pub fn validate_block_signing_key_and_metadata_signature(
+    /// # Args:
+    ///
+    /// `metadata` - [BlockMetadata] to verify
+    /// `block_index` - [BlockIndex] to verify metadata at
+    pub fn verify_block_signing_key_and_metadata_signature(
         &self,
         metadata: &BlockMetadata,
         block_index: BlockIndex,
@@ -95,75 +101,75 @@ mod tests {
     use mc_crypto_keys::Ed25519Signature;
 
     #[test]
-    fn happy_path() {
+    fn test_metadata_verification_of_valid_signing_key_and_metadata_signature() {
         let verifier = make_verifier();
         let metadata = make_metadata(1);
         assert_eq!(
-            verifier.validate_block_signing_key_and_metadata_signature(&metadata, 0),
+            verifier.verify_block_signing_key_and_metadata_signature(&metadata, 0),
             Ok(())
         );
         assert_eq!(
-            verifier.validate_block_signing_key_and_metadata_signature(&metadata, 10),
+            verifier.verify_block_signing_key_and_metadata_signature(&metadata, 10),
             Ok(())
         );
         assert_eq!(
-            verifier.validate_block_signing_key_and_metadata_signature(&metadata, 100),
+            verifier.verify_block_signing_key_and_metadata_signature(&metadata, 100),
             Ok(())
         );
         assert_eq!(
-            verifier.validate_block_signing_key_and_metadata_signature(&metadata, 1000),
+            verifier.verify_block_signing_key_and_metadata_signature(&metadata, 1000),
             Ok(())
         );
     }
 
     #[test]
-    fn unrecognized_key() {
+    fn test_metadata_verification_with_unrecognized_key() {
         let verifier = make_verifier();
         let metadata = make_metadata(10);
         assert_eq!(
-            verifier.validate_block_signing_key_and_metadata_signature(&metadata, 0),
+            verifier.verify_block_signing_key_and_metadata_signature(&metadata, 0),
             Err(VerificationError::UnknownMessageSigningKey)
         );
     }
 
     #[test]
-    fn expired_key() {
+    fn test_metadata_verification_with_expired_key() {
         let verifier = make_verifier();
         let metadata = make_metadata(2);
         assert_eq!(
-            verifier.validate_block_signing_key_and_metadata_signature(&metadata, 11),
+            verifier.verify_block_signing_key_and_metadata_signature(&metadata, 11),
             Err(VerificationError::InvalidMessageSigningKey)
         );
         assert_eq!(
-            verifier.validate_block_signing_key_and_metadata_signature(&metadata, 10),
+            verifier.verify_block_signing_key_and_metadata_signature(&metadata, 10),
             Ok(())
         );
     }
 
     #[test]
-    fn premature_key() {
+    fn test_metadata_verification_with_premature_key() {
         let verifier = make_verifier();
         let metadata = make_metadata(3);
         assert_eq!(
-            verifier.validate_block_signing_key_and_metadata_signature(&metadata, 1),
+            verifier.verify_block_signing_key_and_metadata_signature(&metadata, 1),
             Err(VerificationError::InvalidMessageSigningKey)
         );
         assert_eq!(
-            verifier.validate_block_signing_key_and_metadata_signature(&metadata, 9),
+            verifier.verify_block_signing_key_and_metadata_signature(&metadata, 9),
             Err(VerificationError::InvalidMessageSigningKey)
         );
         assert_eq!(
-            verifier.validate_block_signing_key_and_metadata_signature(&metadata, 10),
+            verifier.verify_block_signing_key_and_metadata_signature(&metadata, 10),
             Ok(())
         );
     }
 
     #[test]
-    fn bad_metadata_signature() {
+    fn test_metadata_verification_with_bad_metadata_signature() {
         let verifier = make_verifier();
         let ok_metadata = make_metadata(1);
         assert_eq!(
-            verifier.validate_block_signing_key_and_metadata_signature(&ok_metadata, 0),
+            verifier.verify_block_signing_key_and_metadata_signature(&ok_metadata, 0),
             Ok(())
         );
 
@@ -176,7 +182,7 @@ mod tests {
         );
 
         assert!(matches!(
-            verifier.validate_block_signing_key_and_metadata_signature(&metadata, 0),
+            verifier.verify_block_signing_key_and_metadata_signature(&metadata, 0),
             Err(VerificationError::BlockMetadataSignature(_))
         ));
     }
