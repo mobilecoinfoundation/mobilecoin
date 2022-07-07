1921fbe83ecdd5e9c689c87228c15d7ee4af06ac
Remoun Metyas <remoun@mobilecoin.com>
2022-07-07 13:44:23 -0700

    Add a script to push all the protos to buf.build

---
+++ tools/buf_push.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Copyright (c) 2018-2022 The MobileCoin Foundation
+
+set -e  # exit on error
+
+# NB: protos in mobilecoind and fog depend on other dirs in this list,
+# so be mindful of dependency order; a sorted list happens to work.
+DIRS=(
+    api/proto
+    attest/api/proto
+    consensus/api/proto
+    fog/api/proto
+    fog/report/api/proto
+    mint-auditor/api/proto
+    mobilecoind/api/proto
+    util/grpc/proto
+)
+
+for dir in ${DIRS[@]}
+do
+    pushd $dir
+    buf mod update && buf build && buf push
+    popd > /dev/null
+done
