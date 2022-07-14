91a2bf70fcf226d3e613bcf10cf1d12637239a0b
Pouneh Aghababazadeh <paghababazadeh@gmail.com>
2022-07-14 08:26:44 -0700

    Add tests for AVR history (de)serialization
    * Fix deserialization of the AVR history struct.
    * Add tests and utilities
    
    Co-authored-by: Mike Turner <zaphrod.beeblebrox@gmail.com>

--- blockchain/validators/src/metadata/avr/config.rs
+++ blockchain/validators/src/metadata/avr/config.rs
@@ -15,7 +15,7 @@ use std::{fs, option::Option, path::Path};
 #[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
 pub struct AvrHistoryConfig {
     // List of AvrHistoryRecord objects sorted by ResponderId and block range
-    node: Vec<AvrHistoryRecord>,
+    pub node: Vec<AvrHistoryRecord>,
 }
 
 /// Stores a historical AVR record (or lack thereof) for a given
@@ -34,6 +34,7 @@ pub struct AvrHistoryRecord {
 
     /// AVR Report (or lack thereof) for the node & block ranges
     #[serde_as(as = "Option<VerificationReportShadow>")]
+    #[serde(default)]
     pub avr: Option<VerificationReport>,
 }
 
@@ -96,10 +97,58 @@ impl<'de> DeserializeAs<'de, VerificationReport> for VerificationReportShadow {
     where
         D: Deserializer<'de>,
     {
-        #[derive(Deserialize)]
-        struct Helper(#[serde(with = "VerificationReportShadow")] VerificationReport);
-        let helper = Helper::deserialize(deserializer)?;
-        let Helper(v) = helper;
-        Ok(v)
+        VerificationReportShadow::deserialize(deserializer)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+    use serde_json;
+    use super::*;
+    use crate::test_utils::{
+        get_avr_history_config, SAMPLE_AVR_HISTORY_JSON, get_ias_reports,
+        SAMPLE_AVR_HISTORY_TOML,
+    };
+    use tempfile::TempDir;
+
+    #[test]
+    fn test_avr_history_serialization_roundtrip_works() {
+        // Get a manually constructed AVR history config to act as control data
+
+        let control_avr_history = get_avr_history_config();
+
+        // Serialize the config to JSON & TOML
+        let toml_str = toml::to_string_pretty(&control_avr_history).unwrap();
+        let json_str = serde_json::to_string_pretty(&control_avr_history).unwrap();
+
+        let history_from_toml: AvrHistoryConfig = toml::from_str(toml_str.as_str()).unwrap();
+        let history_from_json: AvrHistoryConfig = serde_json::from_str(json_str.as_str()).unwrap();
+
+        // Assert that deserialization from JSON and TOML is the same as the original
+        // config
+        assert_eq!(control_avr_history, history_from_toml);
+        assert_eq!(control_avr_history, history_from_json);
+    }
+
+    #[test]
+    fn test_avr_history_load_from_disk() {
+        // Get a manually constructed AVR history config to act as control data
+        let control_avr_history = get_avr_history_config();
+
+        // Write JSON and TOML to disk
+        let temp = TempDir::new().unwrap();
+        let path_json = temp.path().join("avr-history.json");
+        let path_toml = temp.path().join("avr-history.toml");
+        fs::write(&path_json, SAMPLE_AVR_HISTORY_JSON).unwrap();
+        fs::write(&path_toml, SAMPLE_AVR_HISTORY_TOML).unwrap();
+
+        // Load the config from disk
+        let avr_history_from_json = AvrHistoryConfig::try_from_file(path_json).unwrap();
+        let avr_history_from_toml = AvrHistoryConfig::try_from_file(path_toml).unwrap();
+
+        // Check that the avr histories loaded from disk are the same as the control
+        assert_eq!(control_avr_history, avr_history_from_json);
+        assert_eq!(control_avr_history, avr_history_from_toml);
     }
 }
