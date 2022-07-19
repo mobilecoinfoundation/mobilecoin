cecab07a5a7c878875df170566e2050162f00fe8
Sam Dealy <sam.dealy@gmail.com>
2022-07-19 10:55:33 -0700

    Add readiness API to ShardingStrategy

--- fog/view/server/src/sharding_strategy.rs
+++ fog/view/server/src/sharding_strategy.rs
@@ -6,7 +6,7 @@
 //! TxOuts across Fog View Store instances.
 
 use mc_blockchain_types::BlockIndex;
-use mc_fog_types::common::BlockRange;
+use mc_fog_types::{common::BlockRange, BlockCount};
 use serde::Serialize;
 use std::str::FromStr;
 
@@ -14,6 +14,13 @@ use std::str::FromStr;
 pub trait ShardingStrategy {
     /// Returns true if the Fog View Store should process this block.
     fn should_process_block(&self, block_index: BlockIndex) -> bool;
+
+    /// Returns true if the Fog View Store is ready to serve TxOuts to the
+    /// client.
+    ///
+    /// Different sharding strategies might be ready to serve TxOuts when
+    /// different conditions have been met.
+    fn is_ready_to_serve_tx_outs(&self, processed_block_count: BlockCount) -> bool;
 }
 
 /// Determines whether or not to process a block's TxOuts based on the "epoch"
@@ -33,6 +40,10 @@ impl ShardingStrategy for EpochShardingStrategy {
     fn should_process_block(&self, block_index: BlockIndex) -> bool {
         self.epoch_block_range.contains(block_index)
     }
+
+    fn is_ready_to_serve_tx_outs(&self, processed_block_count: BlockCount) -> bool {
+        self.have_enough_blocks_been_processed(processed_block_count)
+    }
 }
 
 impl Default for EpochShardingStrategy {
@@ -47,6 +58,22 @@ impl EpochShardingStrategy {
     #[allow(dead_code)]
     pub fn new(epoch_block_range: BlockRange) -> Self {
         Self { epoch_block_range }
+    }
+
+    fn have_enough_blocks_been_processed(&self, processed_block_count: BlockCount) -> bool {
+        if self.is_first_epoch() {
+            return true;
+        }
+
+        let epoch_block_range_length =
+            self.epoch_block_range.end_block - self.epoch_block_range.start_block;
+        let minimum_processed_block_count = epoch_block_range_length / 2;
+
+        u64::from(processed_block_count) >= minimum_processed_block_count
+    }
+
+    fn is_first_epoch(&self) -> bool {
+        self.epoch_block_range.start_block == 0
     }
 }
 
@@ -139,5 +166,81 @@ mod epoch_sharding_strategy_tests {
             epoch_sharding_strategy.should_process_block(END_BLOCK_EXCLUSIVE + 1);
 
         assert!(!should_process_block)
+    }
+
+    #[test]
+    fn is_ready_to_serve_tx_outs_first_epoch_processed_block_count_is_zero_less_than_50_percent_processed_returns_true(
+    ) {
+        // The first epoch has a start block == 0.
+        const START_BLOCK: BlockIndex = 0;
+        const END_BLOCK_NON_INCLUSIVE: BlockIndex = 100;
+        let epoch_block_range = BlockRange::new(START_BLOCK, END_BLOCK_NON_INCLUSIVE);
+        let epoch_sharding_strategy = EpochShardingStrategy::new(epoch_block_range);
+
+        let is_ready_to_serve_txouts = epoch_sharding_strategy.is_ready_to_serve_tx_outs(0.into());
+
+        assert!(is_ready_to_serve_txouts)
+    }
+
+    #[test]
+    fn is_ready_to_serve_tx_outs_first_epoch_processed_block_count_is_zero_over_50_percent_processed_returns_true(
+    ) {
+        // The first epoch has a start block == 0.
+        const START_BLOCK: BlockIndex = 0;
+        const END_BLOCK_NON_INCLUSIVE: BlockIndex = 100;
+        let epoch_block_range = BlockRange::new(START_BLOCK, END_BLOCK_NON_INCLUSIVE);
+        let epoch_sharding_strategy = EpochShardingStrategy::new(epoch_block_range);
+
+        let is_ready_to_serve_txouts = epoch_sharding_strategy.is_ready_to_serve_tx_outs(70.into());
+
+        assert!(is_ready_to_serve_txouts)
+    }
+
+    #[test]
+    fn is_ready_to_serve_tx_outs_not_first_epoch_processed_block_count_less_than_50_percent_of_epoch_range_length_returns_false(
+    ) {
+        const START_BLOCK: BlockIndex = 100;
+        const END_BLOCK_NON_INCLUSIVE: BlockIndex = 110;
+        let epoch_block_range_length = END_BLOCK_NON_INCLUSIVE - START_BLOCK;
+        let minimum_processed_block_count = epoch_block_range_length / 2;
+        let epoch_block_range = BlockRange::new(START_BLOCK, END_BLOCK_NON_INCLUSIVE);
+        let epoch_sharding_strategy = EpochShardingStrategy::new(epoch_block_range);
+
+        let is_ready_to_serve_txouts = epoch_sharding_strategy
+            .is_ready_to_serve_tx_outs((minimum_processed_block_count - 1).into());
+
+        assert!(!is_ready_to_serve_txouts)
+    }
+
+    #[test]
+    fn is_ready_to_serve_tx_outs_not_first_epoch_processed_block_count_equals_50_percent_of_epoch_range_length_caught_up_returns_true(
+    ) {
+        const START_BLOCK: BlockIndex = 100;
+        const END_BLOCK_NON_INCLUSIVE: BlockIndex = 110;
+        let epoch_block_range_length = END_BLOCK_NON_INCLUSIVE - START_BLOCK;
+        let minimum_processed_block_count = epoch_block_range_length / 2;
+        let epoch_block_range = BlockRange::new(START_BLOCK, END_BLOCK_NON_INCLUSIVE);
+        let epoch_sharding_strategy = EpochShardingStrategy::new(epoch_block_range);
+
+        let is_ready_to_serve_txouts =
+            epoch_sharding_strategy.is_ready_to_serve_tx_outs(minimum_processed_block_count.into());
+
+        assert!(is_ready_to_serve_txouts)
+    }
+
+    #[test]
+    fn is_ready_to_serve_tx_outs_not_first_epoch_processed_block_count_greater_than_50_percent_of_epoch_range_length_caught_up_returns_true(
+    ) {
+        const START_BLOCK: BlockIndex = 100;
+        const END_BLOCK_NON_INCLUSIVE: BlockIndex = 110;
+        let epoch_block_range_length = END_BLOCK_NON_INCLUSIVE - START_BLOCK;
+        let minimum_processed_block_count = epoch_block_range_length / 2;
+        let epoch_block_range = BlockRange::new(START_BLOCK, END_BLOCK_NON_INCLUSIVE);
+        let epoch_sharding_strategy = EpochShardingStrategy::new(epoch_block_range);
+
+        let is_ready_to_serve_txouts = epoch_sharding_strategy
+            .is_ready_to_serve_tx_outs((minimum_processed_block_count + 1).into());
+
+        assert!(is_ready_to_serve_txouts)
     }
 }
