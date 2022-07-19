501a6afb56c73db8fb06d2afb3b1d2ed1d0f35d5
James Cape <james@mobilecoin.com>
2022-07-19 10:25:33 -0700

    Revert "Revert SGX 2.17 Changes (#2202)"
    This reverts commit 06e09b3e0ffac7f8b6845c9e09bfe992f72a5f59.

--- fog/distribution/src/config.rs
+++ fog/distribution/src/config.rs
@@ -99,7 +99,8 @@ impl Config {
     ) -> ConnectionResult<Vec<SyncConnection<ThickClient<HardcodedCredentialsProvider>>>> {
         let mut mr_signer_verifier =
             MrSignerVerifier::from(mc_consensus_enclave_measurement::sigstruct());
-        mr_signer_verifier.allow_hardening_advisory("INTEL-SA-00334");
+        mr_signer_verifier
+            .allow_hardening_advisories(mc_consensus_enclave_measurement::HARDENING_ADVISORIES);
 
         let mut verifier = Verifier::default();
         verifier.mr_signer(mr_signer_verifier).debug(DEBUG_ENCLAVE);
