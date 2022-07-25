2c8df7c10331f062037b8f32780513e731361d0e
Jason Greathouse <jgreat@jgreat.me>
2022-07-25 15:17:47 -0500

    update lint.sh to require up to date lock files, and echo step for eaiser debugging on reported issues

--- tools/lint.sh
+++ tools/lint.sh
@@ -13,9 +13,12 @@ cargo install cargo-sort
 for toml in $(grep --exclude-dir cargo --exclude-dir rust-mbedtls --include=Cargo.toml -r . -e '\[workspace\]' | cut -d: -f1); do
   pushd $(dirname $toml) >/dev/null
   echo "Linting in $PWD"
+  echo "Run cargo sort"
   cargo sort --workspace --grouped --check
+  echo "Run cargo fmt"
   cargo fmt -- --unstable-features --check
-  cargo clippy --all --all-features
+  echo "Run cargo clippy"
+  cargo clippy --all --all-features --locked
   echo "Linting in $PWD complete."
   popd >/dev/null
 done
