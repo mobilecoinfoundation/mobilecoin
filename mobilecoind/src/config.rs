4944dca861c61fd05e313d8ba828f0f898b87de8
Chris Beck <beck.ct@gmail.com>
2022-08-01 06:07:27 -0600

    more fix to mobilecoind

--- mobilecoind/src/config.rs
+++ mobilecoind/src/config.rs
@@ -203,7 +203,7 @@ impl Config {
         );
 
         // TODO: Supply a network-id to mobilecoind?
-        let conn = GrpcFogReportConnection::new(String::default(), env, logger);
+        let conn = GrpcFogReportConnection::new(self.peers_config.chain_id.to_owned(), env, logger);
 
         let verifier = self.get_fog_ingest_verifier();
 
