84f0ff288fcd28d2d25ce9820bfb16e8b8541743
Millie C <gyro@pop-os.localdomain>
2022-08-04 20:32:40 -0400

    Ledger router bin improvements.

--- fog/ledger/server/src/bin/router.rs
+++ fog/ledger/server/src/bin/router.rs
@@ -3,11 +3,12 @@ use std::{env, sync::Arc, str::FromStr};
 use grpcio::ChannelBuilder;
 use mc_common::logger::log;
 use mc_fog_api::ledger_grpc::LedgerStoreApiClient;
-use mc_fog_ledger_enclave::ENCLAVE_FILE;
-use mc_fog_ledger_server::{LedgerRouterConfig};
+use mc_fog_ledger_enclave::{ENCLAVE_FILE, LedgerSgxEnclave};
+use mc_fog_ledger_server::{LedgerRouterConfig, LedgerRouterServer};
 use clap::Parser;
-use mc_fog_uri::LedgerStoreUri;
+use mc_fog_uri::{LedgerStoreUri, LedgerStoreScheme};
 use mc_util_grpc::ConnectionUriGrpcioChannel;
+use mc_util_uri::UriScheme;
 
 fn main() {
     mc_common::setup_panic_handler();
@@ -24,34 +25,38 @@ fn main() {
         enclave_path.to_str().unwrap(),
         &config.client_responder_id
     );
-
+    let enclave = LedgerSgxEnclave::new(
+        enclave_path,
+        &config.client_responder_id,
+        config.omap_capacity,
+        logger.clone(),
+    );
     
-    let mut fog_view_store_grpc_clients = Vec::new();
+    let mut ledger_store_grpc_clients = Vec::new();
     let grpc_env = Arc::new(
         grpcio::EnvBuilder::new()
             .name_prefix("Main-RPC".to_string())
             .build(),
     );
+    
     for i in 0..50 {
         let shard_uri_string = format!(
-            "insecure-fog-ledger-store://node{}.test.mobilecoin.com:3225",
+            "{}://node{}.test.mobilecoin.com:3225",
+            LedgerStoreScheme::SCHEME_INSECURE,
             i
         );
         let shard_uri = LedgerStoreUri::from_str(&shard_uri_string).unwrap();
-        let fog_view_store_grpc_client = LedgerStoreApiClient::new(
+        let ledger_store_grpc_client = LedgerStoreApiClient::new(
             ChannelBuilder::default_channel_builder(grpc_env.clone())
                 .connect_to_uri(&shard_uri, &logger),
         );
-        fog_view_store_grpc_clients.push(fog_view_store_grpc_client);
+        ledger_store_grpc_clients.push(ledger_store_grpc_client);
     }
-
-    let _enclave = todo!("Fog ledger router enclave not yet implemented.");
-    /*
+    
     let mut router_server =
-        LedgerRouterServer::new(config, enclave, fog_view_store_grpc_clients, logger);
+        LedgerRouterServer::new(config, enclave, ledger_store_grpc_clients, logger);
     router_server.start();
     loop {
         std::thread::sleep(std::time::Duration::from_millis(1000));
     }
-    */
 }
