eee0f78f67e9a477830856517ff91ddc87e006bf
David Alvarez <daveyalvarez03@gmail.com>
2022-08-15 10:36:44 -0700

    partial on generate-sample-ledger

--- util/generate-sample-ledger/src/lib.rs
+++ util/generate-sample-ledger/src/lib.rs
@@ -8,7 +8,10 @@ use mc_account_keys::PublicAddress;
 use mc_blockchain_test_utils::get_blocks_with_recipients;
 use mc_common::logger::{log, Logger};
 use mc_ledger_db::{Ledger, LedgerDB};
-use mc_transaction_core::{constants::TOTAL_MOB, BlockVersion};
+use mc_transaction_core::{
+    constants::TOTAL_MOB, mint::MintTx as CoreMintTx, BlockVersion, TokenId,
+};
+use mc_transaction_core_test_utils::{create_mint_config_tx_and_signers, create_mint_tx};
 use rand::SeedableRng;
 use rand_hc::Hc128Rng as FixedRng;
 use std::path::Path;
@@ -107,4 +110,53 @@ pub fn bootstrap_ledger(
            key_images_per_block,
            mc_util_build_info::git_commit(),
     ).expect("File I/O");
+}
+
+pub fn bootstrap_mints_burns(
+    path: &Path,
+    burn_amount: u64,
+    burn_tx_count: u16,
+    mint_amount: u64,
+    mint_tx_count: u16,
+    seed: Option<[u8; 32]>,
+    logger: Logger,
+) {
+    let mut db = LedgerDB::open(path).expect("Could not open ledger_db");
+    let conn = db.get_conn().unwrap();
+
+    // generate mint config // not sure if this is still needed
+    let max_mintable = if (mint_amount <= TOTAL_MOB) {
+        TOTAL_MOB
+    } else {
+        mint_amount
+    };
+
+    let mut rng = mc_util_test_helper::get_seeded_rnt();
+
+    // create the core mint and mint configs
+    let token_id = TokenId::from(1);
+    let (_mint_config_tx, signers) = create_mint_config_tx_and_signers(token_id1, &mut rng);
+    let mint_tx1 = create_mint_tx(token_id, &signers, 100, &mut rng);
+
+    let mut mint_txs = Vec<MintTx>::with_capacity(100);
+
+    // for 100 records
+    for i in 0..100 {
+        // create the core mint and mint configs
+        let token_id = TokenId::from(i+1);
+        let (_mint_config_tx, signers) = create_mint_config_tx_and_signers(token_id, &mut rng);
+        mint_txs[i] = create_mint_tx(token_id, &signers, 100, &mut rng);
+
+        // store record in db
+        MintTx::insert_from_core_mint_tx(5, None, &mint_tx1, &conn).unwrap();
+    }
+
+    // store the mints
+    // MintTx::insert_from_core_mint_tx(5, None, &mint_tx1, &conn).unwrap();
+
+    // iterate through mint_tx_count, creating gnosis safe deposits and mint
+    // transactions
+
+    // iterate through burn_tx_count, creating burn txos and gnosis safe
+    // withdrawals
 }
