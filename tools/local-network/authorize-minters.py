5cb641dc06637a2da4598a13d7b661b873771b33
Chris Beck <beck.ct@gmail.com>
2022-08-19 20:09:31 -0600

    put mint auditor integration test in CI (#2411)
    * put mint auditor integration test in CI
    
    This is useful for two reasons:
    * Exercise mint auditor test earlier in case we break it
    * Serve as an example for how to get a local network with mints
      and burns in it. This is helpful for local testing of bridge stuff.
    
    The work here is two parts:
    * There are small differences between how jenkins sets up minting
      and how the local network script does.
    * The local network script signs a tokens file because this is required
      to start the network, but it doesn't authorize any minters, because
      it doesn't do anything further once the netwokr starts up.
    * Jenkins both creates governors and then uses the governors to
      authorize minters.
    * The mint auditor integration test requires that minters are ready
      to go.
    * Therefore, we make a python script (which imports and takes constants
      from local-network.py) to authorize minters, which can be done
      after the network has started.
    * Then we make a ci step that starts a local network, then calls the
      authorize minters script, then starts the mint auditor integration
      test.
    
    * fixups
    
    * fixup
    
    * fix compile-proto and ci.yml
    
    * fix ledger base in ci.yml
    
    * fixup ledger base path
    
    * fixup compile protos for python
    
    * fixup to mint auditor test invocation
    
    * update README with info about how to run the integration tests [skip ci]
    
    * Update .github/workflows/ci.yml
    
    Co-authored-by: Remoun Metyas <remoun@mobilecoin.com>
    
    * Update mint-auditor/README.md
    
    Co-authored-by: Eran Rundstein <eran@rundste.in>
    
    * Update mint-auditor/README.md
    
    Co-authored-by: Eran Rundstein <eran@rundste.in>
    
    Co-authored-by: Remoun Metyas <remoun@mobilecoin.com>
    Co-authored-by: Eran Rundstein <eran@rundste.in>

---
+++ tools/local-network/authorize-minters.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# Copyright (c) 2018-2022 The MobileCoin Foundation
+
+# This script can be run after starting a local network in order to generate and
+# authorize minters.
+#
+# It must be kept in sync with the governor setup in local_network.py
+#
+# This is a way to prepare the network for running the mint auditor integration tests
+
+import argparse
+import os
+import subprocess
+import time
+
+from local_network import *
+
+# Generate minter 1 and minter 2 ed25519 keys in the minting keys dir, if they don't already exist
+if not os.path.exists(f'{MINTING_KEYS_DIR}/minter1'):
+    subprocess.check_output(f'openssl genpkey -algorithm ed25519 -out {MINTING_KEYS_DIR}/minter1', shell=True)
+    subprocess.check_output(f'openssl pkey -pubout -in {MINTING_KEYS_DIR}/minter1 -out {MINTING_KEYS_DIR}/minter1.pub', shell=True)
+
+if not os.path.exists(f'{MINTING_KEYS_DIR}/minter2'):
+    subprocess.check_output(f'openssl genpkey -algorithm ed25519 -out {MINTING_KEYS_DIR}/minter2', shell=True)
+    subprocess.check_output(f'openssl pkey -pubout -in {MINTING_KEYS_DIR}/minter2 -out {MINTING_KEYS_DIR}/minter2.pub', shell=True)
+
+# Submit a MintConfigTx that allows minter1.private to mint up to 1 billion token 1 tokens.
+subprocess.check_output(' '.join([
+    f'cd {PROJECT_DIR} && exec {TARGET_DIR}/mc-consensus-mint-client',
+    'generate-and-submit-mint-config-tx',
+    f'--node insecure-mc://localhost:{BASE_CLIENT_PORT}',
+    f'--signing-key {MINTING_KEYS_DIR}/governor1',
+    f'--token-id 1',
+    f'--config 1000000000:1:{MINTING_KEYS_DIR}/minter1.pub',
+    '--total-mint-limit 10000000000'
+]), shell=True)
+
+# Submit a MintConfigTx that allows minter2.private to mint up to 1 billion token 2 tokens.
+subprocess.check_output(' '.join([
+    f'cd {PROJECT_DIR} && exec {TARGET_DIR}/mc-consensus-mint-client',
+    'generate-and-submit-mint-config-tx',
+    f'--node insecure-mc://localhost:{BASE_CLIENT_PORT}',
+    f'--signing-key {MINTING_KEYS_DIR}/governor2',
+    f'--token-id 2',
+    f'--config 1000000000:1:{MINTING_KEYS_DIR}/minter2.pub',
+    '--total-mint-limit 10000000000'
+]), shell=True)
+
