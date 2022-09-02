71e4bcd22b77885935257cbfbf6d9504c2d61386
James Cape <james@mobilecoin.com>
2022-09-02 10:05:12 -0700

    Minor DRY cleanups around sessions and auth messages (#2462)
    * attest::NonceMessage, EnclaveNonceMessage, add handling to attest-ake
    
    * Rustfmt fixes
    
    * Minor DRY cleanups around sessions and auth messages

--- attest/enclave-api/src/lib.rs
+++ attest/enclave-api/src/lib.rs
@@ -16,73 +16,46 @@ use core::hash::Hash;
 use mc_attest_core::{QuoteNonce, Report};
 use serde::{Deserialize, Serialize};
 
+macro_rules! impl_newtype_vec_inout {
+    ($($newtype:ident;)*) => {$(
+        impl From<alloc::vec::Vec<u8>> for $newtype {
+            fn from(src: alloc::vec::Vec<u8>) -> $newtype {
+                $newtype(src)
+            }
+        }
+
+        impl From<$newtype> for alloc::vec::Vec<u8> {
+            fn from(src: $newtype) -> alloc::vec::Vec<u8> {
+                src.0
+            }
+        }
+    )*}
+}
+
+impl_newtype_vec_inout! {
+    ClientAuthRequest; ClientAuthResponse; ClientSession;
+    PeerAuthRequest; PeerAuthResponse; PeerSession;
+}
+
 /// The raw authentication request message, sent from an initiator to a
 /// responder
 #[derive(Clone, Debug, Default, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 pub struct ClientAuthRequest(Vec<u8>);
-
-impl From<Vec<u8>> for ClientAuthRequest {
-    fn from(src: Vec<u8>) -> Self {
-        Self(src)
-    }
-}
-
-impl From<ClientAuthRequest> for Vec<u8> {
-    fn from(src: ClientAuthRequest) -> Vec<u8> {
-        src.0
-    }
-}
 
 /// The raw authentication response message, sent from a responder to an
 /// initiator.
 #[derive(Clone, Debug, Default, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 pub struct ClientAuthResponse(Vec<u8>);
 
-impl From<ClientAuthResponse> for Vec<u8> {
-    fn from(src: ClientAuthResponse) -> Vec<u8> {
-        src.0
-    }
-}
-
-impl From<Vec<u8>> for ClientAuthResponse {
-    fn from(src: Vec<u8>) -> Self {
-        Self(src)
-    }
-}
-
 /// The raw authentication request message, sent from an initiator to a
 /// responder
 #[derive(Clone, Debug, Default, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 pub struct PeerAuthRequest(Vec<u8>);
 
-impl From<Vec<u8>> for PeerAuthRequest {
-    fn from(src: Vec<u8>) -> Self {
-        Self(src)
-    }
-}
-
-impl From<PeerAuthRequest> for Vec<u8> {
-    fn from(src: PeerAuthRequest) -> Vec<u8> {
-        src.0
-    }
-}
-
 /// The raw authentication response message, sent from a responder to an
 /// initiator.
 #[derive(Clone, Debug, Default, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 pub struct PeerAuthResponse(Vec<u8>);
-
-impl From<PeerAuthResponse> for Vec<u8> {
-    fn from(src: PeerAuthResponse) -> Vec<u8> {
-        src.0
-    }
-}
-
-impl From<Vec<u8>> for PeerAuthResponse {
-    fn from(src: Vec<u8>) -> Self {
-        Self(src)
-    }
-}
 
 /// Inbound and outbound messages to/from an enclave.
 #[derive(Clone, Debug, Default, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
@@ -119,39 +92,35 @@ pub struct NewEReportResponse {
 
 /// A helper trait to aid in generic implementation of enclave methods
 pub trait Session:
-    Clone + Default + Eq + Hash + for<'bytes> From<&'bytes [u8]> + Into<Vec<u8>>
+    Clone + Default + Hash + for<'bytes> From<&'bytes [u8]> + Into<Vec<u8>> + PartialEq + PartialOrd
 {
     type Request: Into<Vec<u8>>;
     type Response: From<Vec<u8>>;
 }
 
+macro_rules! impl_newtype_asref_and_from_bytes {
+    ($($newtype:ident;)*) => {$(
+        impl AsRef<[u8]> for $newtype {
+            fn as_ref(&self) -> &[u8] {
+                self.0.as_ref()
+            }
+        }
+
+        impl<'bytes> From<&'bytes [u8]> for $newtype {
+            fn from(src: &[u8]) -> $newtype {
+                Self(alloc::vec::Vec::from(src))
+            }
+        }
+    )*}
+}
+
+impl_newtype_asref_and_from_bytes! {
+    ClientSession; PeerSession;
+}
+
 /// An opaque bytestream used as a client session
 #[derive(Clone, Debug, Default, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
-pub struct ClientSession(pub Vec<u8>);
-
-impl AsRef<[u8]> for ClientSession {
-    fn as_ref(&self) -> &[u8] {
-        self.0.as_ref()
-    }
-}
-
-impl<'bytes> From<&'bytes [u8]> for ClientSession {
-    fn from(src: &[u8]) -> ClientSession {
-        Self(Vec::from(src))
-    }
-}
-
-impl From<Vec<u8>> for ClientSession {
-    fn from(src: Vec<u8>) -> ClientSession {
-        ClientSession(src)
-    }
-}
-
-impl From<ClientSession> for Vec<u8> {
-    fn from(src: ClientSession) -> Vec<u8> {
-        src.0
-    }
-}
+pub struct ClientSession(Vec<u8>);
 
 impl Session for ClientSession {
     type Request = ClientAuthRequest;
@@ -161,30 +130,6 @@ impl Session for ClientSession {
 /// An opaque bytestream used as a peer session ID.
 #[derive(Clone, Debug, Default, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 pub struct PeerSession(Vec<u8>);
-
-impl AsRef<[u8]> for PeerSession {
-    fn as_ref(&self) -> &[u8] {
-        self.0.as_ref()
-    }
-}
-
-impl<'bytes> From<&'bytes [u8]> for PeerSession {
-    fn from(src: &[u8]) -> PeerSession {
-        Self(Vec::from(src))
-    }
-}
-
-impl From<Vec<u8>> for PeerSession {
-    fn from(src: Vec<u8>) -> PeerSession {
-        PeerSession(src)
-    }
-}
-
-impl From<PeerSession> for Vec<u8> {
-    fn from(src: PeerSession) -> Vec<u8> {
-        src.0
-    }
-}
 
 impl Session for PeerSession {
     type Request = PeerAuthRequest;
