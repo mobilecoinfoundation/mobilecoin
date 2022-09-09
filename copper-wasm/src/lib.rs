3309e953c4d89ac0c85e523536aa1ea833f4026f
Eran Rundstein <eran@rundste.in>
2022-09-09 16:39:04 -0700

    simple test

--- copper-wasm/src/lib.rs
+++ copper-wasm/src/lib.rs
@@ -32,3 +32,31 @@ pub fn get_address(view_public_key: &str, spend_public_key: &str) -> Result<Stri
 
     Ok(address)
 }
+
+mod tests {
+    use super::*;
+    use wasm_bindgen_test::*;
+
+    #[wasm_bindgen_test]
+    fn invalid_keys() {
+        let result = get_address("a", "b");
+        assert_eq!(
+            result,
+            Err(JsValue::from(
+                "Failed to parse view_private_key: Odd number of digits"
+            ))
+        );
+    }
+
+    #[wasm_bindgen_test]
+    fn valid_keys() {
+        let result = get_address(
+            "5ea069957773a6415fdba501642579c6121a2c9cc2300989f98872ef70af9e13",
+            "00c992f14f3c7862267eca290d2cff8212b2335e3e00a5f17ff0ba9e24f7874f",
+        );
+        assert_eq!(
+            result,
+            Ok("4VucmHjybn9EhXo5eoTy7RcgBPw7bWgeBta3tQnnvSoKsCnWXtt6berF97hBsKJYeMgzMXy4DFHJ25mnGoDVvQHAKDTt2siDdPoChyejWkW".to_string())
+        );
+    }
+}
