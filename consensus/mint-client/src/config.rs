12fd0a8213b5a63358c19368c837c00bd7d87691
Chris Beck <beck.ct@gmail.com>
2022-09-16 19:03:36 -0600

    Add check to mint client to ensure public address doesn't have fog (#2546)
    * Add check to mint client to ensure public address doesn't have fog
    
    The mint client reads from a b58 address when it creates a MintTxPrefix,
    but it doesn't (before this commit) check if that is actually a fog
    address. If it is, the fog part will get silently removed, and then
    the fog user won't find it.
    
    This change makes it give an error message.
    
    * fixups

--- consensus/mint-client/src/config.rs
+++ consensus/mint-client/src/config.rs
@@ -162,6 +162,9 @@ impl MintTxPrefixParams {
         self,
         fallback_tombstone_block: impl Fn() -> u64,
     ) -> Result<MintTxPrefix, String> {
+        if let Some(fog_url) = self.recipient.fog_report_url() {
+            return Err(format!("This recipient has a fog url, but minting to fog users is not supported right now: '{}'", fog_url));
+        }
         let tombstone_block = self.tombstone.unwrap_or_else(fallback_tombstone_block);
         let nonce = get_or_generate_nonce(self.nonce);
         Ok(MintTxPrefix {
