9a530bbd8f692d04525f6aaa4bdf2aaf99c3ef9c
Eran Rundstein <eran@rundste.in>
2022-09-19 14:15:22 -0700

    Change McRng to be a ZST that forwards calls to thread_rng(). This makes it implement Send, which is currently required for android-bindings

--- crypto/rand/src/fallback.rs
+++ crypto/rand/src/fallback.rs
@@ -1,5 +1,28 @@
 // Copyright (c) 2018-2022 The MobileCoin Foundation
 
-use rand::rngs::ThreadRng;
+use rand::{thread_rng, CryptoRng, Error, RngCore};
 
-pub type McRng = ThreadRng;
+#[derive(Clone, Debug, Default)]
+pub struct McRng;
+
+impl RngCore for McRng {
+    #[inline(always)]
+    fn next_u32(&mut self) -> u32 {
+        thread_rng().next_u32()
+    }
+
+    #[inline(always)]
+    fn next_u64(&mut self) -> u64 {
+        thread_rng().next_u64()
+    }
+
+    fn fill_bytes(&mut self, dest: &mut [u8]) {
+        thread_rng().fill_bytes(dest)
+    }
+
+    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), Error> {
+        thread_rng().try_fill_bytes(dest)
+    }
+}
+
+impl CryptoRng for McRng {}
