7614661adfbe482c6e9ddcbc26f52e66d763da36
Sam Dealy <33067698+samdealy@users.noreply.github.com>
2022-09-26 19:07:14 -0400

    Create db cleaner binary (#2595)

---
+++ fog/sql_recovery_db/cleanup/src/db_cleaner.rs
@@ -0,0 +1,55 @@
+// Copyright (c) 2018-2022 The MobileCoin Foundation
+
+//! A cleanup utility for the Fog SQL DB.
+
+use chrono::{prelude::*, Duration};
+use mc_common::logger::{log, Logger};
+use mc_fog_recovery_db_iface::RecoveryDb;
+use mc_fog_sql_recovery_db::SqlRecoveryDb;
+
+/// Contains helper methods that cleanup the Fog SQL DB.
+pub struct DbCleaner {
+    /// The Fog DB being cleaned up.
+    db: SqlRecoveryDb,
+
+    /// Logger instance.
+    logger: Logger,
+}
+
+impl DbCleaner {
+    pub fn new(db: SqlRecoveryDb, logger: Logger) -> Self {
+        Self { db, logger }
+    }
+
+    /// Identifies expired egress keys and either deletes their associated
+    /// ingest invocations or prints them out.
+    pub fn cleanup_egress_keys(&self, is_dry_run: bool, expiration: Duration) {
+        let expired_date_time = Utc::now()
+            .naive_utc()
+            .checked_sub_signed(expiration)
+            .expect("Expiration should always be in the past");
+
+        let expired_ingest_invocations = self
+            .db
+            .get_expired_invocations(expired_date_time)
+            .expect("Could not retrieve expired ingest invocations.");
+
+        if is_dry_run {
+            log::info!(
+                self.logger,
+                "There are {} expired ingest invocations",
+                expired_ingest_invocations.len()
+            );
+            for (i, expired_ingest_invocation) in expired_ingest_invocations.iter().enumerate() {
+                log::info!(
+                    self.logger,
+                    "Expired Egress key {}\n  ingest_invocation_id: {}\n  egress_public_key: {:?}\n  last_active_at: {:?}",
+                    i + 1,
+                    expired_ingest_invocation.ingest_invocation_id,
+                    expired_ingest_invocation.egress_public_key,
+                    expired_ingest_invocation.last_active_at,
+                );
+            }
+        }
+    }
+}
