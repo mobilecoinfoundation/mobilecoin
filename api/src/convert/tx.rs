a00d7645fc36dc58442563137c0bb155f209c70c
Chris Beck <beck.ct@gmail.com>
2022-09-27 14:27:32 -0600

    Update api/src/convert/tx.rs
    Co-authored-by: Eran Rundstein <eran@rundste.in>

--- api/src/convert/tx.rs
+++ api/src/convert/tx.rs
@@ -266,7 +266,7 @@ mod tests {
             )
             .unwrap();
 
-            // Originator requests an output worth amount2 destined to themselves
+            // Originator requests an output worth 1MOB destined to themselves
             sci_builder
                 .add_partial_fill_output(
                     Amount::new(1000 * MILLIMOB_TO_PICOMOB, Mob::ID),
