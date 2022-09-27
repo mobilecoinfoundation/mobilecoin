b526190f9f312de7fa6c5de2f99c6aadd90d6aca
Chris Beck <beck.ct@gmail.com>
2022-09-26 20:48:58 -0600

    Update transaction/std/src/signed_contingent_input_builder.rs
    Co-authored-by: Nick Santana <nick@mobilecoin.com>

--- transaction/std/src/signed_contingent_input_builder.rs
+++ transaction/std/src/signed_contingent_input_builder.rs
@@ -3115,7 +3115,7 @@ pub mod tests {
             )
             .unwrap();
 
-            // Orignator requests an output worth amount2 destined to themselves
+            // Originator requests an output worth amount2 destined to themselves
             builder
                 .add_fractional_output(amount2, &originator.default_subaddress(), &mut rng)
                 .unwrap();
