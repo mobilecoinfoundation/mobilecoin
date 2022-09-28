2c30fca01ca82db4310174d15a97e12004ecc729
Chris Beck <beck.ct@gmail.com>
2022-09-28 13:52:52 -0600

    WIP burn-script
    This as a similar interface to `drain_accounts.py` and can be used
    in a flow where we create wrapped tokens for one of the bootstrapped
    sample keys, and then burn those tokens, as part of mint auditor
    testing.
    
    This is derived from a script that Eran wrote.
    
    Not tested yet, will likely need fixes.

---
+++ mobilecoind/strategies/burn.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2018-2022 The MobileCoin Foundation
+
+"""
+The purpose of this strategy is to burn a specified amount of funds from some account.
+
+This can be used in tests and demos of the reserve-auditor work flow.
+
+
+
+Example setup and usage:
+```
+    python3 burn.py --key ../../../target/sample_data/keys/account5.json --token-id 1 --value 1000000 --fee 2000
+```
+"""
+import argparse
+import glob
+import logging
+import mobilecoind_api_pb2
+import os
+import sys
+import time
+from accounts import load_key_and_register
+from google.protobuf.empty_pb2 import Empty
+
+logging.basicConfig(stream = sys.stdout, level = logging.INFO, format="%(levelname)s:%(module)s:%(lineno)s: %(message)s")
+
+
+def parse_args() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mobilecoind-host",
+                        default="localhost",
+                        type=str,
+                        help="Mobilecoind host")
+    parser.add_argument("--mobilecoind-port",
+                        default="4444",
+                        type=str,
+                        help="Mobilecoind port")
+    parser.add_argument("--key",
+                        type=str,
+                        help="Path a private account key json, in the sample_data format")
+    parser.add_argument("--max-seconds",
+                        type=int,
+                        default=40,
+                        help="Number of seconds to wait for a tx to clear")
+    parser.add_argument("--token-id",
+                        type=int,
+                        default=0,
+                        help="Token id to burn")
+    parser.add_argument("--value",
+                        type=int,
+                        default=0,
+                        help="Quantity of this token id to burn")
+    parser.add_argument("--fee",
+                        type=int,
+                        default=20,
+                        help="Amount of this token id to use as a fee")
+
+
+    return parser.parse_args()
+
+class MobilecoindClient:
+    def __init__(self, address, ssl=False):
+        if ssl:
+            credentials = grpc.ssl_channel_credentials()
+            self.channel = grpc.secure_channel(address, credentials)
+        else:
+            self.channel = grpc.insecure_channel(address)
+        self.stub = MobilecoindAPIStub(self.channel)
+
+    def new_monitor_from_key_file(self, file):
+        """Create a new monitor from a key file"""
+        return load_key_and_register(file, stub)
+
+    def remove_monitor(self, monitor_id):
+        self.stub.RemoveMonitor(RemoveMonitorRequest(monitor_id=monitor_id))
+
+    def get_b58_public_address(self, monitor_id):
+        return self.stub.GetPublicAddress(
+            GetPublicAddressRequest(monitor_id=monitor_id)
+        ).b58_code
+
+    def get_balance(self, monitor_id, token_id, subaddress_index=0):
+        return self.stub.GetBalance(
+            GetBalanceRequest(
+                monitor_id=monitor_id,
+                subaddress_index=subaddress_index,
+                token_id=token_id,
+            )
+        ).balance
+
+    def get_network_block_index(self):
+        return self.stub.GetNetworkStatus(
+            empty_pb2.Empty()
+        ).network_highest_block_index
+
+    def get_utxos(self, monitor_id, token_id, subaddress_index=0):
+        return self.stub.GetUnspentTxOutList(
+            GetUnspentTxOutListRequest(
+                monitor_id=monitor_id,
+                subaddress_index=subaddress_index,
+                token_id=token_id,
+            )
+        ).output_list
+
+    def generate_burn_redemption_tx(
+        self,
+        monitor_id,
+        input_list,
+        burn_amount,
+        fee,
+        token_id,
+        change_subaddress=0,
+        tombstone=None,
+        redemption_memo=None,
+        enable_destination_memo=False,
+    ):
+        if tombstone is None:
+            tombstone = self.get_network_block_index() + 10
+
+        request = GenerateBurnRedemptionTxRequest(
+            sender_monitor_id=monitor_id,
+            change_subaddress=change_subaddress,
+            input_list=input_list,
+            burn_amount=burn_amount,
+            fee=fee,
+            tombstone=tombstone,
+            token_id=token_id,
+            redemption_memo=redemption_memo,
+            enable_destination_memo=enable_destination_memo,
+        )
+        return self.stub.GenerateBurnRedemptionTx(request).tx_proposal
+
+    def submit_tx(self, tx_proposal):
+        return self.stub.SubmitTx(
+            SubmitTxRequest(
+                tx_proposal=tx_proposal,
+            )
+        )
+
+    def wait_for_monitor_to_sync(self, monitor_id, poll_interval=0.1):
+        while True:
+            network_block_index = self.get_network_block_index()
+            status = self.stub.GetMonitorStatus(
+                GetMonitorStatusRequest(
+                    monitor_id=monitor_id
+                )
+            ).status
+            if status.next_block >= network_block_index:
+                break
+            time.sleep(poll_interval)
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    logging.debug(args)
+
+    client = MobilecoindClient(f"{args.mobilecoind_host}:{args.mobilecoind_port}")
+    monitor = client.new_monitor_from_keyfile(args.key)
+
+    client.wait_for_monitor_to_sync(monitor)
+
+    balance = client.get_balance(monitor, args.token_id)
+
+    if balance < args.value + args.fee:
+        raise Exception(f"Insufficient balance: {balance} < {args.value} + {args.fee}")
+
+    utxos = client2.get_utxos(monitor_id, args.token_id)
+    tx_proposal = client2.generate_burn_redemption_tx(
+        monitor_id,
+        utxos,
+        args.value,
+        args.fee,
+        args.token_id,
+    )
+    logging.info("Submitting burn transaction")
+    print(tx_proposal)
+    submit_response = client2.submit_tx(tx_proposal)
+    print(submit_response)
+
+    time.sleep(3)
+
+    client.wait_for_monitor_to_sync(monitor)
+    new_balance = client.get_balance(monitor, args.token_id)
+
+    if new_balance == balance - args.value - args.fee:
+        logging.info("Burn transaction was successful")
