1a2847ead9c51d1f356a54d44162d27b62914093
Chris Beck <beck.ct@gmail.com>
2022-10-04 10:31:09 -0600

    review comments

--- consensus/tool/src/main.rs
+++ consensus/tool/src/main.rs
@@ -11,7 +11,7 @@ use mc_connection::BlockInfo;
 use mc_consensus_api::{consensus_common_grpc::BlockchainApiClient, empty::Empty};
 use mc_util_grpc::ConnectionUriGrpcioChannel;
 use mc_util_uri::ConsensusClientUri;
-use serde_json::{to_string_pretty};
+use serde_json::to_string_pretty;
 use std::{sync::Arc, time::Duration};
 
 /// A cli tool for interrogating one or more consensus nodes.
@@ -22,9 +22,11 @@ use std::{sync::Arc, time::Duration};
 ///
 /// $ mc-consensus-tool status mc://localhost:3200 | jq .network_block_version
 ///
-/// $ mc-consensus-tool wait-for-quiet mc://localhost:3200 mc://localhost:3201 mc://localhost:3202
+/// $ STOP_BLOCK=$(mc-consensus-tool wait-for-quiet \
+/// mc://localhost:3200 mc://localhost:3201 mc://localhost:3202)
 ///
-/// $ MC_PEERS=mc://localhost:3200,mc://localhost:3201 mc-consensus-tool wait-for-block --index=5005
+/// $ MC_PEERS=mc://localhost:3200,mc://localhost:3201 \
+/// mc-consensus-tool wait-for-block --index=$(STOP_BLOCK)
 #[derive(Clone, Debug, Parser)]
 #[clap(name = "mc-consensus-tool")]
 pub struct Config {
@@ -47,10 +49,11 @@ pub enum ToolCommand {
     /// given block index.
     WaitForBlock {
         /// Block index which must appear
+        #[clap(long, env = "MC_INDEX")]
         index: u64,
 
         /// Polling period in seconds
-        #[clap(long, default_value = "1")]
+        #[clap(long, env = "MC_PERIOD", default_value = "1")]
         period: u64,
     },
     /// Wait-for-quiet: Blocks until the network is 'quiet'. This means that the
@@ -58,14 +61,14 @@ pub enum ToolCommand {
     /// block_index on STDOUT.
     WaitForQuiet {
         /// Number of seconds the network must stop moving for.
-        #[clap(long, default_value = "20")]
+        #[clap(long, env = "MC_PERIOD", default_value = "20")]
         period: u64,
 
         /// Wait for quiet at some block index greater than this number.
         /// For example, of `--beyond-block=19` is the argument, then this
         /// command will block until the network is quiet and block
         /// index 20 has appeared.
-        #[clap(long)]
+        #[clap(long, env = "MC_BEYOND_BLOCK")]
         beyond_block: Option<u64>,
     },
 }
@@ -82,25 +85,29 @@ fn main() {
         .map(|uri| {
             let ch =
                 ChannelBuilder::default_channel_builder(env.clone()).connect_to_uri(uri, &logger);
-            BlockchainApiClient::new(ch)
+            (uri.clone(), BlockchainApiClient::new(ch))
         })
         .collect();
 
     match config.tool_command {
         ToolCommand::Status => {
-            for conn in &blockchain_conns {
+            for (uri, conn) in &blockchain_conns {
                 let last_block_info = conn
                     .get_last_block_info(&Empty::new())
                     .expect("get last block info");
 
                 let block_info = BlockInfo::from(last_block_info);
 
-                println!("{}", to_string_pretty(&block_info).expect("json error"))
+                println!(
+                    "{}: {}",
+                    uri,
+                    to_string_pretty(&block_info).expect("json error")
+                )
             }
         }
         ToolCommand::WaitForBlock { index, period } => loop {
             let mut needs_retry = false;
-            for conn in &blockchain_conns {
+            for (uri, conn) in &blockchain_conns {
                 match conn.get_last_block_info(&Empty::new()) {
                     Ok(last_block_info) => {
                         if last_block_info.index < index {
@@ -108,7 +115,7 @@ fn main() {
                         }
                     }
                     Err(err) => {
-                        eprintln!("get_last_block_info(): {}", err);
+                        eprintln!("{}: get_last_block_info(): {}", uri, err);
                         needs_retry = true;
                     }
                 };
@@ -127,7 +134,7 @@ fn main() {
 
             let last_block_index = loop {
                 let mut was_updated = false;
-                for conn in &blockchain_conns {
+                for (uri, conn) in &blockchain_conns {
                     match conn.get_last_block_info(&Empty::new()) {
                         Ok(last_block_info) => {
                             if last_block_index != Some(last_block_info.index) {
@@ -136,7 +143,7 @@ fn main() {
                             }
                         }
                         Err(err) => {
-                            eprintln!("get_last_block_info(): {}", err);
+                            eprintln!("{}: get_last_block_info(): {}", uri, err);
                             was_updated = true;
                         }
                     };
