f9f724795ed5e65e76a2a6970a1703a58de9af68
wjuan-mob <william@mobilecoin.com>
2022-10-04 18:45:21 -0400

    Feature/cherry pick tob 4 (#2651)
    * Add mint config tx duplicate nonce across tokens test
    
    (cherry picked from commit b4fdc016897f72a073180dc93d4cba080e39ec8e)
    
    * Filter nonces in combine mint config tx across tokens
    
    (cherry picked from commit 548a07db7d7634c3ebcc41a6509a669061b926d8)
    
    * Add test for duplicate nonce across token in mint tx combine
    
    * Fix duplicate nonce across multiple tokens in mint tx combine
    
    (cherry picked from commit 410c4dd68e3daf6120089996d1114b872d2f7a5d)
    
    * Remove unnecessary code from test.
    
    (cherry picked from commit 5e8c8e27af2080ad6e77e0c7325c263055535ed4)
    
    * Update check mint tx nonce to take token_id
    
    (cherry picked from commit 5b7909d96f3936ee7f7968096f4d1e72032c249c)
    
    * Update write mint tx to store by token_id and nonce. Add test for duplicate nonce.
    
    (cherry picked from commit d42a3557cf93ccce29dd9cfb5b94fc6521a2b29a)
    
    * Update check mint tx nonce to include token_id.
    
    (cherry picked from commit 34f1c29371f077d062ee3d655be57bad6f79f59c)
    
    * Fix comments.
    
    (cherry picked from commit 9755feaaf9bd8d286853873b96be914147c64cbe)
    
    * Lint.
    
    (cherry picked from commit e567e9c549c77d786fc1f6c2b80ed502eadcc4ca)
    
    * Add migration code for mint stores.
    
    (cherry picked from commit 05fc8e8498b619ea3a28ea494fd74a19b020e911)
    
    * Lint
    
    (cherry picked from commit b5165c789247b9b10ef210772364cee3a7d1c160)
    
    * Apply suggestions from code review
    
    Co-authored-by: Eran Rundstein <eran@rundste.in>
    (cherry picked from commit 6ad7aaf0591f22b129a25a942b30ccb42f46ed44)
    
    * Update write_validated_mint_config_txs to take token_id as well as nonce for the key.
    
    (cherry picked from commit 989bda781eb31d54a6b1d4ef2b1edd929231411f)
    
    * Fix failing test due to change from master to v2.
    
    * Lint.
    
    * Bring in change from master to get consensus common pb2 to build.

--- ledger/migration/src/lib.rs
+++ ledger/migration/src/lib.rs
@@ -110,7 +110,29 @@ pub fn migrate(ledger_db_path: impl AsRef<Path>, logger: &Logger) {
                 );
                 db_txn.commit().expect("Failed committing transaction");
             }
+            // Version 2022_09_21 came after 2022_02_22 and introduced nonces unique to token_ids.
+            Err(MetadataStoreError::VersionIncompatible(2022_02_22, _)) => {
+                log::info!(
+                    logger,
+                    "Ledger db migrating from version 2022_02_22 to 2022_09_21..."
+                );
+                MintConfigStore::create(&env).expect("Failed creating MintConfigStore");
+                MintTxStore::create(&env).expect("Failed creating MintTxStore");
 
+                update_mint_stores_to_store_nonce_with_token_id(&env, logger)
+                    .expect("Failed backfilling nonces");
+
+                let mut db_txn = env.begin_rw_txn().expect("Failed starting rw transaction");
+                metadata_store
+                    .set_version(&mut db_txn, 2022_09_21)
+                    .expect("Failed setting metadata version");
+                log::info!(
+                    logger,
+                    "Ledger db migration complete, now at version: {:?}",
+                    metadata_store.get_version(&db_txn),
+                );
+                db_txn.commit().expect("Failed committing transaction");
+            }
             // Don't know how to migrate.
             Err(err) => {
                 panic!("Error while migrating: {:?}", err);
@@ -248,6 +270,64 @@ fn backfill_empty_mint_stores(env: &Environment, logger: &Logger) -> Result<(), 
             log::info!(
                 logger,
                 "Backfilling empty mint stores: {}% complete",
+                percents
+            );
+        }
+    }
+    Ok(db_txn.commit()?)
+}
+
+/// A utility function for converting nonces stored in mint_config_store and
+/// mint_tx_store to include token_id.
+fn update_mint_stores_to_store_nonce_with_token_id(
+    env: &Environment,
+    logger: &Logger,
+) -> Result<(), Error> {
+    // Open pre-existing databases that has data we need.
+    let mint_config_store = MintConfigStore::new(env)?;
+    let mint_tx_store = MintTxStore::new(env)?;
+    let counts_db = env.open_db(Some(COUNTS_DB_NAME))?;
+    let mut db_txn = env.begin_rw_txn()?;
+
+    let num_blocks = key_bytes_to_u64(db_txn.get(counts_db, &NUM_BLOCKS_KEY)?);
+    log::info!(
+        logger,
+        "Mint config store has {:?}",
+        mint_config_store.get_active_mint_configs_map(&db_txn)?;
+    );
+
+    let mut percents: u64 = 0;
+    for block_index in 0..num_blocks {
+        let block_index_bytes = u64_to_key_bytes(block_index);
+        let validated_mint_configs =
+            mint_config_store.get_validated_mint_config_txs_by_block_index(block_index, &db_txn)?;
+        for validated_mint_config_tx in validated_mint_configs {
+            let mint_config_tx = &validated_mint_config_tx.mint_config_tx;
+
+            MintConfigStore::check_mint_config(mint_config_tx)?;
+
+            mint_config_store.write_block_index_by_nonce_and_token_id(
+                mint_config_tx,
+                &mut db_txn,
+                block_index_bytes,
+            )?;
+        }
+        let mint_txs = mint_tx_store.get_mint_txs_by_block_index(block_index, &db_txn)?;
+        for mint_tx in mint_txs {
+            mint_tx_store.write_block_index_by_mint_tx_nonce_and_token_id(
+                &mint_tx,
+                &mut db_txn,
+                block_index_bytes,
+            )?;
+        }
+
+        // Throttled logging.
+        let new_percents = block_index * 100 / num_blocks;
+        if new_percents != percents {
+            percents = new_percents;
+            log::info!(
+                logger,
+                "Backfilling dropped nonce mint stores: {}% complete",
                 percents
             );
         }
