8e8ae53a08e03e756c74264af01961cf47de585a
Andrew Wygle <andrew@mobilecoin.com>
2022-10-23 23:58:47 -0700

    Plumb together integration test w/ no proxy

--- fog/ledger/server/tests/router_integration.rs
+++ fog/ledger/server/tests/router_integration.rs
@@ -2,23 +2,31 @@
 use mc_common::logger;
 use mc_common::time::SystemTimeProvider;
 use mc_common::logger::{log, Logger};
-use mc_fog_ledger_server::{KeyImageStoreServer, LedgerStoreConfig};
+use mc_fog_ledger_server::{KeyImageStoreServer, LedgerStoreConfig, KeyImageRouterServer, LedgerRouterConfig};
 
-use mc_fog_uri::KeyImageStoreUri;
+use mc_fog_api::ledger_grpc::KeyImageStoreApiClient;
+
+use mc_fog_uri::{KeyImageStoreUri, FogLedgerUri};
 //use mc_util_uri::AdminUri;
 //use mc_attest_core::ProviderId;
+use mc_attest_verifier::{Verifier, MrSignerVerifier, DEBUG_ENCLAVE};
 use mc_util_uri::ConnectionUri;
+use mc_util_grpc::{GrpcRetryConfig, ConnectionUriGrpcioChannel};
 use mc_fog_test_infra::get_enclave_path;
 use mc_fog_ledger_enclave::LedgerSgxEnclave;
+use mc_fog_ledger_connection::FogKeyImageGrpcClient;
 use mc_ledger_db::test_utils::recreate_ledger_db;
 use mc_watcher::watcher_db::WatcherDB;
 use std::path::PathBuf;
 use std::str::FromStr;
+use std::sync::Arc;
 use tempdir::TempDir;
 use url::Url;
+use grpcio::ChannelBuilder;
 //use core::time::Duration;
 
 const TEST_URL: &str = "http://www.my_url1.com";
+const CHAIN_ID: &str = "local";
 
 fn setup_watcher_db(path: PathBuf, logger: Logger) -> WatcherDB {
     let url = Url::parse(TEST_URL).unwrap();
@@ -31,8 +39,8 @@ fn setup_watcher_db(path: PathBuf, logger: Logger) -> WatcherDB {
 // Test that a fog ledger connection is able to get valid merkle proofs by
 fn create_store_config(store_uri: &KeyImageStoreUri, omap_capacity: u64) -> LedgerStoreConfig {
     LedgerStoreConfig {
-        chain_id: "local".to_string(),
-        client_responder_id: store_uri.responder_id().expect("Couldn't get responder ID for router"),
+        chain_id: CHAIN_ID.to_string(),
+        client_responder_id: store_uri.responder_id().expect("Couldn't get responder ID for store"),
         client_listen_uri: store_uri.clone(),
         ledger_db: Default::default(),
         watcher_db: Default::default(),
@@ -45,8 +53,9 @@ fn create_store_config(store_uri: &KeyImageStoreUri, omap_capacity: u64) -> Ledg
     }
 }
 
-fn create_stores(omap_capacity: u64, store_count: usize, logger: Logger) -> Vec<KeyImageStoreServer> {
+fn create_stores(omap_capacity: u64, store_count: usize, grpc_env: Arc<grpcio::Environment>, logger: Logger) -> (Vec<KeyImageStoreServer>, Vec<KeyImageStoreApiClient>) {
     let mut stores = vec![];
+    let mut store_clients = vec![];
     for _ in 0..store_count {
         let port = portpicker::pick_unused_port().expect("couldn't get unused port");
         let uri = KeyImageStoreUri::from_str(&format!("insecure-key-image-store://127.0.0.1:{}", port)).unwrap();
@@ -76,8 +85,66 @@ fn create_stores(omap_capacity: u64, store_count: usize, logger: Logger) -> Vec<
         );
         
         stores.push(store);
+        
+        let store_client = KeyImageStoreApiClient::new(
+            ChannelBuilder::default_channel_builder(grpc_env.clone())
+                .connect_to_uri(&uri, &logger),
+        );
+        
+        store_clients.push(store_client);
     }
-    unimplemented!();
+    
+    (stores, store_clients)
+}
+
+const GRPC_RETRY_CONFIG: GrpcRetryConfig = GrpcRetryConfig {
+    grpc_retry_count: 3,
+    grpc_retry_millis: 20,
+};
+
+fn create_router(omap_capacity: u64, shards: Vec<KeyImageStoreApiClient>, grpc_env: Arc<grpcio::Environment>, logger: Logger) -> (KeyImageRouterServer, FogKeyImageGrpcClient) {
+    let port = portpicker::pick_unused_port().expect("couldn't get unused port");
+    let uri = FogLedgerUri::from_str(&format!("insecure-fog-ledger://127.0.0.1:{}", port)).unwrap();
+
+    let config = LedgerRouterConfig {
+        client_responder_id: uri.responder_id().expect("Couldn't get responder ID for router"),
+        client_listen_uri: uri.clone(),
+        omap_capacity,
+    };
+
+    let enclave = LedgerSgxEnclave::new(
+        get_enclave_path(mc_fog_ledger_enclave::ENCLAVE_FILE),
+        &config.client_responder_id.clone(),
+        config.omap_capacity,
+        logger.clone(),
+    );
+
+    let router = KeyImageRouterServer::new(
+        config,
+        enclave,
+        shards,
+        logger.clone()
+    );
+
+    let mut mr_signer_verifier =
+        MrSignerVerifier::from(mc_fog_ledger_enclave_measurement::sigstruct());
+    mr_signer_verifier.allow_hardening_advisories(
+        mc_fog_ledger_enclave_measurement::HARDENING_ADVISORIES,
+    );
+
+    let mut verifier = Verifier::default();
+    verifier.mr_signer(mr_signer_verifier).debug(DEBUG_ENCLAVE);
+
+    let router_client = FogKeyImageGrpcClient::new(
+        CHAIN_ID.to_string(),
+        uri,
+        GRPC_RETRY_CONFIG,
+        verifier,
+        grpc_env.clone(),
+        logger,
+    );
+    
+    (router, router_client)
 }
 
 #[test]
@@ -86,5 +153,15 @@ fn router_integration_test() {
         logger::create_app_logger(logger::o!());
     log::info!(logger, "test");
     
-    let _stores = create_stores(1000, 5, logger);
+    let omap_capacity = 1000;
+    let num_stores = 5;
+    
+    let grpc_env = Arc::new(grpcio::EnvBuilder::new().build());
+    
+    let (_stores, store_clients) = create_stores(omap_capacity, num_stores, grpc_env.clone(), logger.clone());
+    // router talks directly to stores for these tests
+    // shard tests are done in CI/CD
+    let (_router, _router_client) = create_router(omap_capacity, store_clients, grpc_env, logger);
+    
+    unimplemented!();
 }
