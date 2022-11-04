1fa2d36aacdd12eadb2f4c9c22dbc15ddec88f98
Chris Beck <beck.ct@gmail.com>
2022-11-04 10:53:26 -0600

    more tests

--- transaction/extra/tests/verifier.rs
+++ transaction/extra/tests/verifier.rs
@@ -2,7 +2,7 @@
 
 //! Tests of the streaming verifier
 
-use mc_account_keys::AccountKey;
+use mc_account_keys::{AccountKey, ShortAddressHash};
 use mc_crypto_keys::{RistrettoPrivate, RistrettoPublic};
 use mc_crypto_ring_signature_signer::NoKeysRingSigner;
 use mc_fog_report_validation_test_utils::{FullyValidatedFogPubkey, MockFogResolver};
@@ -10,9 +10,9 @@ use mc_transaction_builder::test_utils::get_unsigned_transaction;
 use mc_transaction_core::{
     constants::{MAX_INPUTS, MAX_OUTPUTS, RING_SIZE},
     tx::Tx,
-    BlockVersion,
+    Amount, BlockVersion, TokenId,
 };
-use mc_transaction_extra::{verify_tx_summary, UnsignedTx};
+use mc_transaction_extra::{verify_tx_summary, TransactionEntity, UnsignedTx};
 use mc_util_from_random::FromRandom;
 use mc_util_serial::encode;
 use rand::{rngs::StdRng, SeedableRng};
@@ -21,7 +21,9 @@ use std::collections::BTreeMap;
 
 // Get an unsigned Tx and the sender account keys with the maximum allowed size
 // right now
-fn get_current_max_size_transaction(rng: &mut impl CryptoRngCore) -> (UnsignedTx, AccountKey) {
+fn get_current_max_size_transaction(
+    rng: &mut impl CryptoRngCore,
+) -> (UnsignedTx, AccountKey, AccountKey) {
     let sender = AccountKey::random_with_fog(rng);
     let recipient = AccountKey::random_with_fog(rng);
 
@@ -54,12 +56,15 @@ fn get_current_max_size_transaction(rng: &mut impl CryptoRngCore) -> (UnsignedTx
         )
         .unwrap(),
         sender,
+        recipient,
     )
 }
 
 // Get an unsigned Tx and the sender account keys with the minimum possible size
 // right now
-fn get_current_min_size_transaction(rng: &mut impl CryptoRngCore) -> (UnsignedTx, AccountKey) {
+fn get_current_min_size_transaction(
+    rng: &mut impl CryptoRngCore,
+) -> (UnsignedTx, AccountKey, AccountKey) {
     let sender = AccountKey::random_with_fog(rng);
     let recipient = AccountKey::random_with_fog(rng);
 
@@ -92,35 +97,15 @@ fn get_current_min_size_transaction(rng: &mut impl CryptoRngCore) -> (UnsignedTx
         )
         .unwrap(),
         sender,
+        recipient,
     )
-}
-
-#[test]
-fn test_max_size_tx_summary_verification() {
-    let mut rng: StdRng = SeedableRng::from_seed([1u8; 32]);
-
-    let (unsigned_tx, sender) = get_current_max_size_transaction(&mut rng);
-    let (signing_data, tx_summary, tx_summary_unblinding_data, extended_message_digest) =
-        unsigned_tx.get_signing_data(&mut rng).unwrap();
-
-    let (mlsag_signing_digest, _report) = verify_tx_summary(
-        &extended_message_digest.0.try_into().unwrap(),
-        &tx_summary,
-        &tx_summary_unblinding_data,
-        *sender.view_private_key(),
-    )
-    .unwrap();
-    assert_eq!(
-        &mlsag_signing_digest[..],
-        &signing_data.mlsag_signing_digest[..]
-    );
 }
 
 #[test]
 fn test_max_size_tx_payload_sizes() {
     let mut rng: StdRng = SeedableRng::from_seed([1u8; 32]);
 
-    let (unsigned_tx, _sender) = get_current_max_size_transaction(&mut rng);
+    let (unsigned_tx, _sender, _recipient) = get_current_max_size_transaction(&mut rng);
     let (signing_data, tx_summary, tx_summary_unblinding_data, _extended_message_digest) =
         unsigned_tx.get_signing_data(&mut rng).unwrap();
     let signature_rct = signing_data
@@ -162,7 +147,7 @@ fn test_max_size_tx_payload_sizes() {
 fn test_min_size_tx_payload_sizes() {
     let mut rng: StdRng = SeedableRng::from_seed([1u8; 32]);
 
-    let (unsigned_tx, _sender) = get_current_min_size_transaction(&mut rng);
+    let (unsigned_tx, _sender, _recipient) = get_current_min_size_transaction(&mut rng);
     let (signing_data, tx_summary, tx_summary_unblinding_data, _extended_message_digest) =
         unsigned_tx.get_signing_data(&mut rng).unwrap();
     let signature_rct = signing_data
@@ -198,4 +183,74 @@ fn test_min_size_tx_payload_sizes() {
 
     let tx_in_summary_unblinding_wire = encode(&tx_summary_unblinding_data.inputs[0]);
     assert_eq!(tx_in_summary_unblinding_wire.len(), 45);
+}
+
+#[test]
+fn test_max_size_tx_summary_verification() {
+    let mut rng: StdRng = SeedableRng::from_seed([1u8; 32]);
+
+    let (unsigned_tx, sender, recipient) = get_current_max_size_transaction(&mut rng);
+    let (signing_data, tx_summary, tx_summary_unblinding_data, extended_message_digest) =
+        unsigned_tx.get_signing_data(&mut rng).unwrap();
+
+    let (mlsag_signing_digest, report) = verify_tx_summary(
+        &extended_message_digest.0.try_into().unwrap(),
+        &tx_summary,
+        &tx_summary_unblinding_data,
+        *sender.view_private_key(),
+    )
+    .unwrap();
+    assert_eq!(
+        &mlsag_signing_digest[..],
+        &signing_data.mlsag_signing_digest[..]
+    );
+
+    let recipient_hash = ShortAddressHash::from(&recipient.default_subaddress());
+    let balance_changes: Vec<_> = report.balance_changes.iter().collect();
+    assert_eq!(
+        balance_changes,
+        vec![
+            (&(TransactionEntity::Ourself, TokenId::from(0)), &-16000),
+            (
+                &(TransactionEntity::Address(recipient_hash), TokenId::from(0)),
+                &160
+            )
+        ]
+    );
+    assert_eq!(report.network_fee, Amount::new(15840, TokenId::from(0)));
+}
+
+#[test]
+fn test_min_size_tx_summary_verification() {
+    let mut rng: StdRng = SeedableRng::from_seed([2u8; 32]);
+
+    let (unsigned_tx, sender, recipient) = get_current_min_size_transaction(&mut rng);
+    let (signing_data, tx_summary, tx_summary_unblinding_data, extended_message_digest) =
+        unsigned_tx.get_signing_data(&mut rng).unwrap();
+
+    let (mlsag_signing_digest, report) = verify_tx_summary(
+        &extended_message_digest.0.try_into().unwrap(),
+        &tx_summary,
+        &tx_summary_unblinding_data,
+        *sender.view_private_key(),
+    )
+    .unwrap();
+    assert_eq!(
+        &mlsag_signing_digest[..],
+        &signing_data.mlsag_signing_digest[..]
+    );
+
+    let recipient_hash = ShortAddressHash::from(&recipient.default_subaddress());
+    let balance_changes: Vec<_> = report.balance_changes.iter().collect();
+    assert_eq!(
+        balance_changes,
+        vec![
+            (&(TransactionEntity::Ourself, TokenId::from(0)), &-1000),
+            (
+                &(TransactionEntity::Address(recipient_hash), TokenId::from(0)),
+                &10
+            )
+        ]
+    );
+    assert_eq!(report.network_fee, Amount::new(990, TokenId::from(0)));
 }
