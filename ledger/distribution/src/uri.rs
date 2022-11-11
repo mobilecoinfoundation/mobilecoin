a3abd1a43bafdf63616f602d49c0833bf3d0257d
Brian <brian.william.corbin@gmail.com>
2022-11-10 18:36:55 -0800

    fix lint

--- ledger/distribution/src/uri.rs
+++ ledger/distribution/src/uri.rs
@@ -76,7 +76,7 @@ impl FromStr for Uri {
                     }
                 });
 
-                let region = region_param.map_or(Region::default, |param| {
+                let region = region_param.map_or_else(Region::default, |param| {
                     Region::from_str(&param).unwrap_or(Region::Custom {
                         name: "custom".to_string(),
                         endpoint: param,
