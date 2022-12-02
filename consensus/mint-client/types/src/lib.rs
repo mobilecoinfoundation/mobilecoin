de67506feba244a0da30b2b4a0fab5773ea6952e
Eran Rundstein <eran@rundste.in>
2022-12-02 12:30:11 -0800

    fmt

--- consensus/mint-client/types/src/lib.rs
+++ consensus/mint-client/types/src/lib.rs
@@ -1,10 +1,11 @@
 // Copyright (c) 2018-2022 The MobileCoin Foundation
 
 //! Data types used by the mint client.
-//! Provided in a separate crate in order to allow usage by other apps while maintaining compatibility with the mint client.
+//! Provided in a separate crate in order to allow usage by other apps while
+//! maintaining compatibility with the mint client.
 
-mod tx_file;
 mod mint_config_tx_file;
+mod tx_file;
 
 pub use mint_config_tx_file::{MintConfig, MintConfigTxFile};
 pub use tx_file::TxFile;
