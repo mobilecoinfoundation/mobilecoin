71bcab029f131795b21aa5038eeabcfde0344ba6
Andrew Wygle <andrew@mobilecoin.com>
2022-12-02 16:11:40 -0800

    Key Image Router Server + Binary

--- fog/ledger/server/src/lib.rs
+++ fog/ledger/server/src/lib.rs
@@ -18,7 +18,7 @@ mod router_admin_service;
 use mc_util_metrics::ServiceMetrics;
 
 pub use block_service::BlockService;
-pub use config::LedgerServerConfig;
+pub use config::{LedgerRouterConfig, LedgerServerConfig};
 pub use key_image_service::KeyImageService;
 pub use merkle_proof_service::MerkleProofService;
 pub use server::LedgerServer;
