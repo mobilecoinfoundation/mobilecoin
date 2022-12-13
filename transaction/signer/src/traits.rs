dd4e98145c1de48b77c102c10f3d537af6aced72
ryan kurte <ryan@kurte.nz>
2022-12-14 10:05:01 +1300

    update memo hmac signer trait

--- transaction/signer/src/traits.rs
+++ transaction/signer/src/traits.rs
@@ -94,10 +94,34 @@ pub trait MemoHmacSigner {
 
     /// Compute the HMAC signature for the provided memo and target address
     fn compute_memo_hmac_sig(
-        &mut self,
+        &self,
+        sender_subaddress_index: u64,
         tx_public_key: &TxOutPublic,
         target_subaddress: PublicSubaddress,
         memo_type: &[u8; 2],
         memo_data_sans_hmac: &[u8; 48],
     ) -> Result<[u8; 16], Self::Error>;
+}
+
+/// Memo signer impl for reference types
+impl <T: MemoHmacSigner> MemoHmacSigner for &T {
+    type Error = <T as MemoHmacSigner>::Error;
+
+    fn compute_memo_hmac_sig(
+        &self,
+        sender_subaddress_index: u64,
+        tx_public_key: &TxOutPublic,
+        target_subaddress: PublicSubaddress,
+        memo_type: &[u8; 2],
+        memo_data_sans_hmac: &[u8; 48],
+    ) -> Result<[u8; 16], Self::Error> {
+        <T as MemoHmacSigner>::compute_memo_hmac_sig(
+            self,
+            sender_subaddress_index,
+            tx_public_key,
+            target_subaddress,
+            memo_type,
+            memo_data_sans_hmac,
+        )
+    }
 }
