76b8e170f9d2c3f0f8f8d64aea571d7d901ba4bd
Emily C <gyrocoder@gmail.com>
2022-12-14 12:49:29 -0500

    Ledger enclave support for router and store (#2896)
    * Pulling changes in from milliec/ledger-router-dev
    
    * Run cargo fmt
    
    * Making requested changes and clarifications to Fog ledger router comments.
    
    * Additional comment fixes
    
    * Fix CI lint
    
    * PR feedback nits
    
    Co-authored-by: Andrew Wygle <andrew@mobilecoin.com>

--- fog/ledger/enclave/trusted/src/lib.rs
+++ fog/ledger/enclave/trusted/src/lib.rs
@@ -56,11 +56,35 @@ pub fn ecall_dispatcher(inbuf: &[u8]) -> Result<Vec<u8>, sgx_status_t> {
             serialize(&ENCLAVE.get_outputs_data(resp, client))
         }
         // Check Key image
-        EnclaveCall::CheckKeyImages(req, untrusted_keyimagequery_response) => {
-            serialize(&ENCLAVE.check_key_images(req, untrusted_keyimagequery_response))
+        EnclaveCall::CheckKeyImages(req, response) => {
+            serialize(&ENCLAVE.check_key_images(req, response))
         }
         // Add Key Image Data
         EnclaveCall::AddKeyImageData(records) => serialize(&ENCLAVE.add_key_image_data(records)),
+
+        // Router / Store system
+        // Router-side
+        EnclaveCall::LedgerStoreInit(responder_id) => {
+            serialize(&ENCLAVE.ledger_store_init(responder_id))
+        }
+        EnclaveCall::LedgerStoreConnect(responder_id, client_auth_response) => {
+            serialize(&ENCLAVE.ledger_store_connect(responder_id, client_auth_response))
+        }
+        EnclaveCall::DecryptAndSealQuery(client_query) => {
+            serialize(&ENCLAVE.decrypt_and_seal_query(client_query))
+        }
+        EnclaveCall::CreateMultiKeyImageStoreQueryData(msg) => {
+            serialize(&ENCLAVE.create_multi_key_image_store_query_data(msg))
+        }
+        EnclaveCall::CollateQueryResponses(sealed_query, shard_query_responses) => {
+            serialize(&ENCLAVE.collate_shard_query_responses(sealed_query, shard_query_responses))
+        }
+        EnclaveCall::CheckKeyImageStore(req, response) => {
+            serialize(&ENCLAVE.check_key_image_store(req, response))
+        }
+        EnclaveCall::FrontendAccept(auth_message) => {
+            serialize(&ENCLAVE.frontend_accept(auth_message))
+        }
     }
     .or(Err(sgx_status_t::SGX_ERROR_UNEXPECTED))
 }
