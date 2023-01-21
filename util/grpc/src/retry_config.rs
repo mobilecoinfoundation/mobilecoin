4cf14bd7927d84140140eedd305dfd7c5df4989d
Jason Greathouse <jgreat@jgreat.me>
2023-01-21 14:03:55 -0800

    fix: make retries back off using fibonacci sequence with max and limit retry.

--- util/grpc/src/retry_config.rs
+++ util/grpc/src/retry_config.rs
@@ -8,32 +8,50 @@ use std::time::Duration;
 
 /// An object which represents a retry policy for retriable errors for a grpc
 /// connection
+/// Use fibonacci back off. 24 retries starting at 100ms with a max back off of
+/// 60000ms is about 10 min.
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Parser, Serialize)]
 pub struct GrpcRetryConfig {
     /// How many times to retry when we get retriable errors (grpc connection)
-    #[clap(long, default_value = "3", env = "MC_GRPC_RETRY_COUNT")]
+    #[clap(long, default_value = "24", env = "MC_GRPC_RETRY_COUNT")]
     pub grpc_retry_count: usize,
 
-    /// How long to back off (milliseconds) when we get retriable errors (grpc
-    /// connection)
-    #[clap(long, default_value = "20", env = "MC_GRPC_RETRY_MILLIS")]
-    pub grpc_retry_millis: u64,
+    /// Minimum delay to back off (milliseconds) when we get retriable errors
+    /// (grpc connection)
+    #[clap(long, default_value = "100", env = "MC_GRPC_RETRY_MIN_DELAY_MILLIS")]
+    pub grpc_retry_min_delay_millis: u64,
+
+    /// Maximum delay to back off (milliseconds) when we get retriable errors
+    /// (grpc connection)
+    #[clap(long, default_value = "60000", env = "MC_GRPC_RETRY_MAX_DELAY_MILLIS")]
+    pub grpc_retry_max_delay_millis: u64,
 }
 
 impl Default for GrpcRetryConfig {
     fn default() -> Self {
         Self {
-            grpc_retry_count: 3,
-            grpc_retry_millis: 20,
+            grpc_retry_count: 24,
+            grpc_retry_min_delay_millis: 100,
+            grpc_retry_max_delay_millis: 60000,
         }
     }
 }
 
 impl GrpcRetryConfig {
+    /// Set the max delay for a retry - We don't want to wait forever.
+    pub fn set_max_delay(duration: Duration, max: u64) -> Duration {
+        if duration > Duration::from_millis(max) {
+            Duration::from_millis(max)
+        } else {
+            duration
+        }
+    }
+
     /// Get a duration iterator for use with retry crate based on this config
-    pub fn get_retry_iterator(&self) -> impl Iterator<Item = Duration> {
-        delay::Fixed::from_millis(self.grpc_retry_millis)
+    pub fn get_retry_iterator(&self) -> impl Iterator<Item = Duration> + '_ {
+        delay::Fibonacci::from_millis(self.grpc_retry_min_delay_millis)
             .take(self.grpc_retry_count)
+            .map(|x| GrpcRetryConfig::set_max_delay(x, self.grpc_retry_max_delay_millis))
             .map(delay::jitter)
     }
 
