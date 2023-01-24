46f1d44a7054c044a61623954e9dadacbb1a92d3
awygle <andrew@mobilecoin.com>
2023-01-24 12:33:57 -0800

    Key Image Router Server + Binary (#2921)
    * Ledger enclave support for router and store (#2896)
    
    * Pulling changes in from milliec/ledger-router-dev
    
    * Run cargo fmt
    
    * Making requested changes and clarifications to Fog ledger router comments.
    
    * Additional comment fixes
    
    * Fix CI lint
    
    * PR feedback nits
    
    Co-authored-by: Andrew Wygle <andrew@mobilecoin.com>
    
    * Fog Ledger Router Admin service
    
    * Sort itertools properly in Cargo.toml
    
    * Key Image Router Server + Binary
    
    * Update router config for parameterized retries
    
    * Remove dead_code declaration on router service constructor
    
    * Changes due to rebase
    
    * normalize naming - ledger router, key image store
    
    * Linting fixes
    
    * Accept code review suggestions
    
    * Updates for GRPCIO 0.12
    
    * Remove some unwraps in ledger_router binary
    
    Co-authored-by: Emily C <gyrocoder@gmail.com>

--- fog/ledger/server/src/lib.rs
+++ fog/ledger/server/src/lib.rs
@@ -6,18 +6,19 @@ mod config;
 mod counters;
 mod db_fetcher;
 mod error;
-mod key_image_router_service;
 mod key_image_service;
 mod merkle_proof_service;
 mod router_admin_service;
 mod router_handlers;
+mod router_server;
+mod router_service;
 mod server;
 mod untrusted_tx_out_service;
 
 use mc_util_metrics::ServiceMetrics;
 
 pub use block_service::BlockService;
-pub use config::LedgerServerConfig;
+pub use config::{LedgerRouterConfig, LedgerServerConfig};
 pub use key_image_service::KeyImageService;
 pub use merkle_proof_service::MerkleProofService;
 pub use server::LedgerServer;
@@ -26,3 +27,4 @@ pub use untrusted_tx_out_service::UntrustedTxOutService;
 lazy_static::lazy_static! {
     pub static ref SVC_COUNTERS: ServiceMetrics = ServiceMetrics::new_and_registered("fog_ledger");
 }
+pub use router_server::LedgerRouterServer;
