da5a118589290e2d90b1bf95822342c9249bd51c
Chris Beck <beck.ct@gmail.com>
2023-02-01 18:32:58 -0700

    make a way to store hardening advisory data with enclave measurements and load it
    This is meant to address comments on PR #3078, and allow that a client
    can load multiple MRENCLAVE measurements on startup and also apply different
    sets of hardening advisories to these as appropriate.
    
    The idea is that there is a file named e.g. `trusted-measurements.json`
    which contains a json schema organizing enclave measurements, their respective
    lists of mitigated hardening advisories, and what release they are associated
    to, as well as any additional metadata for setting up MRSIGNER verifiers,
    if that is desired.
    
    Clients like mobilecoind or full-service can load this on startup to
    configure their verifiers. SDKs could likely bake this in as a string literal,
    or take it as a parameter.
    
    This is more correct than what we are currently doing with passing CSS
    files around, and it makes it easy to trust "previous release and next release".
    
    If there is approval we will try to redevelop PR #3078 based on this.

---
+++ attest/verifier/config/README.md
@@ -0,0 +1,80 @@
+attest-verifier-config
+======================
+
+This crate specifies and implements a method for configuring an attestation
+verifier based on sigstructs and other configuration data, which it finds using
+a search path which is given to it. This search path is called the "attestation
+trust root search path".
+
+This is loosely based on how OS'es like linux often have a designated path where SSL trust roots are stored,
+e.g. `/etc/ssl/certs`, `/usr/local/share/certs`. This is a bit different in that,
+we're not assuming that there's an OS level path for mobilecoin attestation roots,
+rather it's expected that when a mobilecoin client installs itself, somewhere in its installation
+path it would install this trust root file. The app would then load that data using the code in this crate.
+So, these trust roots would be private to an installation of the app.
+
+Format
+------
+
+We propose that the verifier config is stored in a file called e.g. `trusted-measurements.json`.
+
+The file has the following (example) schema:
+
+```
+{
+    "v3": {
+       "consensus": {
+           "MRENCLAVE": "207c9705bf640fdb960034595433ee1ff914f9154fbe4bc7fc8a97e912961e5c",
+           "mitigated_hardening_advisories": ["INTEL-SA-00334", "INTEL-SA-00615"]
+       },
+       "fog-ingest": {
+           "MRENCLAVE": "3370f131b41e5a49ed97c4188f7a976461ac6127f8d222a37929ac46b46d560e",
+           "mitigated_hardening_advisories": ["INTEL-SA-00334", "INTEL-SA-00615"]
+       },
+       "fog-ledger": {
+           "MRENCLAVE": "fd4c1c82cca13fa007be15a4c90e2b506c093b21c2e7021a055cbb34aa232f3f",
+           "mitigated_hardening_advisories": ["INTEL-SA-00334", "INTEL-SA-00615"],
+       },
+       "fog-view": {
+           "MRENCLAVE": "dca7521ce4564cc2e54e1637e533ea9d1901c2adcbab0e7a41055e719fb0ff9d",
+           "mitigated_hardening_advisories": ["INTEL-SA-00334", "INTEL-SA-00615"],
+       }
+    },
+    "v4": {
+       "consensus": {
+           "MRENCLAVE": "e35bc15ee92775029a60a715dca05d310ad40993f56ad43bca7e649ccc9021b5",
+           "mitigated_hardening_advisories": ["INTEL-SA-00334", "INTEL-SA-00615", "INTEL-SA-00657]
+       },
+       "fog-ingest": {
+           "MRENCLAVE": "a8af815564569aae3558d8e4e4be14d1bcec896623166a10494b4eaea3e1c48c",
+           "mitigated_hardening_advisories": ["INTEL-SA-00334", "INTEL-SA-00615", "INTEL-SA-00657]
+       },
+       "fog-ledger": {
+           "MRENCLAVE": "da209f4b24e8f4471bd6440c4e9f1b3100f1da09e2836d236e285b274901ed3b",
+           "mitigated_hardening_advisories": ["INTEL-SA-00334", "INTEL-SA-00615", "INTEL-SA-00657]
+       },
+       "fog-view": {
+           "MRENCLAVE": "8c80a2b95a549fa8d928dd0f0771be4f3d774408c0f98bf670b1a2c390706bf3",
+           "mitigated_hardening_advisories": ["INTEL-SA-00334", "INTEL-SA-00615", "INTEL-SA-00657]
+       }
+    }
+}
+```
+
+Here, the outermost layer is a list of enclave versions that are trusted. Usually, clients should trust
+the current release and the next release. These measurements can be checked against the mobilecoin github
+releases page: https://github.com/mobilecoinfoundation/mobilecoin/releases.
+
+The use of `MRSIGNER` is also supported, but the product svn (security version number) must be supplied.
+
+```
+...
+  "v3": {
+    "fog-ingest": {
+        "MRSIGNER": "2c1a561c4ab64cbc04bfa445cdf7bed9b2ad6f6b04d38d3137f3622b29fdb30e",
+        "product_svn": 5,
+        "mitigated_hardening_advisories": ["INTEL-SA-00334", "INTEL-SA-00615"],
+    }
+  }
+}
+```
---
+++ attest/verifier/config/src/lib.rs
@@ -0,0 +1,453 @@
+// Copyright (c) 2018-2022 The MobileCoin Foundation
+
+//! This crate defines a serialization format for collections of enclave
+//! measurements and associated hardening advisories and other data, which can
+//! be used to configure an attestation verifier appropriately.
+
+#![no_std]
+#![deny(missing_docs)]
+#![allow(non_snake_case)]
+
+extern crate alloc;
+
+use alloc::{borrow::ToOwned, collections::BTreeMap, string::String, vec::Vec};
+use displaydoc::Display;
+use mc_attest_core::{MrEnclave, MrSigner};
+use mc_attest_verifier::{MrEnclaveVerifier, MrSignerVerifier, Verifier, DEBUG_ENCLAVE};
+use serde::{Deserialize, Serialize};
+use serde_hex::{SerHex, Strict};
+
+/// Defines a json schema for an individual attestation status verifier.
+/// This is either a MRENCLAVE or MRSIGNER type, and the type is inferred by
+/// the presence of these fields.
+/// Unknown fields are flagged as an error.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(untagged)]
+#[serde(deny_unknown_fields)]
+pub enum StatusVerifierConfig {
+    /// A MRENCLAVE-based status verifier
+    Mrenclave {
+        /// The hex-encoded bytes of the MRENCLAVE measurement. This is
+        /// enclavehash in the .css file
+        #[serde(with = "SerHex::<Strict>")]
+        MRENCLAVE: [u8; 32],
+        /// The list of config advisories that are known to be mitigated in
+        /// software at this enclave revision.
+        #[serde(default)]
+        mitigated_config_advisories: Vec<String>,
+        /// The list of hardening advisories that are known to be mitigated in
+        /// software at this enclave revision.
+        #[serde(default)]
+        mitigated_hardening_advisories: Vec<String>,
+    },
+    /// A MRSIGNER-based status verifier
+    Mrsigner {
+        /// The hex-encoded bytes of the MRSIGNER measurement. This is a digest
+        /// of the modulus in the .css file. Use a tool to see what it is.
+        #[serde(with = "SerHex::<Strict>")]
+        MRSIGNER: [u8; 32],
+        /// The product id that this verifier checks for.
+        product_id: u16,
+        /// The minimum security version number that is considered valid by this
+        /// verifier.
+        minimum_svn: u16,
+        /// The list of config advisories that are known to be mitigated in
+        /// software at this enclave revision.
+        #[serde(default)]
+        mitigated_config_advisories: Vec<String>,
+        /// The list of hardening advisories that are known to be mitigated in
+        /// software at this enclave revision.
+        #[serde(default)]
+        mitigated_hardening_advisories: Vec<String>,
+    },
+}
+
+impl StatusVerifierConfig {
+    /// Build status verifier corresponding to ourself, and add it to a given
+    /// verifier
+    pub fn add_to_verifier(&self, verifier: &mut Verifier) {
+        match self {
+            Self::Mrenclave {
+                MRENCLAVE,
+                mitigated_config_advisories,
+                mitigated_hardening_advisories,
+            } => {
+                let mut mr_enclave_verifier =
+                    MrEnclaveVerifier::new(MrEnclave::from(MRENCLAVE.clone()));
+                for advisory in mitigated_config_advisories.iter() {
+                    mr_enclave_verifier.allow_config_advisory(advisory);
+                }
+                for advisory in mitigated_hardening_advisories.iter() {
+                    mr_enclave_verifier.allow_hardening_advisory(advisory);
+                }
+                verifier.mr_enclave(mr_enclave_verifier);
+            }
+            Self::Mrsigner {
+                MRSIGNER,
+                product_id,
+                minimum_svn,
+                mitigated_config_advisories,
+                mitigated_hardening_advisories,
+            } => {
+                let mut mr_signer_verifier = MrSignerVerifier::new(
+                    MrSigner::from(MRSIGNER.clone()),
+                    (*product_id).into(),
+                    (*minimum_svn).into(),
+                );
+                for advisory in mitigated_config_advisories.iter() {
+                    mr_signer_verifier.allow_config_advisory(advisory);
+                }
+                for advisory in mitigated_hardening_advisories.iter() {
+                    mr_signer_verifier.allow_hardening_advisory(advisory);
+                }
+                verifier.mr_signer(mr_signer_verifier);
+            }
+        }
+    }
+}
+
+/// Defines a json schema for a "trusted-measurements.json" file.
+/// See README.md for example.
+///
+/// The outermost string key of this is the release version number. This is not
+/// interpretted by the software, but could be added as a debug string or
+/// something, and helps with maintenance of the file.
+///
+/// The second string key, within a release, is the name of the enclave that
+/// this is a measurement for.
+///
+/// The VerifierConfig object contains measurement and hardening advisory data
+/// for that enclave at that release.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(transparent)]
+pub struct TrustedMeasurementSet {
+    table: BTreeMap<String, BTreeMap<String, StatusVerifierConfig>>,
+}
+
+impl TrustedMeasurementSet {
+    /// Create a verifier from this measurement set for a given enclave name.
+    pub fn create_verifier(&self, enclave_name: &str) -> Result<Verifier, Error> {
+        let mut count = 0usize;
+
+        let mut verifier = Verifier::default();
+        verifier.debug(DEBUG_ENCLAVE);
+
+        for (_release, measurements) in self.table.iter() {
+            if let Some(measurement) = measurements.get(enclave_name) {
+                // TODO: it would be nice to add the release name in also as like
+                // a debug string somewhere that would show up in error messages
+                // when attestation fails?
+                measurement.add_to_verifier(&mut verifier);
+                count += 1;
+            }
+        }
+
+        if count == 0 {
+            return Err(Error::NoMeasurementsFound(enclave_name.to_owned()));
+        }
+
+        Ok(verifier)
+    }
+}
+
+/// An error which can occur when trying to load attestation trust roots from a
+/// search path
+#[derive(Display, Debug)]
+pub enum Error {
+    /// No measurements found for enclave name "{0}"
+    NoMeasurementsFound(String),
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    use hex_literal::hex;
+
+    const TEST_DATA: &str = r#"{
+    "v3": {
+       "consensus": {
+           "MRENCLAVE": "207c9705bf640fdb960034595433ee1ff914f9154fbe4bc7fc8a97e912961e5c",
+           "mitigated_hardening_advisories": ["INTEL-SA-00334", "INTEL-SA-00615"]
+       },
+       "fog-ingest": {
+           "MRENCLAVE": "3370f131b41e5a49ed97c4188f7a976461ac6127f8d222a37929ac46b46d560e",
+           "mitigated_hardening_advisories": ["INTEL-SA-00334", "INTEL-SA-00615"]
+       },
+       "fog-ledger": {
+           "MRENCLAVE": "fd4c1c82cca13fa007be15a4c90e2b506c093b21c2e7021a055cbb34aa232f3f",
+           "mitigated_hardening_advisories": ["INTEL-SA-00334", "INTEL-SA-00615"]
+       },
+       "fog-view": {
+           "MRENCLAVE": "dca7521ce4564cc2e54e1637e533ea9d1901c2adcbab0e7a41055e719fb0ff9d",
+           "mitigated_hardening_advisories": ["INTEL-SA-00334", "INTEL-SA-00615"]
+       }
+    },
+    "v4": {
+       "consensus": {
+           "MRENCLAVE": "e35bc15ee92775029a60a715dca05d310ad40993f56ad43bca7e649ccc9021b5",
+           "mitigated_hardening_advisories": ["INTEL-SA-00334", "INTEL-SA-00615", "INTEL-SA-00657"]
+       },
+       "fog-ingest": {
+           "MRENCLAVE": "a8af815564569aae3558d8e4e4be14d1bcec896623166a10494b4eaea3e1c48c",
+           "mitigated_hardening_advisories": ["INTEL-SA-00334", "INTEL-SA-00615", "INTEL-SA-00657"]
+       },
+       "fog-ledger": {
+           "MRENCLAVE": "da209f4b24e8f4471bd6440c4e9f1b3100f1da09e2836d236e285b274901ed3b",
+           "mitigated_hardening_advisories": ["INTEL-SA-00334", "INTEL-SA-00615", "INTEL-SA-00657"]
+       },
+       "fog-view": {
+           "MRENCLAVE": "8c80a2b95a549fa8d928dd0f0771be4f3d774408c0f98bf670b1a2c390706bf3",
+           "mitigated_hardening_advisories": ["INTEL-SA-00334", "INTEL-SA-00615", "INTEL-SA-00657"]
+       }
+    }
+}"#;
+
+    #[test]
+    fn test_loading() {
+        let tms: TrustedMeasurementSet = serde_json::from_str(TEST_DATA).unwrap();
+
+        assert_eq!(tms.table.len(), 2);
+        let v3 = tms.table.get("v3").unwrap();
+        assert_eq!(v3.len(), 4);
+        let v3_consensus = v3.get("consensus").unwrap();
+        match v3_consensus {
+            StatusVerifierConfig::Mrenclave {
+                MRENCLAVE,
+                mitigated_config_advisories,
+                mitigated_hardening_advisories,
+            } => {
+                assert_eq!(
+                    MRENCLAVE,
+                    &hex!("207c9705bf640fdb960034595433ee1ff914f9154fbe4bc7fc8a97e912961e5c")
+                );
+                assert_eq!(mitigated_config_advisories, &Vec::<String>::default());
+                assert_eq!(
+                    mitigated_hardening_advisories,
+                    &["INTEL-SA-00334", "INTEL-SA-00615"]
+                );
+            }
+            _ => panic!("unexpected"),
+        };
+        let v3_fog_view = v3.get("fog-view").unwrap();
+        match v3_fog_view {
+            StatusVerifierConfig::Mrenclave {
+                MRENCLAVE,
+                mitigated_config_advisories,
+                mitigated_hardening_advisories,
+            } => {
+                assert_eq!(
+                    MRENCLAVE,
+                    &hex!("dca7521ce4564cc2e54e1637e533ea9d1901c2adcbab0e7a41055e719fb0ff9d")
+                );
+                assert_eq!(mitigated_config_advisories, &Vec::<String>::default());
+                assert_eq!(
+                    mitigated_hardening_advisories,
+                    &["INTEL-SA-00334", "INTEL-SA-00615"]
+                );
+            }
+            _ => panic!("unexpected"),
+        };
+
+        let v4 = tms.table.get("v4").unwrap();
+        assert_eq!(v4.len(), 4);
+        let v4_consensus = v4.get("consensus").unwrap();
+        match v4_consensus {
+            StatusVerifierConfig::Mrenclave {
+                MRENCLAVE,
+                mitigated_config_advisories,
+                mitigated_hardening_advisories,
+            } => {
+                assert_eq!(
+                    MRENCLAVE,
+                    &hex!("e35bc15ee92775029a60a715dca05d310ad40993f56ad43bca7e649ccc9021b5")
+                );
+                assert_eq!(mitigated_config_advisories, &Vec::<String>::default());
+                assert_eq!(
+                    mitigated_hardening_advisories,
+                    &["INTEL-SA-00334", "INTEL-SA-00615", "INTEL-SA-00657"]
+                );
+            }
+            _ => panic!("unexpected"),
+        };
+        let v4_fog_view = v4.get("fog-view").unwrap();
+        match v4_fog_view {
+            StatusVerifierConfig::Mrenclave {
+                MRENCLAVE,
+                mitigated_config_advisories,
+                mitigated_hardening_advisories,
+            } => {
+                assert_eq!(
+                    MRENCLAVE,
+                    &hex!("8c80a2b95a549fa8d928dd0f0771be4f3d774408c0f98bf670b1a2c390706bf3")
+                );
+                assert_eq!(mitigated_config_advisories, &Vec::<String>::default());
+                assert_eq!(
+                    mitigated_hardening_advisories,
+                    &["INTEL-SA-00334", "INTEL-SA-00615", "INTEL-SA-00657"]
+                );
+            }
+            _ => panic!("unexpected"),
+        };
+
+        let _ = tms.create_verifier("consensus").unwrap();
+        let _ = tms.create_verifier("fog-ingest").unwrap();
+        let _ = tms.create_verifier("fog-ledger").unwrap();
+        let _ = tms.create_verifier("fog-view").unwrap();
+
+        assert!(tms.create_verifier("impostor").is_err());
+    }
+
+    const TEST_DATA2: &str = r#"{
+    "v3": {
+       "consensus": {
+           "MRENCLAVE": "207c9705bf640fdb960034595433ee1ff914f9154fbe4bc7fc8a97e912961e5c",
+           "mitigated_config_advisories": ["FOO"],
+           "mitigated_hardening_advisories": ["INTEL-SA-00334", "INTEL-SA-00615"]
+       },
+       "fog-ingest": {
+           "MRSIGNER": "2c1a561c4ab64cbc04bfa445cdf7bed9b2ad6f6b04d38d3137f3622b29fdb30e",
+           "product_id": 1,
+           "minimum_svn": 4,
+           "mitigated_config_advisories": ["FOO"],
+           "mitigated_hardening_advisories": ["INTEL-SA-00334", "INTEL-SA-00615"]
+       },
+       "fog-ledger": {
+           "MRSIGNER": "2c1a561c4ab64cbc04bfa445cdf7bed9b2ad6f6b04d38d3137f3622b29fdb30e",
+           "product_id": 2,
+           "minimum_svn": 4,
+           "mitigated_config_advisories": ["FOO"],
+           "mitigated_hardening_advisories": ["INTEL-SA-00334", "INTEL-SA-00615"]
+       },
+       "fog-view": {
+           "MRSIGNER": "2c1a561c4ab64cbc04bfa445cdf7bed9b2ad6f6b04d38d3137f3622b29fdb30e",
+           "product_id": 3,
+           "minimum_svn": 4,
+           "mitigated_config_advisories": ["FOO"],
+           "mitigated_hardening_advisories": ["INTEL-SA-00334", "INTEL-SA-00615"]
+       }
+    },
+    "v4": {
+       "consensus": {
+           "MRENCLAVE": "e35bc15ee92775029a60a715dca05d310ad40993f56ad43bca7e649ccc9021b5",
+           "mitigated_config_advisories": ["FOO"],
+           "mitigated_hardening_advisories": ["INTEL-SA-00334", "INTEL-SA-00615", "INTEL-SA-00657"]
+       },
+       "fog-ingest": {
+           "MRENCLAVE": "a8af815564569aae3558d8e4e4be14d1bcec896623166a10494b4eaea3e1c48c",
+           "mitigated_config_advisories": ["FOO"],
+           "mitigated_hardening_advisories": ["INTEL-SA-00334", "INTEL-SA-00615", "INTEL-SA-00657"]
+       },
+       "fog-ledger": {
+           "MRENCLAVE": "da209f4b24e8f4471bd6440c4e9f1b3100f1da09e2836d236e285b274901ed3b",
+           "mitigated_config_advisories": ["FOO"],
+           "mitigated_hardening_advisories": ["INTEL-SA-00334", "INTEL-SA-00615", "INTEL-SA-00657"]
+       },
+       "fog-view": {
+           "MRENCLAVE": "8c80a2b95a549fa8d928dd0f0771be4f3d774408c0f98bf670b1a2c390706bf3",
+           "mitigated_config_advisories": ["FOO"],
+           "mitigated_hardening_advisories": ["INTEL-SA-00334", "INTEL-SA-00615", "INTEL-SA-00657"]
+       }
+    }
+}"#;
+
+    #[test]
+    fn test_loading2() {
+        let tms: TrustedMeasurementSet = serde_json::from_str(TEST_DATA2).unwrap();
+
+        assert_eq!(tms.table.len(), 2);
+        let v3 = tms.table.get("v3").unwrap();
+        assert_eq!(v3.len(), 4);
+        let v3_consensus = v3.get("consensus").unwrap();
+        match v3_consensus {
+            StatusVerifierConfig::Mrenclave {
+                MRENCLAVE,
+                mitigated_config_advisories,
+                mitigated_hardening_advisories,
+            } => {
+                assert_eq!(
+                    MRENCLAVE,
+                    &hex!("207c9705bf640fdb960034595433ee1ff914f9154fbe4bc7fc8a97e912961e5c")
+                );
+                assert_eq!(mitigated_config_advisories, &["FOO"]);
+                assert_eq!(
+                    mitigated_hardening_advisories,
+                    &["INTEL-SA-00334", "INTEL-SA-00615"]
+                );
+            }
+            _ => panic!("unexpected"),
+        };
+        let v3_fog_view = v3.get("fog-view").unwrap();
+        match v3_fog_view {
+            StatusVerifierConfig::Mrsigner {
+                MRSIGNER,
+                product_id,
+                minimum_svn,
+                mitigated_config_advisories,
+                mitigated_hardening_advisories,
+            } => {
+                assert_eq!(
+                    MRSIGNER,
+                    &hex!("2c1a561c4ab64cbc04bfa445cdf7bed9b2ad6f6b04d38d3137f3622b29fdb30e")
+                );
+                assert_eq!(*product_id, 3);
+                assert_eq!(*minimum_svn, 4);
+                assert_eq!(mitigated_config_advisories, &["FOO"]);
+                assert_eq!(
+                    mitigated_hardening_advisories,
+                    &["INTEL-SA-00334", "INTEL-SA-00615"]
+                );
+            }
+            _ => panic!("unexpected"),
+        };
+
+        let v4 = tms.table.get("v4").unwrap();
+        assert_eq!(v4.len(), 4);
+        let v4_consensus = v4.get("consensus").unwrap();
+        match v4_consensus {
+            StatusVerifierConfig::Mrenclave {
+                MRENCLAVE,
+                mitigated_config_advisories,
+                mitigated_hardening_advisories,
+            } => {
+                assert_eq!(
+                    MRENCLAVE,
+                    &hex!("e35bc15ee92775029a60a715dca05d310ad40993f56ad43bca7e649ccc9021b5")
+                );
+                assert_eq!(mitigated_config_advisories, &["FOO"]);
+                assert_eq!(
+                    mitigated_hardening_advisories,
+                    &["INTEL-SA-00334", "INTEL-SA-00615", "INTEL-SA-00657"]
+                );
+            }
+            _ => panic!("unexpected"),
+        };
+        let v4_fog_view = v4.get("fog-view").unwrap();
+        match v4_fog_view {
+            StatusVerifierConfig::Mrenclave {
+                MRENCLAVE,
+                mitigated_config_advisories,
+                mitigated_hardening_advisories,
+            } => {
+                assert_eq!(
+                    MRENCLAVE,
+                    &hex!("8c80a2b95a549fa8d928dd0f0771be4f3d774408c0f98bf670b1a2c390706bf3")
+                );
+                assert_eq!(mitigated_config_advisories, &["FOO"]);
+                assert_eq!(
+                    mitigated_hardening_advisories,
+                    &["INTEL-SA-00334", "INTEL-SA-00615", "INTEL-SA-00657"]
+                );
+            }
+            _ => panic!("unexpected"),
+        };
+
+        let _ = tms.create_verifier("consensus").unwrap();
+        let _ = tms.create_verifier("fog-ingest").unwrap();
+        let _ = tms.create_verifier("fog-ledger").unwrap();
+        let _ = tms.create_verifier("fog-view").unwrap();
+
+        assert!(tms.create_verifier("impostor").is_err());
+    }
+}
