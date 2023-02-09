dfa338f506d657983fd2c749ca287d67a3d542a9
awygle <andrew@mobilecoin.com>
2023-02-09 10:19:57 -0800

    Implement Sharding Strategy (#2873)
    * Ledger enclave support for router and store (#2896)
    
    * Pulling changes in from milliec/ledger-router-dev
    
    * Run cargo fmt
    
    * Making requested changes and clarifications to Fog ledger router comments.
    
    * Additional comment fixes
    
    * Fix CI lint
    
    * PR feedback nits
    
    Co-authored-by: Andrew Wygle <andrew@mobilecoin.com>
    
    * Fog Ledger Router Admin service
    
    * Sort itertools properly in Cargo.toml
    
    * Key Image Router Server + Binary
    
    * Update router config for parameterized retries
    
    * Changes due to rebase
    
    * normalize naming - ledger router, key image store
    
    * Linting fixes
    
    * Accept code review suggestions
    
    * Updates for GRPCIO 0.12
    
    * Remove some unwraps in ledger_router binary
    
    * Pulling changes in from milliec/ledger-router-dev
    
    * Making requested changes and clarifications to Fog ledger router comments.
    
    * PR feedback nits
    
    * Key Image Router Service
    
    * Update fog/ledger/server/src/key_image_router_service.rs
    
    Co-authored-by: Nick Santana <nick@mobilecoin.com>
    
    * Address PR feedback around logging and comments.
    
    * Parameterize allowed number of retries for query loop
    
    * Fog Ledger Router Admin service
    
    * Sort itertools properly in Cargo.toml
    
    * Key Image Router Server + Binary
    
    * Key image store changes pulled in from milliec/ledger-router-dev
    
    * Cargo fmt
    
    * Run clippy
    
    * Sort itertools properly in Cargo.toml
    
    * Rebase and update to match current fog ledger router branch
    
    * Remove unused dependencies in fog-ledger-server
    
    Co-authored-by: Nick Santana <nick@mobilecoin.com>
    
    * Apply suggestions - Remove unused deps
    
    Co-authored-by: Nick Santana <nick@mobilecoin.com>
    
    * Apply suggestions from comments
    
    Applying suggestions from @nick-mobilecoin's review
    
    Co-authored-by: Nick Santana <nick@mobilecoin.com>
    
    * Resolving some code quality issues in direct_key_image_store_check()
    
    * Cargo fmt
    
    * key image server and key image service moved to router server and router service
    
    * Fog Ledger Router Admin service
    
    * Key Image Router Server + Binary
    
    * Update router config for parameterized retries
    
    * Changes due to rebase
    
    * normalize naming - ledger router, key image store
    
    * Linting fixes
    
    * Accept code review suggestions
    
    * Implement ShardingStrategy into DbFetcher
    
    * Split out DbFetcher creation and starting for ease of integration
    
    * Add a DbFetcher to the ledger store server.
    
    * Start the db_fetcher in the Fog Ledger Server to reflect the new behavior
    
    * Run cargo fmt
    
    * Run cargo clippy
    
    * Other clippy and fmt changes
    
    * Sort dependencies
    
    * Remove unimplemented! that snuck in from other branch
    
    * Update and sync with latest Fog ledger router
    
    * Remove some unwraps
    
    * Lint again
    
    * rebase fixups
    
    * PR feedback
    
    ---------
    
    Co-authored-by: Emily C <gyrocoder@gmail.com>
    Co-authored-by: Nick Santana <nick@mobilecoin.com>

--- fog/ledger/server/src/key_image_store_server.rs
+++ fog/ledger/server/src/key_image_store_server.rs
@@ -18,28 +18,36 @@ use mc_util_grpc::{
 use mc_watcher::watcher_db::WatcherDB;
 
 use crate::{
-    config::LedgerStoreConfig, server::DbPollSharedState, KeyImageClientListenUri, KeyImageService,
+    config::LedgerStoreConfig, db_fetcher::DbFetcher, server::DbPollSharedState,
+    sharding_strategy::ShardingStrategy, KeyImageClientListenUri, KeyImageService,
 };
 
-pub struct KeyImageStoreServer {
+pub struct KeyImageStoreServer<E, SS>
+where
+    E: LedgerEnclaveProxy,
+    SS: ShardingStrategy + Send + Sync + 'static,
+{
     server: grpcio::Server,
     client_listen_uri: KeyImageStoreUri,
+    db_fetcher: DbFetcher<LedgerDB, E, SS>,
     logger: Logger,
 }
 
-impl KeyImageStoreServer {
+impl<E, SS> KeyImageStoreServer<E, SS>
+where
+    E: LedgerEnclaveProxy,
+    SS: ShardingStrategy + Send + Sync + 'static,
+{
     /// Creates a new key image store server instance
-    pub fn new_from_config<E>(
+    pub fn new_from_config(
         config: LedgerStoreConfig,
         enclave: E,
         ledger: LedgerDB,
         watcher: WatcherDB,
+        sharding_strategy: SS,
         time_provider: impl TimeProvider + 'static,
         logger: Logger,
-    ) -> KeyImageStoreServer
-    where
-        E: LedgerEnclaveProxy,
-    {
+    ) -> KeyImageStoreServer<E, SS> {
         let client_authenticator: Arc<dyn Authenticator + Sync + Send> =
             if let Some(shared_secret) = config.client_auth_token_secret.as_ref() {
                 Arc::new(TokenAuthenticator::new(
@@ -58,22 +66,21 @@ impl KeyImageStoreServer {
             enclave,
             ledger,
             watcher,
+            sharding_strategy,
             logger,
         )
     }
 
-    pub fn new<E>(
+    pub fn new(
         chain_id: String,
         client_authenticator: Arc<dyn Authenticator + Sync + Send>,
         client_listen_uri: KeyImageStoreUri,
         enclave: E,
         ledger: LedgerDB,
         watcher: WatcherDB,
+        sharding_strategy: SS,
         logger: Logger,
-    ) -> KeyImageStoreServer
-    where
-        E: LedgerEnclaveProxy,
-    {
+    ) -> KeyImageStoreServer<E, SS> {
         let shared_state = Arc::new(Mutex::new(DbPollSharedState::default()));
 
         let key_image_service = KeyImageService::new(
@@ -81,22 +88,27 @@ impl KeyImageStoreServer {
             chain_id,
             ledger,
             watcher,
-            enclave,
+            enclave.clone(),
             shared_state,
             client_authenticator.clone(),
             logger.clone(),
         );
-        Self::new_from_service(key_image_service, client_listen_uri, logger)
+        Self::new_from_service(
+            key_image_service,
+            client_listen_uri,
+            enclave,
+            sharding_strategy,
+            logger,
+        )
     }
 
-    pub fn new_from_service<E>(
-        key_image_service: KeyImageService<LedgerDB, E>,
+    pub fn new_from_service(
+        mut key_image_service: KeyImageService<LedgerDB, E>,
         client_listen_uri: KeyImageStoreUri,
+        enclave: E,
+        sharding_strategy: SS,
         logger: Logger,
-    ) -> KeyImageStoreServer
-    where
-        E: LedgerEnclaveProxy,
-    {
+    ) -> KeyImageStoreServer<E, SS> {
         let readiness_indicator = ReadinessIndicator::default();
 
         let env = Arc::new(
@@ -106,13 +118,16 @@ impl KeyImageStoreServer {
         );
 
         // Health check service
-        let health_service =
-            mc_util_grpc::HealthService::new(Some(readiness_indicator.into()), logger.clone())
-                .into_service();
+        let health_service = mc_util_grpc::HealthService::new(
+            Some(readiness_indicator.clone().into()),
+            logger.clone(),
+        )
+        .into_service();
 
         // Build our store server.
         // Init ledger store service.
-        let ledger_store_service = ledger_grpc::create_key_image_store_api(key_image_service);
+        let ledger_store_service =
+            ledger_grpc::create_key_image_store_api(key_image_service.clone());
         log::debug!(logger, "Constructed Key Image Store GRPC Service");
 
         // Package service into grpc server
@@ -126,11 +141,22 @@ impl KeyImageStoreServer {
             .register_service(ledger_store_service)
             .register_service(health_service)
             .build_using_uri(&client_listen_uri, logger.clone())
-            .unwrap();
+            .expect("Could not build Key Image Store Server");
+
+        let db_fetcher = DbFetcher::new(
+            key_image_service.get_ledger(),
+            enclave,
+            sharding_strategy,
+            key_image_service.get_watcher(),
+            key_image_service.get_db_poll_shared_state(),
+            readiness_indicator,
+            logger.clone(),
+        );
 
         Self {
             server,
             client_listen_uri,
+            db_fetcher,
             logger,
         }
     }
@@ -143,15 +169,21 @@ impl KeyImageStoreServer {
             "API listening on {}",
             self.client_listen_uri.addr()
         );
+        self.db_fetcher.start();
     }
 
     /// Stops the server
     pub fn stop(&mut self) {
         block_on(self.server.shutdown()).expect("Could not stop grpc server");
+        self.db_fetcher.stop().expect("Could not stop DbFetcher");
     }
 }
 
-impl Drop for KeyImageStoreServer {
+impl<E, SS> Drop for KeyImageStoreServer<E, SS>
+where
+    E: LedgerEnclaveProxy,
+    SS: ShardingStrategy + Send + Sync + 'static,
+{
     fn drop(&mut self) {
         self.stop();
     }
