601d7b8ad3bfa4107dbb54acf6f16e0758d1d11d
ryan kurte <ryan@kurte.nz>
2023-02-25 16:23:38 +1300

    fix missing serde include

--- transaction/extra/src/signed_contingent_input.rs
+++ transaction/extra/src/signed_contingent_input.rs
@@ -14,6 +14,7 @@ use mc_transaction_core::{
     AmountError, RevealedTxOutError, TokenId, TxOutConversionError, UnmaskedAmount,
 };
 use prost::Message;
+use serde::{Deserialize, Serialize};
 use zeroize::Zeroize;
 
 /// A signed contingent input is a "transaction fragment" which can be
