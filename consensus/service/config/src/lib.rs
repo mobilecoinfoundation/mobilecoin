4e23c5fc51768c53b5f183517b83b9f38048f05a
Millie C <gyrocoder@gmail.com>
2023-03-02 13:53:44 -0500

    Cargo fmt

--- consensus/service/config/src/lib.rs
+++ consensus/service/config/src/lib.rs
@@ -128,12 +128,12 @@ pub struct Config {
 
     /// Maximum number of client session tracking structures to retain in
     /// a least-recently-used cache.
-    /// 
+    ///
     /// MAX_CLIENT_SESSIONS in crypto/ake/enclave/src/lib.rs controls what is,
-    /// effectively, the same session-tracking data on the unsecure side that 
-    /// this variable controls in the enclave. 
+    /// effectively, the same session-tracking data on the unsecure side that
+    /// this variable controls in the enclave.
     /// If MAX_CLIENT_SESSIONS changes, please change the default on this
-    /// config setting to match. 
+    /// config setting to match.
     #[clap(long, default_value = "10000", env = "MC_CLIENT_TRACKING_CAPACITY")]
     pub client_tracking_capacity: usize,
 }
