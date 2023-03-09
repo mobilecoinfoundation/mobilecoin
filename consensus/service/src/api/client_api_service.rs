f70ce5a11ea41a0ca77de1a66079155612c23433
Millie C <gyrocoder@gmail.com>
2023-03-08 21:28:18 -0500

    Push an Instant::now() for every time validation fails on a tx proposal

--- consensus/service/src/api/client_api_service.rs
+++ consensus/service/src/api/client_api_service.rs
@@ -130,12 +130,26 @@ impl ClientApiService {
         msg: Message,
     ) -> Result<ProposeTxResponse, ConsensusGrpcError> {
         counters::ADD_TX_INITIATED.inc();
+        let session_id = ClientSession::from(msg.channel_id.clone());
         let tx_context = self.enclave.client_tx_propose(msg.into())?;
 
         // Cache the transaction. This performs the well-formedness checks.
         let tx_hash = self.tx_manager.insert(tx_context).map_err(|err| {
             if let TxManagerError::TransactionValidation(cause) = &err {
                 counters::TX_VALIDATION_ERROR_COUNTER.inc(&format!("{cause:?}"));
+
+                let tracking_window = Duration::from_secs(5); // TODO, placeholder before draft PR gets published. 
+                let mut tracker = self.tracked_sessions.lock().expect("Mutex poisoned");
+                let record = if let Some(record) = tracker.get_mut(&session_id) {
+                    record
+                } else {
+                    tracker.put(session_id.clone(), ClientSessionTracking::new());
+                    tracker.get_mut(&session_id)
+                        .expect("Adding session-tracking record should be atomic.")
+                    
+                };
+                let _recent_failure_count = record.fail_tx_proposal(&Instant::now(), &tracking_window);
+                // TODO: drop session when recent_failure_count reaches some number
             }
             err
         })?;
@@ -256,16 +270,6 @@ impl ConsensusClientApi for ClientApiService {
     ) {
         let _timer = SVC_COUNTERS.req(&ctx);
 
-        {
-            let session = ClientSession::from(msg.channel_id.clone());
-            let mut tracker = self.tracked_sessions.lock().expect("Mutex poisoned");
-            if let Some(_session_info) = tracker.get(&session) {
-                // TODO: Update fields
-            } else {
-                // TODO: Populate new session metadata
-                tracker.put(session, ClientSessionTracking::new());
-            }
-        }
         if let Err(err) = check_request_chain_id(&self.config.chain_id, &ctx) {
             return send_result(ctx, sink, Err(err), &self.logger);
         }
