6de4fc083fa204a44f58cbb4a43036a4a310d58d
awygle <andrew@mobilecoin.com>
2023-03-17 11:14:14 -0700

    Fog Ledger Router integration test (#3188)
    * Router integration test
    
    * Clippy updates after rebase
    
    * Implement Drop for TestEnvironment
    
    * Eliminate temp dirs after test
    
    * Remove unused GRPC arguments
    
    * Load environment config from JSON file. Might delete later.
    
    * Revert "Load environment config from JSON file. Might delete later."
    
    This reverts commit a677b59111c37bd24543d29b34e2424d6a9d80e3.
    
    * Remove magic numbers

--- fog/ledger/test_infra/src/lib.rs
+++ fog/ledger/test_infra/src/lib.rs
@@ -2,6 +2,12 @@
 
 //! Functionality for mocking and testing components in the ledger server
 
+use http::Uri;
+use hyper::{
+    client::HttpConnector,
+    service::{make_service_fn, service_fn},
+    Body, Client, Request, Response, Server,
+};
 use mc_attest_core::{IasNonce, Quote, QuoteNonce, Report, TargetInfo, VerificationReport};
 use mc_attest_enclave_api::{
     ClientAuthRequest, ClientAuthResponse, ClientSession, EnclaveMessage, NonceAuthRequest,
@@ -24,6 +30,9 @@ use mc_transaction_core::{
     tx::{TxOut, TxOutMembershipElement, TxOutMembershipProof},
     TokenId,
 };
+use rand::seq::SliceRandom;
+use std::{net::SocketAddr, sync::Arc};
+use tokio::{sync::oneshot, task::JoinHandle};
 
 #[derive(Default, Clone)]
 pub struct MockEnclave {}
@@ -284,5 +293,73 @@ impl Ledger for MockLedger {
         _mint_tx: &MintTx,
     ) -> Result<ActiveMintConfig, Error> {
         unimplemented!()
+    }
+}
+
+pub struct ShardProxyServer {
+    server_handle: Option<JoinHandle<Result<(), hyper::Error>>>,
+    stop_channel: Option<oneshot::Sender<()>>,
+}
+
+impl ShardProxyServer {
+    async fn route(
+        request: Request<Body>,
+        client: Arc<Client<HttpConnector>>,
+        endpoints: Arc<Vec<String>>,
+    ) -> Result<Response<Body>, hyper::Error> {
+        let endpoint = {
+            let mut rng = rand::thread_rng();
+            endpoints.choose(&mut rng).unwrap()
+        };
+        let (mut parts, body) = request.into_parts();
+
+        let mut uri_parts = parts.uri.clone().into_parts();
+        uri_parts.authority = Some(endpoint.parse().unwrap());
+        uri_parts.scheme = Some("http".parse().unwrap());
+        parts.uri = Uri::from_parts(uri_parts).unwrap();
+
+        let request = Request::from_parts(parts, body);
+        let response = client.request(request).await;
+        response
+    }
+
+    async fn shutdown(channel: oneshot::Receiver<()>) {
+        channel.await.unwrap_or(());
+    }
+
+    pub fn new(address: &SocketAddr, endpoints: Vec<String>) -> Self {
+        let client = Arc::new(Client::builder().http2_only(true).build_http());
+        let endpoints = Arc::new(endpoints);
+        let (tx, rx) = oneshot::channel::<()>();
+
+        let make_service = make_service_fn(move |_| {
+            let client = client.clone();
+            let endpoints = endpoints.clone();
+
+            async move {
+                Ok::<_, hyper::Error>(service_fn(move |req| {
+                    Self::route(req, client.clone(), endpoints.clone())
+                }))
+            }
+        });
+
+        let server = Server::bind(address).serve(make_service);
+        let graceful = server.with_graceful_shutdown(Self::shutdown(rx));
+
+        let server_handle = tokio::spawn(async move { graceful.await });
+
+        Self {
+            server_handle: Some(server_handle),
+            stop_channel: Some(tx),
+        }
+    }
+
+    pub async fn stop(&mut self) {
+        if let Some(stop_channel) = self.stop_channel.take() {
+            let _ = stop_channel.send(());
+        }
+        if let Some(server_handle) = self.server_handle.take() {
+            let _ = server_handle.await;
+        }
     }
 }
