c8ccf98e74be8b9f61b4c0431e126acc8f5b402c
Chris Beck <beck.ct@gmail.com>
2023-03-23 09:07:08 -0600

    add a code comment

--- sgx/urts/src/panic.rs
+++ sgx/urts/src/panic.rs
@@ -15,8 +15,18 @@ pub unsafe extern "C" fn report_panic_message(msg: *const u8, msg_len: usize) {
 
 fn report_panic_message_impl(panic_msg_bytes: &[u8]) {
     match str::from_utf8(panic_msg_bytes) {
+        // We log to both STDERR and the slog interface, which goes to STDOUT.
+        // The reasoning is, we want most stuff to be directed at slog, because
+        // that's what our production logging is picking up right now, and
+        // those logs will have timing, source data, etc.
+        // However, when running locally in SGX_MODE=SW, what typically happens
+        // during an enclave panic is that the process crashes almost immediately
+        // after this OCALL returns, and slog doesn't manage to flush its buffer
+        // before the process dies, which makes it hard for a dev to debug.
+        // By logging to STDERR also we make sure the message gets to an OS
+        // buffer before the process dies.
         Ok(v) => {
-            eprintln!("Enclave panic:\n{}\n", v);
+            eprintln!("Enclave panic:\n{}", v);
             global_log::crit!("Enclave panic:\n{}\n", v)
         },
         Err(e) => {
