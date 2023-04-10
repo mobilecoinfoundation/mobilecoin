c641991502c34610d2fae2ed88a23165470d8c63
Chris Beck <5683852+cbeck88@users.noreply.github.com>
2023-04-10 14:50:19 -0600

    refactor mobilecoind api to not depend on consensus-api (#3307)
    * refactor mobilecoind api to not depend on consensus-api
    
    I am having a lot of build problems in another project due to
    mc-mobilecoind-api pulling in mbedtls, which exports some symbols
    that conflicts with another C library that some rust code depends on.
    
    I determined that the only reason I have mbedtls in my build is
    that `mobilecoind-api` depends on `consensus-api` and this pulls
    in `mc-attest-core` etc.
    
    This change allows me to break this dependency.
    
    * remove stuff from build.rs that isn't needed anymore
    
    * cargo lock
    
    * fixup
    
    * fixup
    
    * restore deprecated field (avoid compatibility break)
    
    * add changelog entry
    
    * fixup

--- mobilecoind/src/service.rs
+++ mobilecoind/src/service.rs
@@ -2158,6 +2158,24 @@ impl<T: BlockchainConnection + UserTxConnection + 'static, FPR: FogPubkeyResolve
                 RpcStatus::with_message(RpcStatusCode::INTERNAL, "no peers reachable".to_owned())
             })?;
 
+        let mut mcd_last_block_info = api::LastBlockInfo::new();
+        mcd_last_block_info.set_index(last_block_info.block_index);
+        mcd_last_block_info.set_mob_minimum_fee(
+            last_block_info
+                .minimum_fees
+                .get(&TokenId::from(0))
+                .cloned()
+                .unwrap_or(0),
+        );
+        mcd_last_block_info.set_minimum_fees(
+            last_block_info
+                .minimum_fees
+                .into_iter()
+                .map(|(token_id, fee)| (*token_id, fee))
+                .collect(),
+        );
+        mcd_last_block_info.set_network_block_version(last_block_info.network_block_version);
+
         let mut response = api::GetNetworkStatusResponse::new();
 
         response.set_network_highest_block_index(
@@ -2172,7 +2190,7 @@ impl<T: BlockchainConnection + UserTxConnection + 'static, FPR: FogPubkeyResolve
         );
         response.set_local_block_index(local_block_index);
         response.set_is_behind(network_state.is_behind(local_block_index));
-        response.set_last_block_info(last_block_info.into());
+        response.set_last_block_info(mcd_last_block_info);
 
         Ok(response)
     }
