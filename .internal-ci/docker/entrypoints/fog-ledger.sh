0b95182becf00af59c99884f8b5f5b9a13bd0e48
Aaron Weisberg <aaronweisberg@gmail.com>
2023-06-01 10:43:53 -0700

    move mkdir

--- .internal-ci/docker/entrypoints/fog-ledger.sh
+++ .internal-ci/docker/entrypoints/fog-ledger.sh
@@ -12,12 +12,12 @@ then
     #   Need to fix .internal-ci/helm/mc-core-common-config/templates/mobilecoind-supervisord-mobilecoind-configmap.yaml
     
     echo "MC_LEDGER_DB_URL set, restoring ${data}/ledger/data.mdb from backup"
-    mkdir -p "${data}/ledger"
     if [[ -f "${data}/ledger/data.mdb" ]]
     then
         echo "Found existing ledger database, skipping download"
     else
         echo "Downloading ledger data.mdb"
+        mkdir -p "${data}/ledger"
         curl -L "${MC_LEDGER_DB_URL}" -o "${data}/ledger/data.mdb"
     fi
 fi
@@ -25,12 +25,12 @@ fi
 if [[ -n "${MC_WATCHER_DB_URL}" ]]
 then
     echo "MC_WATCHER_DB_URL set, restoring ${data}/watcher/data.mdb from backup"
-    mkdir -p "${data}/watcher"
     if [[ -f "${data}/watcher/data.mdb" ]]
     then
         echo "Found existing watcher database, skipping download"
     else
         echo "Downloading watcher data.mdb"
+        mkdir -p "${data}/watcher"
         curl -L "${MC_WATCHER_DB_URL}" -o "${data}/watcher/data.mdb"
     fi
 fi
