b38fff2ec5e62c6176ee849fcfbdad6240256590
Aaron Weisberg <aaronweisberg@gmail.com>
2023-06-25 21:43:11 -0700

    chore: Update metrics endpoint log message

--- fog/view/server/src/bin/router.rs
+++ fog/view/server/src/bin/router.rs
@@ -86,7 +86,7 @@ async fn main() {
     router_server.start();
 
     let metrics_path = warp::path(config.metric_path.clone()).and_then(metrics_handler);
-    log::info!(logger.clone(), "Metrics API listening on :3030");
+    log::info!(logger.clone(), "Starting metrics endpoint");
     warp::serve(metrics_path)
         .run(([0, 0, 0, 0], config.metric_port))
         .await;
