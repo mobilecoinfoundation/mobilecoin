f827b2d3727209f63a9b9cfa73601e874fea6681
Nick Santana <nick@mobilecoin.com>
2023-07-28 14:20:18 -0700

    Please forgive me
    Killing the hardware ias client

--- attest/net/src/lib.rs
+++ attest/net/src/lib.rs
@@ -9,15 +9,6 @@ extern crate core;
 mod traits;
 
 pub use crate::traits::{Error, RaClient, Result};
+mod sim;
+pub type Client = crate::sim::SimClient;
 
-// Export the "build-configured" RaClient so that downstream doesn't need
-// to copy paste this cfg_if every where and have a build.rs unnecessarily
-cfg_if::cfg_if! {
-    if #[cfg(feature = "sgx-sim")] {
-        mod sim;
-        pub type Client = crate::sim::SimClient;
-    } else {
-        mod ias;
-        pub type Client = crate::ias::IasClient;
-    }
-}
