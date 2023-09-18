705984fb8be014aed2e2147cdc9ffed8eb77c561
Nick Santana <nick@mobilecoin.com>
2023-09-18 09:21:49 -0700

    Move cargo fmt before cargo clippy
    cargo fmt is now run before cargo clippy in `tools/lint.sh` this ensures
    the quicker check to fail will fail first.

--- tools/lint.sh
+++ tools/lint.sh
@@ -36,8 +36,8 @@ cargo install --version 1.0.9 --locked cargo-sort
 # We want to check with --all-targets since it checks test code, but that flag
 # leads to build errors in enclave workspaces, so check it here.
 cargo sort --workspace --grouped $CHECK
-cargo clippy --all --all-features --all-targets -- -D warnings
 cargo fmt -- --unstable-features $CHECK
+cargo clippy --all --all-features --all-targets -- -D warnings
 
 # Run on all the nested workspaces.
 # We exclude `cargo` paths as these are crates from `[patch.crates-io]`
@@ -45,8 +45,8 @@ for toml in $(find . -mindepth 2 -type f -not -path '*/cargo/*' -name Cargo.toml
   pushd $(dirname $toml) >/dev/null
   echo "Linting in $PWD"
   cargo sort --workspace --grouped $CHECK
-  cargo clippy --all --all-features -- -D warnings
   cargo fmt -- --unstable-features $CHECK
+  cargo clippy --all --all-features -- -D warnings
   echo "Linting in $PWD complete."
   popd >/dev/null
 done
