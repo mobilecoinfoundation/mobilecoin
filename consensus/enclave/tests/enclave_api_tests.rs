e993818b5c7f14d811c59877152acdf62603ce10
Nick Santana <nick@mobilecoin.com>
2023-10-14 12:44:25 -0700

    Fix tests for identities

--- consensus/enclave/tests/enclave_api_tests.rs
+++ consensus/enclave/tests/enclave_api_tests.rs
@@ -62,7 +62,8 @@ fn consensus_enclave_client_tx_propose(logger: Logger) {
     let auth_response_msg = AuthMessage::from(auth_response_msg);
 
     // Now the client should have a working cipher state
-    let auth_response_event = AuthResponseInput::new(auth_response_msg.into(), [], None);
+    let identity = mc_consensus_enclave_measurement::mr_signer_identity(None);
+    let auth_response_event = AuthResponseInput::new(auth_response_msg.into(), [identity], None);
     let (mut initiator, _attestation_evidence) =
         initiator.try_next(&mut rng, auth_response_event).unwrap();
 
--- fog/ledger/server/tests/store.rs
+++ fog/ledger/server/tests/store.rs
@@ -234,7 +234,8 @@ pub fn direct_key_image_store_check(logger: Logger) {
     // AuthResponseOutput
     let auth_message = attest::AuthMessage::from(client_auth_response);
     // Initiator accepts responder's message.
-    let auth_response_event = AuthResponseInput::new(auth_message.into(), [], None);
+    let identity = mc_fog_ledger_enclave_measurement::mr_signer_identity(None);
+    let auth_response_event = AuthResponseInput::new(auth_message.into(), [identity], None);
     // Should be a valid noise connection at this point.
     let (mut noise_connection, _verification_report) = initiator
         .try_next(&mut rng, auth_response_event)
