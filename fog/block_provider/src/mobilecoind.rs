1fe37f141ecf9cc7030f04461e8d35c19a08b06d
Eran Rundstein <eran@rundste.in>
2023-11-08 11:56:38 -0800

    poll_block_timestamp

--- fog/block_provider/src/mobilecoind.rs
+++ fog/block_provider/src/mobilecoind.rs
@@ -4,19 +4,27 @@ use crate::{
     BlockDataWithTimestamp, BlockProvider, BlocksDataResponse, Error, TxOutInfoByPublicKeyResponse,
 };
 use grpcio::{ChannelBuilder, EnvBuilder};
+use mc_api::watcher::TimestampResultCode;
 use mc_blockchain_types::{Block, BlockData, BlockIndex};
-use mc_common::logger::Logger;
+use mc_common::logger::{log, Logger};
 use mc_crypto_keys::CompressedRistrettoPublic;
 use mc_mobilecoind_api::{
-    mobilecoind_api_grpc::MobilecoindApiClient, GetBlocksDataRequest, MobilecoindUri,
+    mobilecoind_api_grpc::MobilecoindApiClient, GetBlockRequest, GetBlocksDataRequest,
+    MobilecoindUri,
 };
 use mc_transaction_core::tx::{TxOut, TxOutMembershipProof};
 use mc_util_grpc::ConnectionUriGrpcioChannel;
-use std::{sync::Arc, time::Duration};
+use mc_watcher::watcher_db::{POLL_BLOCK_TIMESTAMP_POLLING_FREQUENCY, POLL_BLOCK_TIMESTAMP_ERROR_RETRY_FREQUENCY};
+use std::{
+    sync::Arc,
+    thread::sleep,
+    time::{Duration, Instant},
+};
 
 #[derive(Clone)]
 pub struct MobilecoindBlockProvider {
     client: MobilecoindApiClient,
+    logger: Logger,
 }
 
 impl MobilecoindBlockProvider {
@@ -29,7 +37,10 @@ impl MobilecoindBlockProvider {
 
         let client = MobilecoindApiClient::new(ch);
 
-        Box::new(Self { client })
+        Box::new(Self {
+            client,
+            logger: logger.clone(),
+        })
     }
 }
 
@@ -82,8 +93,66 @@ impl BlockProvider for MobilecoindBlockProvider {
 
     /// Poll indefinitely for a watcher timestamp, logging warnings if we wait
     /// for more than watcher_timeout.
-    fn poll_block_timestamp(&self, _block_index: BlockIndex, _watcher_timeout: Duration) -> u64 {
-        todo!()
+    fn poll_block_timestamp(&self, block_index: BlockIndex, watcher_timeout: Duration) -> u64 {
+        // special case the origin block has a timestamp of u64::MAX
+        if block_index == 0 {
+            return u64::MAX;
+        }
+
+        let request = GetBlockRequest {
+            block: block_index,
+            ..Default::default()
+        };
+
+        // Timer that tracks how long we have had WatcherBehind error for,
+        // if this exceeds watcher_timeout, we log a warning.
+        let mut watcher_behind_timer = Instant::now();
+        loop {
+            match self.client.get_block(&request) {
+                Ok(response) => match response.timestamp_result_code {
+                    TimestampResultCode::WatcherBehind => {
+                        if watcher_behind_timer.elapsed() > watcher_timeout {
+                            log::warn!(self.logger, "watcher is still behind on block index = {} after waiting {} seconds, caller will be blocked", block_index, watcher_timeout.as_secs());
+                            watcher_behind_timer = Instant::now();
+                        }
+                        sleep(POLL_BLOCK_TIMESTAMP_POLLING_FREQUENCY);
+                    }
+                    TimestampResultCode::BlockIndexOutOfBounds => {
+                        log::warn!(self.logger, "block index {} was out of bounds, we should not be scanning it, we will have junk timestamps for it", block_index);
+                        return u64::MAX;
+                    }
+                    TimestampResultCode::Unavailable => {
+                        log::crit!(self.logger, "watcher configuration is wrong and timestamps will not be available with this configuration. caller is blocked at block index {}", block_index);
+                        sleep(POLL_BLOCK_TIMESTAMP_ERROR_RETRY_FREQUENCY);
+                    }
+                    TimestampResultCode::WatcherDatabaseError => {
+                        log::crit!(self.logger, "The watcher database has an error which prevents us from getting timestamps. caller is blocked at block index {}", block_index);
+                        sleep(POLL_BLOCK_TIMESTAMP_ERROR_RETRY_FREQUENCY);
+                    }
+                    TimestampResultCode::TimestampFound => {
+                        return response.timestamp;
+                    }
+                    other => {
+                        log::crit!(
+                            self.logger,
+                            "Unexpected result code {:?} for block index {}",
+                            other,
+                            block_index
+                        );
+                        sleep(POLL_BLOCK_TIMESTAMP_ERROR_RETRY_FREQUENCY);
+                    }
+                },
+                Err(err) => {
+                    log::error!(
+                            self.logger,
+                            "Could not obtain timestamp for block {} due to error {}, this may mean the watcher is not correctly configured. will retry",
+                            block_index,
+                            err
+                        );
+                    sleep(POLL_BLOCK_TIMESTAMP_ERROR_RETRY_FREQUENCY);
+                }
+            };
+        }
     }
 
     /// Get TxOut and membership proof by tx out index.
