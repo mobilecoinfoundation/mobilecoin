111149ca99d4d1b3ef5ac9e1a61e9a080770bac0
Nick Santana <nick@mobilecoin.com>
2023-11-08 09:10:06 -0800

    Reticulating splines...
    Bringing in a static buffer

--- sgx/alloc/src/lib.rs
+++ sgx/alloc/src/lib.rs
@@ -5,32 +5,102 @@
 
 use core::alloc::{GlobalAlloc, Layout};
 use mc_sgx_debug::eprintln;
-use mc_sgx_sync::Mutex;
-use lazy_static::lazy_static;
+// use mc_sgx_sync::Mutex;
+// use lazy_static::lazy_static;
+use core::fmt;
+
+/// Byte size of [`WriteBuffer`].
+///
+/// Attempting to write more than this many bytes to the [`WriteBuffer`] will
+/// result in an error.
+pub const BUFFER_SIZE: usize = 512;
+
+/// A buffer which implements the [`fmt::Write`] trait.
+#[derive(Debug)]
+pub struct WriteBuffer {
+    buf: [u8; BUFFER_SIZE],
+    pos: usize,
+}
+
+impl WriteBuffer {
+    /// Create a new empty [`WriteBuffer`]
+    pub const fn new() -> Self {
+        WriteBuffer {
+            buf: [0; BUFFER_SIZE],
+            pos: 0,
+        }
+    }
+
+    /// Clear the contents in the [`WriteBuffer`]
+    pub fn clear(&mut self) {
+        self.pos = 0;
+    }
+}
+
+impl AsRef<str> for WriteBuffer {
+    fn as_ref(&self) -> &str {
+        // Shouldn't fail because [`Write::write_str()`] is the only public way
+        // to add content. [`Write::write_str()`] takes a `&str` so for this to
+        // fail someone must have coerced invalid UTF-8 to a string prior to
+        // this method invocation.
+        core::str::from_utf8(&self.buf[..self.pos])
+            .expect("`WriteBuffer` is not valid UTF-8. It should have only been given `&str`s")
+    }
+}
+
+impl AsRef<[u8]> for WriteBuffer {
+    fn as_ref(&self) -> &[u8] {
+        &self.buf[..self.pos]
+    }
+}
+
+impl fmt::Write for WriteBuffer {
+    fn write_str(&mut self, string: &str) -> fmt::Result {
+        let bytes = string.as_bytes();
+
+        let remaining = &mut self.buf[self.pos..];
+        if remaining.len() < bytes.len() {
+            return Err(fmt::Error);
+        }
+
+        let new_contents = &mut remaining[..bytes.len()];
+        new_contents.copy_from_slice(bytes);
+
+        self.pos += bytes.len();
+
+        Ok(())
+    }
+}
 
 // Our allocator uses malloc and free exposed by intel libsgx_tstdc
 extern "C" {
     // pub fn malloc(size: usize) -> *mut u8;
     pub fn memalign(align: usize, size: usize) -> *mut u8;
     pub fn free(p: *mut u8);
+    /// This is the OCALL prototype for passing message buffers to the untrusted
+    /// code. See sgx_debug_edl crate.
+    fn eprintln_message(msg: *const u8, msg_len: usize);
 }
 
-lazy_static! {
-    static ref TOTAL_HEAP: Mutex<u64> = Mutex::new(0);
-}
+// lazy_static! {
+//     static ref TOTAL_HEAP: Mutex<u64> = Mutex::new(0);
+// }
 
 // Our allocator definition
 struct SgxAllocator;
 
 unsafe impl GlobalAlloc for SgxAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        let memory = TOTAL_HEAP.lock().unwrap();
-        memory.checked_add(layout.size() as u64).unwrap();
-        eprintln!("MEMORY USED: {:?}", memory);
+        let mut buf = WriteBuffer;
+        write!(&mut buf, "ALLOCATING {} bytes", layout.size()).unwrap();
+        let contents = buf.as_ref();
+        eprintln_message(contents.as_ptr(), contents.len());
+        // let memory = TOTAL_HEAP.lock().unwrap();
+        // memory.checked_add(layout.size() as u64).unwrap();
         memalign(layout.align(), layout.size())
     }
-    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        TOTAL_HEAP.lock().unwrap().checked_sub(layout.size() as u64).unwrap();
+    unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
+        // TOTAL_HEAP.lock().unwrap().checked_sub(layout.size() as u64).unwrap();
         free(ptr)
     }
 }
