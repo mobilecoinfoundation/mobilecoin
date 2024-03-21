db8bc78dcc545771a413b8bd225d1beaf1382384
Eran Rundstein <eran@rundste.in>
2024-03-21 12:49:26 -0700

    add memo_builder method

--- mobilecoind/src/transaction_memo.rs
+++ mobilecoind/src/transaction_memo.rs
@@ -1,10 +1,12 @@
 // Copyright (c) 2018-2024 The MobileCoin Foundation
 
+use mc_account_keys::AccountKey;
 use mc_api::ConversionError;
 use mc_mobilecoind_api::{
     mobilecoind_api, TransactionMemo_RTH_oneof_payment_id, TransactionMemo_oneof_transaction_memo,
 };
-use mc_transaction_extra::BurnRedemptionMemo;
+use mc_transaction_builder::{BurnRedemptionMemoBuilder, EmptyMemoBuilder, MemoBuilder, RTHMemoBuilder};
+use mc_transaction_extra::{BurnRedemptionMemo, SenderMemoCredential};
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum TransactionMemo {
@@ -39,6 +41,57 @@ pub enum TransactionMemo {
 
     /// Burn Redemption memo.
     BurnRedemption([u8; BurnRedemptionMemo::MEMO_DATA_LEN]),
+}
+
+impl TransactionMemo {
+    pub fn memo_builder(&self, account_key: &AccountKey) -> Box<dyn MemoBuilder + Send + Sync> {
+        match self {
+            Self::Empty => Box::<EmptyMemoBuilder>::default(),
+            Self::RTH { subaddress_index } => {
+                let memo_builder = generate_rth_memo_builder(subaddress_index, account_key);
+                Box::new(memo_builder)
+            }
+            Self::RTHWithPaymentIntentId {
+                subaddress_index,
+                payment_intent_id,
+            } => {
+                let mut memo_builder = generate_rth_memo_builder(subaddress_index, account_key);
+                memo_builder.set_payment_intent_id(*payment_intent_id);
+                Box::new(memo_builder)
+            }
+            Self::RTHWithPaymentRequestId {
+                subaddress_index,
+                payment_request_id,
+            } => {
+                let mut memo_builder = generate_rth_memo_builder(subaddress_index, account_key);
+                memo_builder.set_payment_request_id(*payment_request_id);
+                Box::new(memo_builder)
+            }
+            Self::BurnRedemption(memo_data) => {
+                let mut memo_builder = BurnRedemptionMemoBuilder::new(*memo_data);
+                memo_builder.enable_destination_memo();
+                Box::new(memo_builder)
+            }
+        }
+    }
+}
+
+fn generate_rth_memo_builder(
+    subaddress_index: &Option<u64>,
+    account_key: &AccountKey,
+) -> RTHMemoBuilder {
+    let mut memo_builder = RTHMemoBuilder::default();
+    let sender_memo_credential = match subaddress_index {
+        Some(subaddress_index) => SenderMemoCredential::new_from_address_and_spend_private_key(
+            &account_key.subaddress(*subaddress_index),
+            account_key.subaddress_spend_private(*subaddress_index),
+        ),
+        None => SenderMemoCredential::from(account_key),
+    };
+    memo_builder.set_sender_credential(sender_memo_credential);
+    memo_builder.enable_destination_memo();
+
+    memo_builder
 }
 
 impl TryFrom<&mobilecoind_api::TransactionMemo> for TransactionMemo {
